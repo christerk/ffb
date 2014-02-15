a6b39c796296b2f02eb32e2e832134a8758b732e
tecbeast <Georg@Laptop.fritz.box>
2014-02-15 21:03:21 +0100

    fixed version numbers

--- FantasyFootballClient/src/com/balancedbytes/games/ffb/client/FantasyFootballClient.java
+++ FantasyFootballClient/src/com/balancedbytes/games/ffb/client/FantasyFootballClient.java
@@ -38,8 +38,8 @@ import com.balancedbytes.games.ffb.util.StringTool;
  */
 public class FantasyFootballClient implements IConnectionListener, IDialogCloseListener {
 
-  public static final String CLIENT_VERSION = "1.1.2";
-  public static final String SERVER_VERSION = "1.1.2";
+  public static final String CLIENT_VERSION = "1.1.1";
+  public static final String SERVER_VERSION = "1.1.1";
   
   private Game fGame;
   private UserInterface fUserInterface;
