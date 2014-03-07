4183d4c66e3a5bb22206b572467d397e786e88ae
j129340 <j129340@V100WPWMK1JJ269.v100dpk1.v100.intern>
2014-03-07 17:10:34 +0100

    more cards and pitches

--- FantasyFootballServer/src/com/balancedbytes/games/ffb/server/FantasyFootballServer.java
+++ FantasyFootballServer/src/com/balancedbytes/games/ffb/server/FantasyFootballServer.java
@@ -37,8 +37,8 @@ import com.fumbbl.rng.Fortuna;
  */
 public class FantasyFootballServer {
   
-  public static final String SERVER_VERSION = "1.1.2";
-  public static final String CLIENT_VERSION = "1.1.2";
+  public static final String SERVER_VERSION = "1.1.3";
+  public static final String CLIENT_VERSION = "1.1.3";
 
   private static final String _USAGE = "java -jar FantasyFootballServer.jar standalone\n"
                                      + "java -jar FantasyFootballServer.jar standalone initDb\n"
