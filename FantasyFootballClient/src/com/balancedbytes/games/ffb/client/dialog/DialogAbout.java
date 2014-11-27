0bd95f771a9a94c14a20723fb961a1601cdb6fd4
tecbeast <georg@seipler.de>
2014-11-27 12:35:45 +0100

    about dialog

--- FantasyFootballClient/src/com/balancedbytes/games/ffb/client/dialog/DialogAbout.java
+++ FantasyFootballClient/src/com/balancedbytes/games/ffb/client/dialog/DialogAbout.java
@@ -22,7 +22,7 @@ public class DialogAbout extends Dialog {
   private static final String[] _PLAYTESTERS = {
     "Ale1972, Asharak, Avantar, Avien, Ballcrusher, Balle2000, Barre, Benjysmyth, BiggieB, Brainsaw, Calthor, Carnis, CircularLogic, Chavo,",
     "Clarkin, Cmelchior, Cyrus-Havoc, Don Tomaso, DanTitan76, DukeTyrion, Dynamo380, Ebenezer, Ehlers, Flix, Floppeditbackwards,",
-    "Freak_in_a_Frock, Freppa, Gandresch, Garaygos, Garion, Gjopie, Hangar18, Happygrue, Hitonagashi, Howlett, Janekt, JanMattys, Janzki,",
+    "Freak_in_a_Frock, Freppa, Gandresch, Garaygos, Gjopie, Hangar18, Happygrue, Hitonagashi, Howlett, Janekt, JanMattys, Janzki,",
     "Jarvis_Pants, JoeMalik, Koigokoro, LeBlanc, Lerysh, Lewdgrip, Loraxwolfsbane, Louky, LoxleyAndy, Magistern, Malitrius, Mickael, Mtknight,",
     "MxFr, Nazgob, Nighteye, On1, PhrollikK, Purplegoo, RamonSalazar, Ravenmore, Razin, RedDevilCG, Reisender, Relezite, Shadow46x2, Sl8,",
     "Stej, Steve, Svemole, SvenS, Tarabaralla, Teluriel, Tensai, Thul, Tortured-Robot, Treborius, Ulrik, Ultwe, Uomotigre3, Uuni, Vesikannu,",
