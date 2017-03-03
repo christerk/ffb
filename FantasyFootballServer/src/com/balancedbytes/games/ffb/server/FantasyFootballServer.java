517bd25c25e14e827b8105f76e6867e2e5629315
tecbeast <georg@seipler.de>
2017-03-03 15:59:52 +0100

    updated version numbers

--- FantasyFootballServer/src/com/balancedbytes/games/ffb/server/FantasyFootballServer.java
+++ FantasyFootballServer/src/com/balancedbytes/games/ffb/server/FantasyFootballServer.java
@@ -36,8 +36,8 @@ import com.fumbbl.rng.Fortuna;
  */
 public class FantasyFootballServer {
   
-  public static final String SERVER_VERSION = "1.2.5";
-  public static final String CLIENT_VERSION = "1.2.5";
+  public static final String SERVER_VERSION = "1.2.6";
+  public static final String CLIENT_VERSION = "1.2.6";
 
   private static final String _USAGE = "java -jar FantasyFootballServer.jar standalone\n"
                                      + "java -jar FantasyFootballServer.jar standalone initDb\n"
