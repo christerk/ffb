c4d037d4a2ab96b72f485b5e2dee8ebbdba45079
tecbeast <georg@seipler.de>
2017-03-22 22:13:50 +0100

    improved logging

--- FantasyFootballServer/src/com/balancedbytes/games/ffb/server/request/ServerRequestSaveReplay.java
+++ FantasyFootballServer/src/com/balancedbytes/games/ffb/server/request/ServerRequestSaveReplay.java
@@ -3,6 +3,7 @@ package com.balancedbytes.games.ffb.server.request;
 import com.balancedbytes.games.ffb.server.FantasyFootballServer;
 import com.balancedbytes.games.ffb.server.GameCache;
 import com.balancedbytes.games.ffb.server.GameState;
+import com.balancedbytes.games.ffb.server.IServerLogLevel;
 import com.balancedbytes.games.ffb.server.admin.UtilBackup;
 
 
@@ -36,6 +37,7 @@ public class ServerRequestSaveReplay extends ServerRequest {
     }
     boolean backupOk = UtilBackup.save(gameState);
     if (backupOk) {
+      server.getDebugLog().log(IServerLogLevel.WARN, getGameId(), "Replay stored in file system");
       // request replay to see if backup has been successful, queue delete command
       server.getRequestProcessor().add(new ServerRequestLoadReplay(gameState.getId(), 0, null, ServerRequestLoadReplay.DELETE_GAME));
     }
