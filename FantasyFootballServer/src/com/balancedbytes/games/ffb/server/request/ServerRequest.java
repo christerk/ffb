cf23d9189209684d057bc082dd892469ed704cb5
tecbeast <georg@seipler.de>
2017-03-23 09:56:02 +0100

    formatting

--- FantasyFootballServer/src/com/balancedbytes/games/ffb/server/request/ServerRequest.java
+++ FantasyFootballServer/src/com/balancedbytes/games/ffb/server/request/ServerRequest.java
@@ -1,23 +1,21 @@
 package com.balancedbytes.games.ffb.server.request;
 
-
-
 /**
  * 
  * @author Kalimar
  */
 public abstract class ServerRequest {
-	
-	private String fRequestUrl;
-  
+
+  private String fRequestUrl;
+
   public abstract void process(ServerRequestProcessor pRequestProcessor);
-  
+
   public String getRequestUrl() {
-		return fRequestUrl;
-	}
-  
-  public void setRequestUrl(String pRequestUrl) {
-  	fRequestUrl = pRequestUrl;
+    return fRequestUrl;
   }
-  
+
+  public void setRequestUrl(String pRequestUrl) {
+    fRequestUrl = pRequestUrl;
+  }
+
 }
