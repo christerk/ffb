f3f5e3999279c2d3236f0835e6e994aa2d60444b
tecbeast <georg@seipler.de>
2017-03-28 18:21:15 +0200

    deleting a serialized game does not close it

--- FantasyFootballServer/src/com/balancedbytes/games/ffb/server/handler/ServerCommandHandlerDeleteGame.java
+++ FantasyFootballServer/src/com/balancedbytes/games/ffb/server/handler/ServerCommandHandlerDeleteGame.java
@@ -22,7 +22,9 @@ public class ServerCommandHandlerDeleteGame extends ServerCommandHandler {
 
   public void handleCommand(ReceivedCommand pReceivedCommand) {
     InternalServerCommandDeleteGame deleteGameCommand = (InternalServerCommandDeleteGame) pReceivedCommand.getCommand();
-    getServer().getGameCache().closeGame(deleteGameCommand.getGameId());
+    if (deleteGameCommand.isWithGamesInfo()) {
+      getServer().getGameCache().closeGame(deleteGameCommand.getGameId());
+    }
     getServer().getGameCache().queueDbDelete(deleteGameCommand.getGameId(), deleteGameCommand.isWithGamesInfo());
     getServer().getDebugLog().log(IServerLogLevel.WARN, deleteGameCommand.getGameId(), "GameState deleted from db");
   }
