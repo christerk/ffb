cb2a98e886d08a43a43889ba9fb487237e64f2dd
Christian Huber <huberc@butterbrot.org>
2019-07-19 02:20:56 +0200

    Added class skeletons to filter params for inifile configuration.

---
+++ FantasyFootballServer/src/com/balancedbytes/games/ffb/server/commandline/InifileParamFilter.java
@@ -0,0 +1,8 @@
+package com.balancedbytes.games.ffb.server.commandline;
+
+public class InifileParamFilter {
+
+    public InifileParamFilterResult filterForInifile(String[] argsToFilter) {
+        return new InifileParamFilterResult(null, argsToFilter);
+    }
+}
