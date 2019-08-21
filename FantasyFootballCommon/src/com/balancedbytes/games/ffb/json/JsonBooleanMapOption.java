f23ab30bc4bc1716461dc23854dffd2a31b70c34
Christian Huber <huberc@butterbrot.org>
2019-08-22 01:20:49 +0200

    Add new option type for boolean maps.

--- FantasyFootballCommon/src/com/balancedbytes/games/ffb/json/JsonBooleanMapOption.java
+++ FantasyFootballCommon/src/com/balancedbytes/games/ffb/json/JsonBooleanMapOption.java
@@ -1,4 +1,31 @@
 package com.balancedbytes.games.ffb.json;
 
-public class JsonMapOption {
+import com.eclipsesource.json.JsonObject;
+import com.eclipsesource.json.JsonValue;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class JsonBooleanMapOption extends JsonAbstractOption {
+  public JsonBooleanMapOption(String pKey) {
+    super(pKey);
+  }
+
+  public Map<String, Boolean> getFrom(JsonObject jsonObject) {
+    Map<String, Boolean> map = new HashMap<>();
+    for (String name: jsonObject.names()) {
+      Boolean value = jsonObject.get(name).asBoolean();
+      map.put(name, value);
+    }
+    return map;
+  }
+
+  public void addTo(JsonObject pJsonObject, Map<String, Boolean> map) {
+    JsonObject jsonObject = new JsonObject();
+    for (Map.Entry<String, Boolean> entry: map.entrySet()) {
+      jsonObject.add(entry.getKey(), entry.getValue());
+    }
+
+    addValueTo(pJsonObject, jsonObject);
+  }
 }
