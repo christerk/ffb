2fd3bc17b86701be7b5eeb90f2bed25859be649e
Christer Kaivo-oja <christer@fumbbl.com>
2019-12-18 13:33:18 +0100

    Update to 1.3.6

--- FantasyFootballCommon/src/com/balancedbytes/games/ffb/FantasyFootballConstants.java
+++ FantasyFootballCommon/src/com/balancedbytes/games/ffb/FantasyFootballConstants.java
@@ -1,6 +1,6 @@
 package com.balancedbytes.games.ffb;
 
 public class FantasyFootballConstants {
-	public static final String CLIENT_VERSION = "1.3.5";
-	public static final String SERVER_VERSION = "1.3.5";
+	public static final String CLIENT_VERSION = "1.3.6";
+	public static final String SERVER_VERSION = "1.3.6";
 }
