d9cddea6fd14a6f4c95ac23625a9163691d75d91
Christian Huber <huberc@butterbrot.org>
2020-01-29 23:55:42 +0100

    Implement zap command

--- FantasyFootballCommon/src/com/balancedbytes/games/ffb/net/commands/ServerCommandZapPlayer.java
+++ FantasyFootballCommon/src/com/balancedbytes/games/ffb/net/commands/ServerCommandZapPlayer.java
@@ -1,4 +1,53 @@
 package com.balancedbytes.games.ffb.net.commands;
 
-public class ServerCommandZapPlayer {
+import com.balancedbytes.games.ffb.json.IJsonOption;
+import com.balancedbytes.games.ffb.json.UtilJson;
+import com.balancedbytes.games.ffb.net.NetCommandId;
+import com.eclipsesource.json.JsonObject;
+import com.eclipsesource.json.JsonValue;
+
+public class ServerCommandZapPlayer extends ServerCommand {
+  private String teamId;
+  private String playerId;
+
+  public ServerCommandZapPlayer(String playerId, String teamId) {
+    this.teamId = teamId;
+    this.playerId = playerId;
+  }
+
+  public ServerCommandZapPlayer() {
+  }
+
+  @Override
+  public NetCommandId getId() {
+    return NetCommandId.SERVER_ZAP_PLAYER;
+  }
+
+  public String getTeamId() {
+    return teamId;
+  }
+
+  public String getPlayerId() {
+    return playerId;
+  }
+
+  @Override
+  public Object initFrom(JsonValue pJsonValue) {
+    JsonObject jsonObject = UtilJson.toJsonObject(pJsonValue);
+    UtilNetCommand.validateCommandId(this, (NetCommandId) IJsonOption.NET_COMMAND_ID.getFrom(jsonObject));
+    setCommandNr(IJsonOption.COMMAND_NR.getFrom(jsonObject));
+    teamId = IJsonOption.TEAM_ID.getFrom(jsonObject);
+    playerId = IJsonOption.PLAYER_ID.getFrom(jsonObject);
+    return this;
+  }
+
+  @Override
+  public JsonValue toJsonValue() {
+    JsonObject jsonObject = new JsonObject();
+    IJsonOption.NET_COMMAND_ID.addTo(jsonObject, getId());
+    IJsonOption.COMMAND_NR.addTo(jsonObject, getCommandNr());
+    IJsonOption.TEAM_ID.addTo(jsonObject, teamId);
+    IJsonOption.PLAYER_ID.addTo(jsonObject, playerId);
+    return jsonObject;
+  }
 }
