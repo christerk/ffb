04ce8a1e3d7243bf371a12f2a13935da53a1ba03
Christer Kaivo-oja <christer@fumbbl.com>
2020-05-24 01:37:22 +0200

    Bump version to 1.3.9

--- FantasyFootballCommon/src/com/balancedbytes/games/ffb/FantasyFootballConstants.java
+++ FantasyFootballCommon/src/com/balancedbytes/games/ffb/FantasyFootballConstants.java
@@ -1,6 +1,6 @@
 package com.balancedbytes.games.ffb;
 
 public class FantasyFootballConstants {
-	public static final String CLIENT_VERSION = "1.3.8";
-	public static final String SERVER_VERSION = "1.3.8";
+	public static final String CLIENT_VERSION = "1.3.9";
+	public static final String SERVER_VERSION = "1.3.9";
 }
