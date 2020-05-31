759c6d0e5cd075698c39946bc5aa3bff3a52f28f
Christian Huber <huberc@butterbrot.org>
2020-06-01 00:38:50 +0200

    Ensure streams in ffb client are closed

--- FantasyFootballClient/src/com/balancedbytes/games/ffb/client/FantasyFootballClient.java
+++ FantasyFootballClient/src/com/balancedbytes/games/ffb/client/FantasyFootballClient.java
@@ -255,9 +255,15 @@ public class FantasyFootballClient implements IConnectionListener, IDialogCloseL
 
   public void loadProperties() throws IOException {
     fProperties = new Properties();
-    InputStream propertyInputStream = getClass().getResourceAsStream("/client.ini");
-    fProperties.load(propertyInputStream);
-    propertyInputStream.close();
+    InputStream propertyInputStream = null;
+    try {
+      propertyInputStream = getClass().getResourceAsStream("/client.ini");
+      fProperties.load(propertyInputStream);
+    } finally {
+      if (propertyInputStream != null) {
+        propertyInputStream.close();
+      }
+    }
   }
 
   public ClientData getClientData() {
