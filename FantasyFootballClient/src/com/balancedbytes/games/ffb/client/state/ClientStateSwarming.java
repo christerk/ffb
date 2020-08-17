5585774eb374d5888854788f4e03dbd857e49e9e
Christian Huber <huberc@butterbrot.org>
2020-08-17 15:30:41 +0200

    Extend setup state

--- FantasyFootballClient/src/com/balancedbytes/games/ffb/client/state/ClientStateSwarming.java
+++ FantasyFootballClient/src/com/balancedbytes/games/ffb/client/state/ClientStateSwarming.java
@@ -3,7 +3,7 @@ package com.balancedbytes.games.ffb.client.state;
 import com.balancedbytes.games.ffb.ClientStateId;
 import com.balancedbytes.games.ffb.client.FantasyFootballClient;
 
-public class ClientStateSwarming extends ClientState {
+public class ClientStateSwarming extends ClientStateSetup {
 
   public ClientStateSwarming(FantasyFootballClient pClient) {
     super(pClient);
