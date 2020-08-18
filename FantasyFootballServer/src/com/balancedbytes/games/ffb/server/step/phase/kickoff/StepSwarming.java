d446d39497bd0d2943d87334c32c9c5f01fab94d
Christian Huber <huberc@butterbrot.org>
2020-08-19 00:27:48 +0200

    Add new fields and json boiler plate

--- FantasyFootballServer/src/com/balancedbytes/games/ffb/server/step/phase/kickoff/StepSwarming.java
+++ FantasyFootballServer/src/com/balancedbytes/games/ffb/server/step/phase/kickoff/StepSwarming.java
@@ -1,13 +1,19 @@
 package com.balancedbytes.games.ffb.server.step.phase.kickoff;
 
 import com.balancedbytes.games.ffb.server.GameState;
+import com.balancedbytes.games.ffb.server.IServerJsonOption;
 import com.balancedbytes.games.ffb.server.net.ReceivedCommand;
 import com.balancedbytes.games.ffb.server.step.AbstractStep;
 import com.balancedbytes.games.ffb.server.step.StepAction;
 import com.balancedbytes.games.ffb.server.step.StepCommandStatus;
 import com.balancedbytes.games.ffb.server.step.StepId;
+import com.eclipsesource.json.JsonObject;
+import com.eclipsesource.json.JsonValue;
 
 public class StepSwarming extends AbstractStep {
+
+  private boolean fEndTurn;
+  private boolean handleKickingTeam;
 
   public StepSwarming(GameState pGameState) {
     super(pGameState);
@@ -34,6 +40,22 @@ public class StepSwarming extends AbstractStep {
 
   private void executeStep() {
 
-    getResult().setNextAction(StepAction.NEXT_STEP);
+  }
+
+  @Override
+  public JsonObject toJsonValue() {
+    JsonObject jsonObject = super.toJsonValue();
+    IServerJsonOption.END_TURN.addTo(jsonObject, fEndTurn);
+    IServerJsonOption.HANDLE_KICKING_TEAM.addTo(jsonObject, handleKickingTeam);
+    return jsonObject;
+  }
+
+  @Override
+  public StepSwarming initFrom(JsonValue pJsonValue) {
+    super.initFrom(pJsonValue);
+    JsonObject jsonObject = UtilJson.toJsonObject(pJsonValue);
+    fEndTurn = IServerJsonOption.END_TURN.getFrom(jsonObject);
+    handleKickingTeam = IServerJsonOption.HANDLE_KICKING_TEAM.getFrom(jsonObject);
+    return this;
   }
 }
