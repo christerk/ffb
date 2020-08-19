4f0fa3989f4dbf99cdac0a15844f32433ac9bbbd
Christian Huber <huberc@butterbrot.org>
2020-08-19 23:44:48 +0200

    Only allow swarming players to be set up

--- FantasyFootballClient/src/com/balancedbytes/games/ffb/client/state/ClientStateSwarming.java
+++ FantasyFootballClient/src/com/balancedbytes/games/ffb/client/state/ClientStateSwarming.java
@@ -1,7 +1,11 @@
 package com.balancedbytes.games.ffb.client.state;
 
 import com.balancedbytes.games.ffb.ClientStateId;
+import com.balancedbytes.games.ffb.FieldCoordinate;
+import com.balancedbytes.games.ffb.Skill;
 import com.balancedbytes.games.ffb.client.FantasyFootballClient;
+import com.balancedbytes.games.ffb.model.Player;
+import com.balancedbytes.games.ffb.util.UtilCards;
 
 public class ClientStateSwarming extends ClientStateSetup {
 
@@ -12,5 +16,15 @@ public class ClientStateSwarming extends ClientStateSetup {
   @Override
   public ClientStateId getId() {
     return ClientStateId.SWARMING;
+  }
+
+  @Override
+  public boolean isInitDragAllowed(FieldCoordinate pCoordinate) {
+    if (pCoordinate != null) {
+      Player player = getClient().getGame().getFieldModel().getPlayer(pCoordinate);
+      return player != null && UtilCards.hasSkill(getClient().getGame(), player, Skill.SWARMING);
+    }
+
+    return false;
   }
 }
