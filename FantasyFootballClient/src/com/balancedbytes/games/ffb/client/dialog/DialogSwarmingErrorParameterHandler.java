3f804154886ca149749acb25731b96a8700f79bb
Christian Huber <huberc@butterbrot.org>
2020-08-21 22:31:24 +0200

    Reopen reserves if too many swarming players were placed

--- FantasyFootballClient/src/com/balancedbytes/games/ffb/client/dialog/DialogSwarmingErrorParameterHandler.java
+++ FantasyFootballClient/src/com/balancedbytes/games/ffb/client/dialog/DialogSwarmingErrorParameterHandler.java
@@ -1,9 +1,11 @@
 package com.balancedbytes.games.ffb.client.dialog;
 
+import com.balancedbytes.games.ffb.BoxType;
 import com.balancedbytes.games.ffb.ClientMode;
 import com.balancedbytes.games.ffb.StatusType;
 import com.balancedbytes.games.ffb.client.FantasyFootballClient;
 import com.balancedbytes.games.ffb.client.IIconProperty;
+import com.balancedbytes.games.ffb.client.ui.SideBarComponent;
 import com.balancedbytes.games.ffb.dialog.DialogSwarmingErrorParameter;
 import com.balancedbytes.games.ffb.model.Game;
 
@@ -21,6 +23,11 @@ public class DialogSwarmingErrorParameterHandler extends DialogHandler {
     int actual = ((DialogSwarmingErrorParameter)game.getDialogParameter()).getActual();
 
     if ((ClientMode.PLAYER == getClient().getMode()) && game.isHomePlaying()) {
+      SideBarComponent sideBarHome = getClient().getUserInterface().getSideBarHome();
+      if (!sideBarHome.isBoxOpen()) {
+        sideBarHome.openBox(BoxType.RESERVES);
+      }
+
       setDialog(new DialogInformation(getClient(), "Too many swarming players", new String[] { "You placed " +
         actual + " players rather than the allowed " + allowed + "." },
         DialogInformation.OK_DIALOG, IIconProperty.GAME_REF));
