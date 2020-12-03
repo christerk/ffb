ca9f19a077055563ff31514a1a503aa05af071f3
Christian Huber <huberc@butterbrot.org>
2020-12-03 19:24:33 +0100

    Improve logging as not all skills require a behaviour

--- FantasyFootballServer/src/com/balancedbytes/games/ffb/server/util/UtilSkillBehaviours.java
+++ FantasyFootballServer/src/com/balancedbytes/games/ffb/server/util/UtilSkillBehaviours.java
@@ -6,6 +6,8 @@ import com.balancedbytes.games.ffb.server.DebugLog;
 import com.balancedbytes.games.ffb.server.IServerLogLevel;
 import com.balancedbytes.games.ffb.server.model.SkillBehaviour;
 
+import java.lang.reflect.InvocationTargetException;
+
 public class UtilSkillBehaviours {
 
   public static void registerBehaviours(SkillFactory factory, DebugLog log) {
@@ -15,16 +17,27 @@ public class UtilSkillBehaviours {
       String skillClassName = skill.getClass().getSimpleName();
 
       try {
-        Class<?> behaviourClass = Class.forName(behaviourPackage+"."+skillClassName + "Behaviour");
-        registerBehaviour((SkillBehaviour<?>) behaviourClass.getConstructor((Class<?>[])null).newInstance((Object[])null), factory);
-      } catch (Exception e) {
-        log.log(IServerLogLevel.WARN, "Failed to register behaviour for '" + skill.getName() + "': " + e.getMessage());
+        Class<?> behaviourClass = Class.forName(behaviourPackage + "." + skillClassName + "Behaviour");
+        SkillBehaviour<?> behaviour = (SkillBehaviour<?>) behaviourClass.getConstructor((Class<?>[])null).newInstance((Object[])null);
+        if (registerBehaviour(behaviour, factory, log)) {
+          log.log(IServerLogLevel.DEBUG, "Registered behavior class '" + behaviourClass.getSimpleName() + "' for skill '" + skillClassName + "'");
+        } else {
+          log.log(IServerLogLevel.WARN, "No skill found for '" + behaviour.getClass().getSimpleName());
+        }
+      } catch (ClassNotFoundException e) {
+        log.log(IServerLogLevel.DEBUG, "No behavior found for '" + skillClassName + "'");
+      } catch (InstantiationException | InvocationTargetException | NoSuchMethodException | IllegalAccessException e) {
+        log.log(IServerLogLevel.WARN, "Failed to register behaviour for '" + skillClassName + "': " + e.getMessage());
       }
     }
   }
 
-  private static void registerBehaviour(SkillBehaviour<?> behaviour, SkillFactory factory) {
+  private static boolean registerBehaviour(SkillBehaviour<?> behaviour, SkillFactory factory, DebugLog log) {
     Skill skill = factory.forClass(behaviour.skillClass);
-    behaviour.setSkill(skill);
+    if (skill != null) {
+      behaviour.setSkill(skill);
+      return true;
+    }
+    return false;
   }
 }
