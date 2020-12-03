dfa0f8b8376e1ba7f6b657711fa33a7ac0f724a2
Christian Huber <huberc@butterbrot.org>
2020-12-03 18:06:36 +0100

    Remove static factory field

--- FantasyFootballServer/src/com/balancedbytes/games/ffb/server/util/UtilSkillBehaviours.java
+++ FantasyFootballServer/src/com/balancedbytes/games/ffb/server/util/UtilSkillBehaviours.java
@@ -5,11 +5,8 @@ import com.balancedbytes.games.ffb.model.Skill;
 import com.balancedbytes.games.ffb.server.model.SkillBehaviour;
 
 public class UtilSkillBehaviours {
-  private static SkillFactory factory;
 
-  @SuppressWarnings("unchecked")
   public static void RegisterBehaviours(SkillFactory factory) {
-    UtilSkillBehaviours.factory = factory;
 
     String behaviourPackage = "com.balancedbytes.games.ffb.server.skillbehaviour";
     for (Skill skill : factory.getSkills()) {
@@ -17,13 +14,12 @@ public class UtilSkillBehaviours {
 
       try {
         Class behaviourClass = Class.forName(behaviourPackage+"."+skillClassName + "Behaviour");
-        registerBehaviour((SkillBehaviour<?>) behaviourClass.getConstructor((Class[])null).newInstance((Object[])null));
+        registerBehaviour((SkillBehaviour<?>) behaviourClass.getConstructor((Class[])null).newInstance((Object[])null), factory);
       } catch (Exception e) { }
     }
   }
 
-  private static void registerBehaviour(SkillBehaviour<?> behaviour) {
-    
+  private static void registerBehaviour(SkillBehaviour<?> behaviour, SkillFactory factory) {
     Skill skill = factory.forClass(behaviour.skillClass);
     behaviour.setSkill(skill);
   }
