3ccb1c39abcbfdeaa4c958f7441acc689015b0f3
Christian Huber <huberc@butterbrot.org>
2021-01-23 19:32:14 +0100

    Add PassResult

--- FantasyFootballCommon/src/com/balancedbytes/games/ffb/mechanics/PassResult.java
+++ FantasyFootballCommon/src/com/balancedbytes/games/ffb/mechanics/PassResult.java
@@ -1,5 +1,12 @@
 package com.balancedbytes.games.ffb.mechanics;
 
-public enum PassResult {
+import com.balancedbytes.games.ffb.INamedObject;
+
+public enum PassResult implements INamedObject {
 	FUMBLE, SAVED_FUMBLE, WILDLY_INACCURATE, INACCURATE, ACCURATE;
+
+	@Override
+	public String getName() {
+		return name();
+	}
 }
