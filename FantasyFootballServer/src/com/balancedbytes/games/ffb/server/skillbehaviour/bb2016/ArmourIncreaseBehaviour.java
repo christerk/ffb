bfe39d8daf768141352bc67bd4ff085c92e6f2ac
Christian Huber <huberc@butterbrot.org>
2021-02-03 00:25:32 +0100

    Fix import

--- FantasyFootballServer/src/com/balancedbytes/games/ffb/server/skillbehaviour/bb2016/ArmourIncreaseBehaviour.java
+++ FantasyFootballServer/src/com/balancedbytes/games/ffb/server/skillbehaviour/bb2016/ArmourIncreaseBehaviour.java
@@ -3,7 +3,7 @@ package com.balancedbytes.games.ffb.server.skillbehaviour.bb2016;
 import com.balancedbytes.games.ffb.RulesCollection;
 import com.balancedbytes.games.ffb.RulesCollection.Rules;
 import com.balancedbytes.games.ffb.server.model.SkillBehaviour;
-import com.balancedbytes.games.ffb.skill.bb2020.ArmourIncrease;
+import com.balancedbytes.games.ffb.skill.bb2016.ArmourIncrease;
 
 @RulesCollection(Rules.BB2016)
 public class ArmourIncreaseBehaviour extends SkillBehaviour<ArmourIncrease> {
