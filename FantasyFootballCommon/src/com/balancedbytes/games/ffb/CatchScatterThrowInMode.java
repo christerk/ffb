a24b9866388d39558bdf8a892cc10e9a162085c9
Christian Huber <huberc@butterbrot.org>
2021-02-12 18:06:54 +0100

    Cleanup

--- FantasyFootballCommon/src/com/balancedbytes/games/ffb/CatchScatterThrowInMode.java
+++ FantasyFootballCommon/src/com/balancedbytes/games/ffb/CatchScatterThrowInMode.java
@@ -6,18 +6,25 @@ package com.balancedbytes.games.ffb;
  */
 public enum CatchScatterThrowInMode implements INamedObject {
 
-	CATCH_ACCURATE_PASS("catchAccuratePass", false),
-	CATCH_ACCURATE_PASS_EMPTY_SQUARE("catchAccuratePassEmptySquare", false), CATCH_HAND_OFF("catchHandOff", false),
-	CATCH_SCATTER("catchScatter", false), SCATTER_BALL("scatterBall", false), THROW_IN("throwIn", false),
-	CATCH_MISSED_PASS("catchMissedPass", false), CATCH_KICKOFF("catchKickoff", false),
-	CATCH_THROW_IN("catchThrowIn", false), FAILED_CATCH("failedCatch", false), FAILED_PICK_UP("failedPickUp", false),
 	CATCH_ACCURATE_BOMB("catchAccurateBomb", true),
-	CATCH_ACCURATE_BOMB_EMPTY_SQUARE("catchAccurateBombEmptySquare", true), CATCH_BOMB("catchBomb", true);
+	CATCH_ACCURATE_BOMB_EMPTY_SQUARE("catchAccurateBombEmptySquare", true),
+	CATCH_ACCURATE_PASS("catchAccuratePass", false),
+	CATCH_ACCURATE_PASS_EMPTY_SQUARE("catchAccuratePassEmptySquare", false),
+	CATCH_BOMB("catchBomb", true),
+	CATCH_HAND_OFF("catchHandOff", false),
+	CATCH_KICKOFF("catchKickoff", false),
+	CATCH_MISSED_PASS("catchMissedPass", false),
+	CATCH_SCATTER("catchScatter", false),
+	CATCH_THROW_IN("catchThrowIn", false),
+	FAILED_CATCH("failedCatch", false),
+	FAILED_PICK_UP("failedPickUp", false),
+	SCATTER_BALL("scatterBall", false),
+	THROW_IN("throwIn", false);
 
-	private String fName;
-	private boolean fBomb;
+	private final String fName;
+	private final boolean fBomb;
 
-	private CatchScatterThrowInMode(String pName, boolean pBomb) {
+	CatchScatterThrowInMode(String pName, boolean pBomb) {
 		fName = pName;
 		fBomb = pBomb;
 	}
