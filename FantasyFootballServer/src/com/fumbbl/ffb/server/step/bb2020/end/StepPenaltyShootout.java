e0052f43648576b3b2c4a4eb38b1c58e90541039
Christian Huber <huberc@butterbrot.org>
2021-05-20 19:08:28 +0200

    Implement penalty step

--- FantasyFootballServer/src/com/fumbbl/ffb/server/step/bb2020/end/StepPenaltyShootout.java
+++ FantasyFootballServer/src/com/fumbbl/ffb/server/step/bb2020/end/StepPenaltyShootout.java
@@ -1,4 +1,4 @@
-package com.fumbbl.ffb.server.step.game.end;
+package com.fumbbl.ffb.server.step.bb2020.end;
 
 import com.eclipsesource.json.JsonObject;
 import com.eclipsesource.json.JsonValue;
@@ -6,7 +6,7 @@ import com.fumbbl.ffb.RulesCollection;
 import com.fumbbl.ffb.factory.IFactorySource;
 import com.fumbbl.ffb.model.Game;
 import com.fumbbl.ffb.model.GameResult;
-import com.fumbbl.ffb.report.bb2016.ReportPenaltyShootout;
+import com.fumbbl.ffb.report.bb2020.ReportPenaltyShootout;
 import com.fumbbl.ffb.server.GameState;
 import com.fumbbl.ffb.server.step.AbstractStep;
 import com.fumbbl.ffb.server.step.StepAction;
@@ -17,8 +17,10 @@ import com.fumbbl.ffb.server.step.StepId;
  * 
  * @author Kalimar
  */
-@RulesCollection(RulesCollection.Rules.COMMON)
+@RulesCollection(RulesCollection.Rules.BB2020)
 public final class StepPenaltyShootout extends AbstractStep {
+
+	private static final int SHOOTOUT_LIMIT = 5;
 
 	public StepPenaltyShootout(GameState pGameState) {
 		super(pGameState);
@@ -39,22 +41,34 @@ public final class StepPenaltyShootout extends AbstractStep {
 		GameResult gameResult = game.getGameResult();
 		if ((game.getHalf() > 2)
 				&& (gameResult.getTeamResultHome().getScore() == gameResult.getTeamResultAway().getScore())) {
-			int rollHome = 0, reRollsLeftHome = 0, penaltyScoreHome = 0;
-			int rollAway = 0, reRollsLeftAway = 0, penaltyScoreAway = 0;
-			while (penaltyScoreHome == penaltyScoreAway) {
+			int rollHome = 0, penaltyScoreHome = 0;
+			int rollAway = 0, penaltyScoreAway = 0;
+			while (penaltyScoreHome + penaltyScoreAway < SHOOTOUT_LIMIT) {
 				rollHome = getGameState().getDiceRoller().rollPenaltyShootout();
-				reRollsLeftHome = game.getTurnDataHome().getReRolls();
-				penaltyScoreHome = rollHome + reRollsLeftHome;
 				rollAway = getGameState().getDiceRoller().rollPenaltyShootout();
-				reRollsLeftAway = game.getTurnDataAway().getReRolls();
-				penaltyScoreAway = rollAway + reRollsLeftAway;
+				String winningCoach = null;
+				if (rollAway > rollHome) {
+					winningCoach = game.getTeamAway().getCoach();
+					penaltyScoreAway++;
+				} else if (rollAway < rollHome) {
+					winningCoach = game.getTeamHome().getCoach();
+					penaltyScoreHome++;
+				}
+
+				String teamId = null;
+
+				if (penaltyScoreHome + penaltyScoreAway == SHOOTOUT_LIMIT) {
+					if (penaltyScoreHome > penaltyScoreAway) {
+						gameResult.getTeamResultHome().setScore(gameResult.getTeamResultHome().getScore() + 1);
+						teamId = game.getTeamHome().getId();
+					} else {
+						gameResult.getTeamResultAway().setScore(gameResult.getTeamResultAway().getScore() + 1);
+						teamId = game.getTeamAway().getId();
+					}
+				}
+
+				getResult().addReport(new ReportPenaltyShootout(rollHome, penaltyScoreHome, rollAway, penaltyScoreAway, winningCoach, toOrdinal(penaltyScoreAway + penaltyScoreHome), teamId));
 			}
-			if (penaltyScoreHome > penaltyScoreAway) {
-				gameResult.getTeamResultHome().setScore(gameResult.getTeamResultHome().getScore() + 1);
-			} else {
-				gameResult.getTeamResultAway().setScore(gameResult.getTeamResultAway().getScore() + 1);
-			}
-			getResult().addReport(new ReportPenaltyShootout(rollHome, reRollsLeftHome, rollAway, reRollsLeftAway));
 		}
 		getResult().setNextAction(StepAction.NEXT_STEP);
 	}
@@ -70,6 +84,19 @@ public final class StepPenaltyShootout extends AbstractStep {
 	public StepPenaltyShootout initFrom(IFactorySource game, JsonValue pJsonValue) {
 		super.initFrom(game, pJsonValue);
 		return this;
+	}
+
+	private String toOrdinal(int number) {
+		switch (number) {
+			case 1:
+					return "1st";
+			case 2:
+				return "2nd";
+			case 3:
+				return "3rd";
+			default:
+				return number + "th";
+		}
 	}
 
 }
