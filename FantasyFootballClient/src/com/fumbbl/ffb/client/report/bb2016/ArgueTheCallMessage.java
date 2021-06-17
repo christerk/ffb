ac75ce05f5e9a24f5d0cb3dea8c5423586d67859
Christian Huber <huberc@butterbrot.org>
2021-06-17 22:56:05 +0200

    Add missing filed

--- FantasyFootballClient/src/com/fumbbl/ffb/client/report/bb2016/ArgueTheCallMessage.java
+++ FantasyFootballClient/src/com/fumbbl/ffb/client/report/bb2016/ArgueTheCallMessage.java
@@ -1,14 +1,16 @@
-package com.fumbbl.ffb.client.report;
+package com.fumbbl.ffb.client.report.bb2016;
 
 import com.fumbbl.ffb.RulesCollection;
 import com.fumbbl.ffb.RulesCollection.Rules;
 import com.fumbbl.ffb.client.TextStyle;
+import com.fumbbl.ffb.client.report.ReportMessageBase;
+import com.fumbbl.ffb.client.report.ReportMessageType;
 import com.fumbbl.ffb.model.Player;
-import com.fumbbl.ffb.report.ReportArgueTheCallRoll;
 import com.fumbbl.ffb.report.ReportId;
+import com.fumbbl.ffb.report.bb2016.ReportArgueTheCallRoll;
 
 @ReportMessageType(ReportId.ARGUE_THE_CALL)
-@RulesCollection(Rules.COMMON)
+@RulesCollection(Rules.BB2016)
 public class ArgueTheCallMessage extends ReportMessageBase<ReportArgueTheCallRoll> {
 
     @Override
@@ -18,17 +20,12 @@ public class ArgueTheCallMessage extends ReportMessageBase<ReportArgueTheCallRol
   		status.append("Argue the Call Roll [ ").append(report.getRoll()).append(" ]");
   		println(getIndent(), TextStyle.ROLL, status.toString());
   		if (report.isSuccessful()) {
-  			print(getIndent() + 1, TextStyle.NONE, "The ref refrains from banning ");
-  			print(getIndent() + 1, false, player);
-  			status = new StringBuilder();
-  			status.append(" and ").append(player.getPlayerGender().getNominative());
-  			if (report.isStaysOnPitch()) {
-  				status.append(" stays on the pitch.");
-  			} else {
-  				status.append(" is sent to the reserve instead.");
-  			}
-  			println(getIndent() + 1, TextStyle.NONE, status.toString());
-  		} else {
+			  print(getIndent() + 1, TextStyle.NONE, "The ref refrains from banning ");
+			  print(getIndent() + 1, false, player);
+			  status = new StringBuilder();
+			  status.append(" and ").append(player.getPlayerGender().getNominative()).append(" is sent to the reserve instead.");
+			  println(getIndent() + 1, TextStyle.NONE, status.toString());
+		  } else {
   			print(getIndent() + 1, TextStyle.NONE, "The ref bans ");
   			print(getIndent() + 1, false, player);
   			println(getIndent() + 1, TextStyle.NONE, " from the game.");
