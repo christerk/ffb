dd9daa81d6a457c7016ff3102fe95f79abdf2220
Christian Huber <huberc@butterbrot.org>
2021-06-27 22:49:33 +0200

    Bugfix in stat modifier serialization

--- FantasyFootballCommon/src/com/fumbbl/ffb/factory/TemporaryStatModifierFactory.java
+++ FantasyFootballCommon/src/com/fumbbl/ffb/factory/TemporaryStatModifierFactory.java
@@ -1,25 +1,25 @@
 package com.fumbbl.ffb.factory;
 
-import java.lang.reflect.Constructor;
-import java.lang.reflect.InvocationTargetException;
-
 import com.fumbbl.ffb.FactoryType;
 import com.fumbbl.ffb.FantasyFootballException;
 import com.fumbbl.ffb.RulesCollection;
 import com.fumbbl.ffb.model.Game;
 import com.fumbbl.ffb.modifiers.TemporaryStatModifier;
 
+import java.lang.reflect.Constructor;
+import java.lang.reflect.InvocationTargetException;
+
 @FactoryType(FactoryType.Factory.TEMPORARY_STAT_MODIFIER)
 @RulesCollection(RulesCollection.Rules.COMMON)
 public class TemporaryStatModifierFactory implements INamedObjectFactory<TemporaryStatModifier> {
 	@Override
 	public TemporaryStatModifier forName(String name) {
-		String[] parts = name.split("_");
+		String[] parts = name.split(TemporaryStatModifier.NAME_SEPARATOR);
 		try {
 			Constructor<?> constructor = Class.forName(parts[1]).getConstructor(TemporaryStatModifier.PlayerStat.class);
 			return (TemporaryStatModifier) constructor.newInstance(TemporaryStatModifier.PlayerStat.valueOf(parts[0]));
 		} catch (ClassNotFoundException | NoSuchMethodException | IllegalAccessException | InstantiationException | InvocationTargetException e) {
-			throw new FantasyFootballException("Could not create temporary stat modifier for '" + name + "' due to: ", e );
+			throw new FantasyFootballException("Could not create temporary stat modifier for '" + name + "' due to: ", e);
 		}
 	}
 
