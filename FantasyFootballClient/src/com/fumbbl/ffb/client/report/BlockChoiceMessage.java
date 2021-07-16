2282855f75145285a78d666e41573c9e99c90548
Mike Stowell <michael.stowell@unity3d.com>
2021-07-16 21:36:39 +0100

    Both down result correctly logs that block didnt help when defender has no tacklezones

--- FantasyFootballClient/src/com/fumbbl/ffb/client/report/BlockChoiceMessage.java
+++ FantasyFootballClient/src/com/fumbbl/ffb/client/report/BlockChoiceMessage.java
@@ -1,5 +1,6 @@
 package com.fumbbl.ffb.client.report;
 
+import com.fumbbl.ffb.PlayerState;
 import com.fumbbl.ffb.RulesCollection;
 import com.fumbbl.ffb.RulesCollection.Rules;
 import com.fumbbl.ffb.client.TextStyle;
@@ -41,12 +42,22 @@ public class BlockChoiceMessage extends ReportMessageBase<ReportBlockChoice> {
   				if (defender.hasSkillProperty(NamedProperties.preventFallOnBothDown)) {
   					print(getIndent() + 1, false, defender);
   					status = new StringBuilder();
-  					status
-  						.append(" has been saved by ")
-  						.append(defender.getPlayerGender().getGenitive())
-  						.append(" ")
-  						.append(defender.getSkillWithProperty(NamedProperties.preventFallOnBothDown))
-  						.append(" skill.");
+  					PlayerState playerState = game.getFieldModel().getPlayerState(defender);
+  					if(playerState.hasTacklezones()) {
+  						status
+  							.append(" has been saved by ")
+  							.append(defender.getPlayerGender().getGenitive())
+  							.append(" ")
+  							.append(defender.getSkillWithProperty(NamedProperties.preventFallOnBothDown))
+  							.append(" skill.");
+  					} else {
+  						status
+							.append(" has not been saved by ")
+							.append(defender.getPlayerGender().getGenitive())
+							.append(" ")
+							.append(defender.getSkillWithProperty(NamedProperties.preventFallOnBothDown))
+							.append(" skill, due to having no tacklezones.");
+  					}
   					println(getIndent() + 1, status.toString());
   				}
   				break;
