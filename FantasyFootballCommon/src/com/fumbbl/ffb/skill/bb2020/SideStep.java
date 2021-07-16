03f62ba3c45127af1bbe9f611b434d205f89da20
Mike Stowell <michael.stowell@unity3d.com>
2021-07-16 20:56:21 +0100

    Sidestep no longer cancels grab in 2020

---
+++ FantasyFootballCommon/src/com/fumbbl/ffb/skill/bb2020/SideStep.java
@@ -0,0 +1,35 @@
+package com.fumbbl.ffb.skill.bb2020;
+
+import com.fumbbl.ffb.RulesCollection;
+import com.fumbbl.ffb.SkillCategory;
+import com.fumbbl.ffb.RulesCollection.Rules;
+import com.fumbbl.ffb.model.property.NamedProperties;
+import com.fumbbl.ffb.model.skill.Skill;
+
+/**
+ * A player with this skill is an expert at stepping neatly out of the way of an
+ * attacker. To represent this ability, his coach may choose which square the
+ * player is moved to when he is pushed back, rather than the opposing coach.
+ * Furthermore, the coach may choose to move the player to any adjacent square,
+ * not just the three squares shown on the Push Back diagram. Note that the
+ * player may not use this skill if there are no open squares on the pitch
+ * adjacent to this player. Note that the coach may choose which square the
+ * player is moved to even if the player is Knocked Down after the push back.
+ */
+@RulesCollection(Rules.BB2020)
+public class SideStep extends Skill {
+
+	public SideStep() {
+		super("Side Step", SkillCategory.AGILITY);
+	}
+
+	@Override
+	public void postConstruct() {
+		registerProperty(NamedProperties.canChooseOwnPushedBackSquare);
+	}
+
+	@Override
+	public String[] getSkillUseDescription() {
+		return new String[] { "Using SideStep will allow you to chose the square you are pushed to." };
+	}
+}
