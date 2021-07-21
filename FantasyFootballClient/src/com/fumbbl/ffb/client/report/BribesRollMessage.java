e95772985793052bbf4304e5537ff0332da1665f
Christer Kaivo-oja <christer@fumbbl.com>
2021-07-21 13:32:49 +0200

    Added support for verb form queries from PlayerGender. Also include an example of use (BribesRollMessage).

--- FantasyFootballClient/src/com/fumbbl/ffb/client/report/BribesRollMessage.java
+++ FantasyFootballClient/src/com/fumbbl/ffb/client/report/BribesRollMessage.java
@@ -1,5 +1,6 @@
 package com.fumbbl.ffb.client.report;
 
+import com.fumbbl.ffb.PlayerGender;
 import com.fumbbl.ffb.RulesCollection;
 import com.fumbbl.ffb.RulesCollection.Rules;
 import com.fumbbl.ffb.client.TextStyle;
@@ -18,10 +19,11 @@ public class BribesRollMessage extends ReportMessageBase<ReportBribesRoll> {
   		status.append("Bribes Roll [ ").append(report.getRoll()).append(" ]");
   		println(getIndent(), TextStyle.ROLL, status.toString());
   		if (report.isSuccessful()) {
+  			PlayerGender gender = player.getPlayerGender();
   			print(getIndent() + 1, TextStyle.NONE, "The ref refrains from penalizing ");
   			print(getIndent() + 1, false, player);
   			status = new StringBuilder();
-  			status.append(" and ").append(player.getPlayerGender().getNominative()).append(" remains in the game.");
+  			status.append(" and ").append(gender.getNominative()).append(" "+gender.getVerbForm("remains",  "remain")+" in the game.");
   			println(getIndent() + 1, TextStyle.NONE, status.toString());
   		} else {
   			print(getIndent() + 1, TextStyle.NONE, "The ref appears to be unimpressed and ");
