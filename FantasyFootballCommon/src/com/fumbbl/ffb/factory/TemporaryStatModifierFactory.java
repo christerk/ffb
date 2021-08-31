3380f7472e48347917305408f92baf988f7308c5
Christian Huber <huberc@butterbrot.org>
2021-08-31 19:59:30 +0200

    Add mechanic to reflection constructor call.

--- FantasyFootballCommon/src/com/fumbbl/ffb/factory/TemporaryStatModifierFactory.java
+++ FantasyFootballCommon/src/com/fumbbl/ffb/factory/TemporaryStatModifierFactory.java
@@ -3,6 +3,8 @@ package com.fumbbl.ffb.factory;
 import com.fumbbl.ffb.FactoryType;
 import com.fumbbl.ffb.FantasyFootballException;
 import com.fumbbl.ffb.RulesCollection;
+import com.fumbbl.ffb.mechanics.Mechanic;
+import com.fumbbl.ffb.mechanics.StatsMechanic;
 import com.fumbbl.ffb.model.Game;
 import com.fumbbl.ffb.modifiers.TemporaryStatModifier;
 
@@ -12,12 +14,16 @@ import java.lang.reflect.InvocationTargetException;
 @FactoryType(FactoryType.Factory.TEMPORARY_STAT_MODIFIER)
 @RulesCollection(RulesCollection.Rules.COMMON)
 public class TemporaryStatModifierFactory implements INamedObjectFactory<TemporaryStatModifier> {
+
+	private Game game;
+
 	@Override
 	public TemporaryStatModifier forName(String name) {
+		StatsMechanic mechanic = (StatsMechanic) game.getFactory(FactoryType.Factory.MECHANIC).forName(Mechanic.Type.STAT.name());
 		String[] parts = name.split(TemporaryStatModifier.NAME_SEPARATOR);
 		try {
-			Constructor<?> constructor = Class.forName(parts[1]).getConstructor(TemporaryStatModifier.PlayerStatKey.class);
-			return (TemporaryStatModifier) constructor.newInstance(TemporaryStatModifier.PlayerStatKey.valueOf(parts[0]));
+			Constructor<?> constructor = Class.forName(parts[1]).getConstructor(TemporaryStatModifier.PlayerStatKey.class, StatsMechanic.class);
+			return (TemporaryStatModifier) constructor.newInstance(TemporaryStatModifier.PlayerStatKey.valueOf(parts[0]), mechanic);
 		} catch (ClassNotFoundException | NoSuchMethodException | IllegalAccessException | InstantiationException | InvocationTargetException e) {
 			throw new FantasyFootballException("Could not create temporary stat modifier for '" + name + "' due to: ", e);
 		}
@@ -25,6 +31,6 @@ public class TemporaryStatModifierFactory implements INamedObjectFactory<Tempora
 
 	@Override
 	public void initialize(Game game) {
-		// NOOP
+		this.game = game;
 	}
 }
