c297e52a70c81563bf6a25b744cefc1ba57571da
Christian Huber <huberc@butterbrot.org>
2021-12-08 01:00:39 +0100

    Render basic change list

--- FantasyFootballClient/src/com/fumbbl/ffb/client/dialog/DialogChangeList.java
+++ FantasyFootballClient/src/com/fumbbl/ffb/client/dialog/DialogChangeList.java
@@ -1,6 +1,8 @@
 package com.fumbbl.ffb.client.dialog;
 
 import com.fumbbl.ffb.client.FantasyFootballClient;
+import com.fumbbl.ffb.client.model.ChangeList;
+import com.fumbbl.ffb.client.model.VersionChangeList;
 import com.fumbbl.ffb.dialog.DialogId;
 import com.fumbbl.ffb.model.Game;
 
@@ -10,6 +12,8 @@ import javax.swing.JPanel;
 import javax.swing.JScrollPane;
 import javax.swing.event.InternalFrameEvent;
 import java.awt.Dimension;
+import java.util.List;
+import java.util.stream.Collectors;
 
 public class DialogChangeList extends Dialog {
 
@@ -55,11 +59,47 @@ public class DialogChangeList extends Dialog {
 		aboutPane.setEditable(false);
 		aboutPane.setContentType("text/html");
 
-		aboutPane.setText("");
-		aboutPane.setCaretPosition(0);
+		String text = new ChangeList().getVersions().stream()
+			.filter(VersionChangeList::hasEntries)
+			.map(this::renderVersionChanges)
+			.collect(Collectors.joining());
 
-		return aboutPane;
+		contentPane.setText(text);
+		contentPane.setCaretPosition(0);
 
+		return contentPane;
+
+	}
+
+	private String renderVersionChanges(VersionChangeList list) {
+		StringBuilder builder = new StringBuilder();
+
+		builder.append("<h1>").append(list.getVersion()).append("</h1>");
+
+		if (list.hasFeatures()) {
+			builder.append(renderEntries("Features", list.getFeatures()));
+		}
+
+		if (list.hasImprovements()) {
+			builder.append(renderEntries("Improvements", list.getImprovements()));
+		}
+
+		if (list.hasBugfixes()) {
+			builder.append(renderEntries("Bugfixes", list.getBugfixes()));
+		}
+
+		return builder.toString();
+	}
+
+	private String renderEntries(String title, List<String> entries) {
+		StringBuilder builder = new StringBuilder();
+
+		builder.append("<h2>").append(title).append("</h2>").append("<ul>");
+
+		entries.stream().map(entry -> "<li>" + entry + "</li>").forEach(builder::append);
+
+		builder.append("</ul>");
+		return builder.toString();
 	}
 
 	protected void setLocationToCenter() {
