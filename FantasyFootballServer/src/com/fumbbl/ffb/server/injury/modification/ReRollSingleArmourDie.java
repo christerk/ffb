b47bec7cad435151935d5c9a216c6ea22e39736a
Christian Huber <huberc@butterbrot.org>
2022-01-10 23:46:04 +0100

    Allow old pro to be used on spotted fouls even when armour was broken

--- FantasyFootballServer/src/com/fumbbl/ffb/server/injury/modification/ReRollSingleArmourDie.java
+++ FantasyFootballServer/src/com/fumbbl/ffb/server/injury/modification/ReRollSingleArmourDie.java
@@ -6,7 +6,9 @@ import com.fumbbl.ffb.injury.Chainsaw;
 import com.fumbbl.ffb.injury.Foul;
 import com.fumbbl.ffb.injury.FoulForSpp;
 import com.fumbbl.ffb.injury.InjuryType;
+import com.fumbbl.ffb.injury.context.InjuryContext;
 import com.fumbbl.ffb.injury.context.InjuryContextForModification;
+import com.fumbbl.ffb.report.bb2020.ReportOldPro;
 import com.fumbbl.ffb.server.DiceInterpreter;
 import com.fumbbl.ffb.server.GameState;
 
@@ -24,15 +26,22 @@ public class ReRollSingleArmourDie extends InjuryContextModification {
 	}
 
 	@Override
-	protected boolean modifyArmourInternal(InjuryContextForModification newContext, GameState gameState) {
+	protected boolean tryArmourRollModification(InjuryContext injuryContext, InjuryType injuryType) {
+		return super.tryArmourRollModification(injuryContext, injuryType)
+			|| (injuryContext.fArmorRoll[0] == injuryContext.fArmorRoll[1] && injuryType.isFoul());
+	}
+
+	@Override
+	protected boolean modifyArmourInternal(GameState gameState, InjuryContextForModification newContext, InjuryType injuryType) {
 		DiceInterpreter diceInterpreter = DiceInterpreter.getInstance();
 
+		boolean spottedFoul = (newContext.fArmorRoll[0] == newContext.fArmorRoll[1] && injuryType.isFoul());
 		int replaceIndex = newContext.fArmorRoll[0] < newContext.fArmorRoll[1] ? 0 : 1;
 		int oldValue = newContext.fArmorRoll[replaceIndex];
 		newContext.fArmorRoll[replaceIndex] = 6;
 		newContext.setArmorBroken(diceInterpreter.isArmourBroken(gameState, newContext));
 
-		if (!newContext.isArmorBroken()) {
+		if (!newContext.isArmorBroken() && !spottedFoul) {
 			return false;
 		}
 
