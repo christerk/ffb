70de105d13fed1f334f6408c7a2949b089beeabc
Christian Huber <christian.huber@br.de>
2022-01-12 10:03:13 +0100

    Bump version to 2.3.1

--- FantasyFootballCommon/src/com/fumbbl/ffb/FantasyFootballConstants.java
+++ FantasyFootballCommon/src/com/fumbbl/ffb/FantasyFootballConstants.java
@@ -1,6 +1,6 @@
 package com.fumbbl.ffb;
 
 public class FantasyFootballConstants {
-	public static final String CLIENT_VERSION = "2.3.0";
-	public static final String SERVER_VERSION = "2.3.0";
+	public static final String CLIENT_VERSION = "2.3.1";
+	public static final String SERVER_VERSION = "2.3.1";
 }
