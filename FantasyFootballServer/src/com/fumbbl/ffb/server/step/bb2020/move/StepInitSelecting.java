9373f50f86938d2a077faa43ae901fc90e0db0b5
Christian Huber <huberc@butterbrot.org>
2022-02-18 22:33:26 +0100

    Use proper null check as actingPlayer object is never set to null

--- FantasyFootballServer/src/com/fumbbl/ffb/server/step/bb2020/move/StepInitSelecting.java
+++ FantasyFootballServer/src/com/fumbbl/ffb/server/step/bb2020/move/StepInitSelecting.java
@@ -151,7 +151,7 @@ public final class StepInitSelecting extends AbstractStep {
 						}
 						commandStatus = StepCommandStatus.EXECUTE_STEP;
 
-					} else if (!StringTool.isProvided(actingPlayerCommand.getPlayerId()) && actingPlayer != null) {
+					} else if (!StringTool.isProvided(actingPlayerCommand.getPlayerId()) && actingPlayer.getPlayerId() != null) {
 						boolean unusedBlitz = actingPlayer.getPlayerAction().isBlitzing() && !actingPlayer.hasBlocked();
 						boolean unusedGaze = actingPlayer.getPlayerAction().isGaze() && UtilCards.hasUnusedSkillWithProperty(actingPlayer, NamedProperties.inflictsConfusion);
 						boolean onlyMarkedAsStandingUp = actingPlayer.isStandingUp() && actingPlayer.getCurrentMove() == Constant.MINIMUM_MOVE_TO_STAND_UP;
