8c8c9d189709a251a0f3d3d1cb0c87e4e602bbc4
Christian Huber <huberc@butterbrot.org>
2022-03-01 18:21:03 +0100

    Mark single use actions when treacherous is used

--- FantasyFootballServer/src/com/fumbbl/ffb/server/step/bb2020/StepTreacherous.java
+++ FantasyFootballServer/src/com/fumbbl/ffb/server/step/bb2020/StepTreacherous.java
@@ -65,6 +65,35 @@ public class StepTreacherous extends AbstractStep {
 			});
 
 			actingPlayer.markSkillUsed(skill);
+
+			switch (actingPlayer.getPlayerAction()) {
+				case BLITZ:
+				case BLITZ_MOVE:
+					game.getTurnData().setBlitzUsed(true);
+					break;
+				case KICK_TEAM_MATE:
+				case KICK_TEAM_MATE_MOVE:
+					game.getTurnData().setKtmUsed(true);
+					break;
+				case PASS:
+				case PASS_MOVE:
+				case THROW_TEAM_MATE:
+				case THROW_TEAM_MATE_MOVE:
+					game.getTurnData().setPassUsed(true);
+					break;
+				case HAND_OVER:
+				case HAND_OVER_MOVE:
+					game.getTurnData().setHandOverUsed(true);
+					break;
+				case FOUL:
+				case FOUL_MOVE:
+					if (!actingPlayer.getPlayer().hasSkillProperty(NamedProperties.allowsAdditionalFoul)) {
+						game.getTurnData().setFoulUsed(true);
+					}
+					break;
+				default:
+					break;
+			}
 		}
 
 		getResult().setNextAction(StepAction.NEXT_STEP);
