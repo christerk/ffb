55bb73534f4148a3304dc647b7485698f94e6e31
Christian Huber <huberc@butterbrot.org>
2022-03-04 20:21:44 +0100

    Readd skills deleted on master for release

---
+++ FantasyFootballCommon/src/com/fumbbl/ffb/skill/bb2020/special/StrongPassingGame.java
@@ -0,0 +1,18 @@
+package com.fumbbl.ffb.skill.bb2020.special;
+
+import com.fumbbl.ffb.RulesCollection;
+import com.fumbbl.ffb.RulesCollection.Rules;
+import com.fumbbl.ffb.SkillCategory;
+import com.fumbbl.ffb.model.skill.Skill;
+import com.fumbbl.ffb.model.skill.SkillUsageType;
+
+/**
+ * Once per game, after making a Passing Ability test to perform a Pass action, Skrull may choose to modify the dice roll by adding his Strength characteristic to it
+ */
+
+@RulesCollection(Rules.BB2020)
+public class StrongPassingGame extends Skill {
+	public StrongPassingGame() {
+		super("Strong Passing Game", SkillCategory.TRAIT, SkillUsageType.ONCE_PER_GAME);
+	}
+}
