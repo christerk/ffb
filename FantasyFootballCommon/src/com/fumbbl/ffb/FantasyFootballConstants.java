a80607a0e2adf50522020aa2f518edefbc8d5767
Christian Huber <huberc@butterbrot.org>
2022-05-18 22:44:14 +0200

    Set proper version value 2.9.0

--- FantasyFootballCommon/src/com/fumbbl/ffb/FantasyFootballConstants.java
+++ FantasyFootballCommon/src/com/fumbbl/ffb/FantasyFootballConstants.java
@@ -1,6 +1,6 @@
 package com.fumbbl.ffb;
 
 public class FantasyFootballConstants {
-	public static final String CLIENT_VERSION = "2.8.1";
-	public static final String SERVER_VERSION = "2.8.1";
+	public static final String CLIENT_VERSION = "2.9.0";
+	public static final String SERVER_VERSION = "2.9.0";
 }
