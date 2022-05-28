294a5470789bd560d1545bf3e908bd7bb2d2659b
Christian Huber <huberc@butterbrot.org>
2022-05-29 00:05:00 +0200

    Add Maximum Carnage Skill

--- FantasyFootballCommon/src/com/fumbbl/ffb/model/property/NamedProperties.java
+++ FantasyFootballCommon/src/com/fumbbl/ffb/model/property/NamedProperties.java
@@ -77,6 +77,7 @@ public class NamedProperties {
 			"Can Perform Armour Roll Instead Of Block");
 	public static final ISkillProperty canPerformArmourRollInsteadOfBlockThatMightFail = new NamedProperty(
 		"Can Perform Armour Roll Instead Of Block That Might Fail");
+	public static final ISkillProperty canPerformaSecondChainsawAttack = new NamedProperty("Can Perform Second Chainsaw Attack");
 	public static final ISkillProperty canPlaceBallWhenKnockedDownOrPlacedProne = new NamedProperty("Can Place Ball When Knocked Down Or Placed Prone");
 	public static final ISkillProperty canPushBackToAnySquare = new NamedProperty("Can Push Back To Any Square");
 	public static final ISkillProperty canPileOnOpponent = new NamedProperty("Can Pile On Opponent");
---
+++ FantasyFootballCommon/src/com/fumbbl/ffb/skill/bb2020/special/MaximumCarnage.java
@@ -0,0 +1,20 @@
+package com.fumbbl.ffb.skill.bb2020.special;
+
+import com.fumbbl.ffb.RulesCollection;
+import com.fumbbl.ffb.SkillCategory;
+import com.fumbbl.ffb.model.property.NamedProperties;
+import com.fumbbl.ffb.model.skill.Skill;
+import com.fumbbl.ffb.model.skill.SkillUsageType;
+
+@RulesCollection(RulesCollection.Rules.BB2020)
+public class MaximumCarnage extends Skill {
+
+	public MaximumCarnage() {
+		super("Maximum Carnage", SkillCategory.EXTRAORDINARY, SkillUsageType.ONCE_PER_GAME);
+	}
+
+	@Override
+	public void postConstruct() {
+		registerProperty(NamedProperties.canPerformaSecondChainsawAttack);
+	}
+}
