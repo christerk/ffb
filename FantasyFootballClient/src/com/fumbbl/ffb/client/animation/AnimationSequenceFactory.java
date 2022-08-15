63bde0990c96712960246b5bbe7d80e7801d24ad
Christian Huber <huberc@butterbrot.org>
2022-08-15 19:34:31 +0200

    Scale kick off animations

--- FantasyFootballClient/src/com/fumbbl/ffb/client/animation/AnimationSequenceFactory.java
+++ FantasyFootballClient/src/com/fumbbl/ffb/client/animation/AnimationSequenceFactory.java
@@ -26,45 +26,46 @@ public class AnimationSequenceFactory {
 		if ((pAnimation == null) || (pAnimation.getAnimationType() == null)) {
 			return null;
 		}
-		Dimension fieldDimension = pClient.getUserInterface().getDimensionProvider().dimension(DimensionProvider.Component.FIELD);
+		DimensionProvider dimensionProvider = pClient.getUserInterface().getDimensionProvider();
+		Dimension fieldDimension = dimensionProvider.dimension(DimensionProvider.Component.FIELD);
 
 		switch (pAnimation.getAnimationType()) {
 			case KICKOFF_BLITZ:
-				return AnimationSequenceKickoff.createAnimationSequence(fieldDimension, IIconProperty.ANIMATION_KICKOFF_BLITZ);
+				return AnimationSequenceKickoff.createAnimationSequence(fieldDimension, IIconProperty.ANIMATION_KICKOFF_BLITZ, dimensionProvider.isPortrait());
 			case KICKOFF_BLIZZARD:
-				return AnimationSequenceKickoff.createAnimationSequence(fieldDimension, IIconProperty.ANIMATION_KICKOFF_BLIZZARD);
+				return AnimationSequenceKickoff.createAnimationSequence(fieldDimension, IIconProperty.ANIMATION_KICKOFF_BLIZZARD, dimensionProvider.isPortrait());
 			case KICKOFF_BRILLIANT_COACHING:
-				return AnimationSequenceKickoff.createAnimationSequence(fieldDimension, IIconProperty.ANIMATION_KICKOFF_BRILLIANT_COACHING);
+				return AnimationSequenceKickoff.createAnimationSequence(fieldDimension, IIconProperty.ANIMATION_KICKOFF_BRILLIANT_COACHING, dimensionProvider.isPortrait());
 			case KICKOFF_CHEERING_FANS:
-				return AnimationSequenceKickoff.createAnimationSequence(fieldDimension, IIconProperty.ANIMATION_KICKOFF_CHEERING_FANS);
+				return AnimationSequenceKickoff.createAnimationSequence(fieldDimension, IIconProperty.ANIMATION_KICKOFF_CHEERING_FANS, dimensionProvider.isPortrait());
 			case KICKOFF_GET_THE_REF:
-				return AnimationSequenceKickoff.createAnimationSequence(fieldDimension, IIconProperty.ANIMATION_KICKOFF_GET_THE_REF);
+				return AnimationSequenceKickoff.createAnimationSequence(fieldDimension, IIconProperty.ANIMATION_KICKOFF_GET_THE_REF, dimensionProvider.isPortrait());
 			case KICKOFF_HIGH_KICK:
-				return AnimationSequenceKickoff.createAnimationSequence(fieldDimension, IIconProperty.ANIMATION_KICKOFF_HIGH_KICK);
+				return AnimationSequenceKickoff.createAnimationSequence(fieldDimension, IIconProperty.ANIMATION_KICKOFF_HIGH_KICK, dimensionProvider.isPortrait());
 			case KICKOFF_NICE:
-				return AnimationSequenceKickoff.createAnimationSequence(fieldDimension, IIconProperty.ANIMATION_KICKOFF_NICE);
+				return AnimationSequenceKickoff.createAnimationSequence(fieldDimension, IIconProperty.ANIMATION_KICKOFF_NICE, dimensionProvider.isPortrait());
 			case KICKOFF_PERFECT_DEFENSE:
-				return AnimationSequenceKickoff.createAnimationSequence(fieldDimension, IIconProperty.ANIMATION_KICKOFF_PERFECT_DEFENSE);
+				return AnimationSequenceKickoff.createAnimationSequence(fieldDimension, IIconProperty.ANIMATION_KICKOFF_PERFECT_DEFENSE, dimensionProvider.isPortrait());
 			case KICKOFF_SOLID_DEFENSE:
-				return AnimationSequenceKickoff.createAnimationSequence(fieldDimension, IIconProperty.ANIMATION_KICKOFF_SOLID_DEFENCE);
+				return AnimationSequenceKickoff.createAnimationSequence(fieldDimension, IIconProperty.ANIMATION_KICKOFF_SOLID_DEFENCE, dimensionProvider.isPortrait());
 			case KICKOFF_PITCH_INVASION:
-				return AnimationSequenceKickoff.createAnimationSequence(fieldDimension, IIconProperty.ANIMATION_KICKOFF_PITCH_INVASION);
+				return AnimationSequenceKickoff.createAnimationSequence(fieldDimension, IIconProperty.ANIMATION_KICKOFF_PITCH_INVASION, dimensionProvider.isPortrait());
 			case KICKOFF_POURING_RAIN:
-				return AnimationSequenceKickoff.createAnimationSequence(fieldDimension, IIconProperty.ANIMATION_KICKOFF_POURING_RAIN);
+				return AnimationSequenceKickoff.createAnimationSequence(fieldDimension, IIconProperty.ANIMATION_KICKOFF_POURING_RAIN, dimensionProvider.isPortrait());
 			case KICKOFF_QUICK_SNAP:
-				return AnimationSequenceKickoff.createAnimationSequence(fieldDimension, IIconProperty.ANIMATION_KICKOFF_QUICK_SNAP);
+				return AnimationSequenceKickoff.createAnimationSequence(fieldDimension, IIconProperty.ANIMATION_KICKOFF_QUICK_SNAP, dimensionProvider.isPortrait());
 			case KICKOFF_RIOT:
-				return AnimationSequenceKickoff.createAnimationSequence(fieldDimension, IIconProperty.ANIMATION_KICKOFF_RIOT);
+				return AnimationSequenceKickoff.createAnimationSequence(fieldDimension, IIconProperty.ANIMATION_KICKOFF_RIOT, dimensionProvider.isPortrait());
 			case KICKOFF_TIMEOUT:
-				return AnimationSequenceKickoff.createAnimationSequence(fieldDimension, IIconProperty.ANIMATION_KICKOFF_TIMEOUT);
+				return AnimationSequenceKickoff.createAnimationSequence(fieldDimension, IIconProperty.ANIMATION_KICKOFF_TIMEOUT, dimensionProvider.isPortrait());
 			case KICKOFF_SWELTERING_HEAT:
-				return AnimationSequenceKickoff.createAnimationSequence(fieldDimension, IIconProperty.ANIMATION_KICKOFF_SWELTERING_HEAT);
+				return AnimationSequenceKickoff.createAnimationSequence(fieldDimension, IIconProperty.ANIMATION_KICKOFF_SWELTERING_HEAT, dimensionProvider.isPortrait());
 			case KICKOFF_THROW_A_ROCK:
-				return AnimationSequenceKickoff.createAnimationSequence(fieldDimension, IIconProperty.ANIMATION_KICKOFF_THROW_A_ROCK);
+				return AnimationSequenceKickoff.createAnimationSequence(fieldDimension, IIconProperty.ANIMATION_KICKOFF_THROW_A_ROCK, dimensionProvider.isPortrait());
 			case KICKOFF_OFFICIOUS_REF:
-				return AnimationSequenceKickoff.createAnimationSequence(fieldDimension, IIconProperty.ANIMATION_KICKOFF_OFFICIOUS_REF);
+				return AnimationSequenceKickoff.createAnimationSequence(fieldDimension, IIconProperty.ANIMATION_KICKOFF_OFFICIOUS_REF, dimensionProvider.isPortrait());
 			case KICKOFF_VERY_SUNNY:
-				return AnimationSequenceKickoff.createAnimationSequence(fieldDimension, IIconProperty.ANIMATION_KICKOFF_VERY_SUNNY);
+				return AnimationSequenceKickoff.createAnimationSequence(fieldDimension, IIconProperty.ANIMATION_KICKOFF_VERY_SUNNY, dimensionProvider.isPortrait());
 			case SPELL_FIREBALL:
 				return AnimationSequenceSpecialEffect.createAnimationSequenceFireball(pAnimation.getStartCoordinate());
 			case SPELL_LIGHTNING:
--- FantasyFootballClient/src/com/fumbbl/ffb/client/animation/AnimationSequenceKickoff.java
+++ FantasyFootballClient/src/com/fumbbl/ffb/client/animation/AnimationSequenceKickoff.java
@@ -34,14 +34,22 @@ public class AnimationSequenceKickoff implements IAnimationSequence, ActionListe
 	private int fDelay;
 	private IAnimationListener fListener;
 
-	public static AnimationSequenceKickoff createAnimationSequence(Dimension containerDimension, String pIconProperty) {
-		return new AnimationSequenceKickoff(containerDimension, new AnimationFrame[]{new AnimationFrame(pIconProperty, 0.5f, 0.3, 100),
-			new AnimationFrame(pIconProperty, 0.6f, 0.4, 100), new AnimationFrame(pIconProperty, 0.7f, 0.5, 100),
-			new AnimationFrame(pIconProperty, 0.8f, 0.6, 100), new AnimationFrame(pIconProperty, 0.9f, 0.7, 100),
-			new AnimationFrame(pIconProperty, 1.0f, 0.8, 100), new AnimationFrame(pIconProperty, 1.0f, 0.9, 100),
-			new AnimationFrame(pIconProperty, 1.0f, 1.0, 2000), new AnimationFrame(pIconProperty, 0.8f, 1.0, 100),
-			new AnimationFrame(pIconProperty, 0.6f, 1.0, 100), new AnimationFrame(pIconProperty, 0.4f, 1.0, 100),
-			new AnimationFrame(pIconProperty, 0.2f, 1.0, 100)});
+	public static AnimationSequenceKickoff createAnimationSequence(Dimension containerDimension, String pIconProperty, boolean portrait) {
+		double[] scaleSteps;
+
+		if (portrait) {
+			scaleSteps = new double[]{0.3, 0.35, 0.4, 0.45, 0.5, 0.55, 0.6, 0.65};
+		} else {
+			scaleSteps = new double[]{0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1.0};
+		}
+
+		return new AnimationSequenceKickoff(containerDimension, new AnimationFrame[]{new AnimationFrame(pIconProperty, 0.5f, scaleSteps[0], 100),
+			new AnimationFrame(pIconProperty, 0.6f, scaleSteps[1], 100), new AnimationFrame(pIconProperty, 0.7f, scaleSteps[2], 100),
+			new AnimationFrame(pIconProperty, 0.8f, scaleSteps[3], 100), new AnimationFrame(pIconProperty, 0.9f, scaleSteps[4], 100),
+			new AnimationFrame(pIconProperty, 1.0f, scaleSteps[5], 100), new AnimationFrame(pIconProperty, 1.0f, scaleSteps[6], 100),
+			new AnimationFrame(pIconProperty, 1.0f, scaleSteps[7], 2000), new AnimationFrame(pIconProperty, 0.8f, scaleSteps[7], 100),
+			new AnimationFrame(pIconProperty, 0.6f, scaleSteps[7], 100), new AnimationFrame(pIconProperty, 0.4f, scaleSteps[7], 100),
+			new AnimationFrame(pIconProperty, 0.2f, scaleSteps[7], 100)});
 	}
 
 	public void play(FieldLayer pFieldLayer, IAnimationListener pListener) {
@@ -68,7 +76,7 @@ public class AnimationSequenceKickoff implements IAnimationSequence, ActionListe
 				SoundEngine soundEngine = fFieldLayer.getClient().getUserInterface().getSoundEngine();
 				String soundSetting = fFieldLayer.getClient().getProperty(IClientProperty.SETTING_SOUND_MODE);
 				if (IClientPropertyValue.SETTING_SOUND_ON.equals(soundSetting)
-						|| (IClientPropertyValue.SETTING_SOUND_MUTE_SPECTATORS.equals(soundSetting))) {
+					|| (IClientPropertyValue.SETTING_SOUND_MUTE_SPECTATORS.equals(soundSetting))) {
 					soundEngine.playSound(fFrames[fPosition].getSound());
 				}
 			}
