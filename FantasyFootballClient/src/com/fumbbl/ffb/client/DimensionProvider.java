c16fac674f40b0ef4cd2aa4e17f69661841af310
Christian Huber <huberc@butterbrot.org>
2022-11-27 14:58:55 +0100

    Define dimensions for square layout

--- FantasyFootballClient/src/com/fumbbl/ffb/client/DimensionProvider.java
+++ FantasyFootballClient/src/com/fumbbl/ffb/client/DimensionProvider.java
@@ -14,73 +14,99 @@ public class DimensionProvider {
 
 	private static final int SIDEBAR_WIDTH_L = 145;
 	private static final int SIDEBAR_WIDTH_P = 165;
-	private final Map<Component, Dimension> portraitDimensions = new HashMap<>();
-	private final Map<Component, Dimension> landscapeDimensions = new HashMap<>();
+
+	private final Map<ClientLayout, Map<Component, Dimension>> dimensions = new HashMap<>();
 	private final int fieldSquareSize = 30;
 
 	public DimensionProvider(ClientLayout layout) {
 		this.layout = layout;
+		Map<Component, Dimension> portraitDimensions = new HashMap<>();
+		Map<Component, Dimension> squareDimensions = new HashMap<>();
+		Map<Component, Dimension> landscapeDimensions = new HashMap<>();
+
+		dimensions.put(ClientLayout.LANDSCAPE, landscapeDimensions);
+		dimensions.put(ClientLayout.PORTRAIT, portraitDimensions);
+		dimensions.put(ClientLayout.SQUARE, squareDimensions);
 
 		portraitDimensions.put(Component.FIELD, new Dimension(452, 782));
+		squareDimensions.put(Component.FIELD, new Dimension(452, 782));
 		landscapeDimensions.put(Component.FIELD, new Dimension(782, 452));
 
 		portraitDimensions.put(Component.CHAT, new Dimension(389, 153));
+		squareDimensions.put(Component.CHAT, new Dimension(260, 226));
 		landscapeDimensions.put(Component.CHAT, new Dimension(389, 226));
 
 		portraitDimensions.put(Component.LOG, new Dimension(389, 153));
+		squareDimensions.put(Component.LOG, new Dimension(260, 226));
 		landscapeDimensions.put(Component.LOG, new Dimension(389, 226));
 
 		portraitDimensions.put(Component.REPLAY_CONTROL, new Dimension(389, 26));
+		squareDimensions.put(Component.REPLAY_CONTROL, new Dimension(260, 26));
 		landscapeDimensions.put(Component.REPLAY_CONTROL, new Dimension(389, 26));
 
 		portraitDimensions.put(Component.TURN_DICE_STATUS, new Dimension(SIDEBAR_WIDTH_P, 101));
+		squareDimensions.put(Component.TURN_DICE_STATUS, new Dimension(SIDEBAR_WIDTH_P, 101));
 		landscapeDimensions.put(Component.TURN_DICE_STATUS, new Dimension(SIDEBAR_WIDTH_L, 92));
 
 		portraitDimensions.put(Component.RESOURCE, new Dimension(SIDEBAR_WIDTH_P, 185));
+		squareDimensions.put(Component.RESOURCE, new Dimension(SIDEBAR_WIDTH_P, 185));
 		landscapeDimensions.put(Component.RESOURCE, new Dimension(SIDEBAR_WIDTH_L, 168));
 
 		portraitDimensions.put(Component.BUTTON_BOX, new Dimension(SIDEBAR_WIDTH_P, 24));
+		squareDimensions.put(Component.BUTTON_BOX, new Dimension(SIDEBAR_WIDTH_P, 24));
 		landscapeDimensions.put(Component.BUTTON_BOX, new Dimension(SIDEBAR_WIDTH_L, 22));
 
 		portraitDimensions.put(Component.BOX, new Dimension(SIDEBAR_WIDTH_P, 472));
+		squareDimensions.put(Component.BOX, new Dimension(SIDEBAR_WIDTH_P, 472));
 		landscapeDimensions.put(Component.BOX, new Dimension(SIDEBAR_WIDTH_L, 430));
 
 		portraitDimensions.put(Component.PLAYER_DETAIL, new Dimension(SIDEBAR_WIDTH_P, 472));
+		squareDimensions.put(Component.PLAYER_DETAIL, new Dimension(SIDEBAR_WIDTH_P, 472));
 		landscapeDimensions.put(Component.PLAYER_DETAIL, new Dimension(SIDEBAR_WIDTH_L, 430));
 
 		portraitDimensions.put(Component.SIDEBAR, new Dimension(SIDEBAR_WIDTH_P, sidebarHeight(portraitDimensions)));
+		squareDimensions.put(Component.SIDEBAR, new Dimension(SIDEBAR_WIDTH_P, sidebarHeight(portraitDimensions)));
 		landscapeDimensions.put(Component.SIDEBAR, new Dimension(SIDEBAR_WIDTH_L, sidebarHeight(landscapeDimensions)));
 
 		portraitDimensions.put(Component.PLAYER_PORTRAIT, new Dimension(133, 162));
+		squareDimensions.put(Component.PLAYER_PORTRAIT, new Dimension(133, 162));
 		landscapeDimensions.put(Component.PLAYER_PORTRAIT, new Dimension(121, 147));
 
 		portraitDimensions.put(Component.PLAYER_PORTRAIT_OFFSET, new Dimension(13, 32));
+		squareDimensions.put(Component.PLAYER_PORTRAIT_OFFSET, new Dimension(13, 32));
 		landscapeDimensions.put(Component.PLAYER_PORTRAIT_OFFSET, new Dimension(3, 32));
 
 		portraitDimensions.put(Component.PLAYER_STAT_OFFSET, new Dimension(4, 198));
+		squareDimensions.put(Component.PLAYER_STAT_OFFSET, new Dimension(4, 198));
 		landscapeDimensions.put(Component.PLAYER_STAT_OFFSET, new Dimension(3, 179));
 
 		portraitDimensions.put(Component.PLAYER_STAT_BOX, new Dimension(31, 30));
+		squareDimensions.put(Component.PLAYER_STAT_BOX, new Dimension(31, 30));
 		landscapeDimensions.put(Component.PLAYER_STAT_BOX, new Dimension(28, 29));
 
 		portraitDimensions.put(Component.PLAYER_STAT_BOX_MISC, new Dimension(0, 15));
+		squareDimensions.put(Component.PLAYER_STAT_BOX_MISC, new Dimension(0, 15));
 		landscapeDimensions.put(Component.PLAYER_STAT_BOX_MISC, new Dimension(0, 14));
 
 		portraitDimensions.put(Component.PLAYER_SPP_OFFSET, new Dimension(10, 245));
+		squareDimensions.put(Component.PLAYER_SPP_OFFSET, new Dimension(10, 245));
 		landscapeDimensions.put(Component.PLAYER_SPP_OFFSET, new Dimension(8, 222));
 
 		portraitDimensions.put(Component.PLAYER_SKILL_OFFSET, new Dimension(10, 270));
+		squareDimensions.put(Component.PLAYER_SKILL_OFFSET, new Dimension(10, 270));
 		landscapeDimensions.put(Component.PLAYER_SKILL_OFFSET, new Dimension(8, 246));
 
 		portraitDimensions.put(Component.BOX_BUTTON, new Dimension(82, 22));
+		squareDimensions.put(Component.BOX_BUTTON, new Dimension(82, 22));
 		landscapeDimensions.put(Component.BOX_BUTTON, new Dimension(72, 22));
 
 		portraitDimensions.put(Component.END_TURN_BUTTON, new Dimension(163, 34));
+		squareDimensions.put(Component.END_TURN_BUTTON, new Dimension(163, 34));
 		landscapeDimensions.put(Component.END_TURN_BUTTON, new Dimension(143, 31));
 	}
 
 	public Dimension dimension(Component component) {
-		return (layout != ClientLayout.LANDSCAPE ? portraitDimensions : landscapeDimensions).get(component);
+		return dimensions.get(layout).get(component);
 	}
 
 	public boolean isPortrait() {
