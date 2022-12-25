051e3ea940349398b9b39d3447773f91e34dc08b
Christian Huber <huberc@butterbrot.org>
2022-12-25 23:41:31 +0100

    Create skeleton for generator and test

---
+++ FantasyFootballCommon/src/com/fumbbl/ffb/marking/MarkerGenerator.java
@@ -0,0 +1,10 @@
+package com.fumbbl.ffb.marking;
+
+import com.fumbbl.ffb.model.Player;
+
+public class MarkerGenerator {
+
+	public PlayerMarker generate(Player<?> player) {
+		return null;
+	}
+}
---
+++ FantasyFootballServer/test/com/fumbbl/ffb/marking/MarkerGeneratorTest.java
@@ -0,0 +1,14 @@
+package com.fumbbl.ffb.marking;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MarkerGeneratorTest {
+
+	@Test
+	public void generate() {
+		
+	}
+
+}
