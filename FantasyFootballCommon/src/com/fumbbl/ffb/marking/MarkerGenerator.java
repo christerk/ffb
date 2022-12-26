1de0ca3cd9222f67e44850449bf2417f98d73f8b
Christian Huber <huberc@butterbrot.org>
2022-12-26 22:04:28 +0100

    Implement basic config matching

--- FantasyFootballCommon/src/com/fumbbl/ffb/marking/MarkerGenerator.java
+++ FantasyFootballCommon/src/com/fumbbl/ffb/marking/MarkerGenerator.java
@@ -1,10 +1,22 @@
 package com.fumbbl.ffb.marking;
 
 import com.fumbbl.ffb.model.Player;
+import com.fumbbl.ffb.model.skill.Skill;
+
+import java.util.Arrays;
+import java.util.HashSet;
 
 public class MarkerGenerator {
 
 	public String generate(Player<?> player, AutoMarkingConfig config, boolean playsForMarkingCoach) {
-		return null;
+		StringBuilder marking = new StringBuilder();
+
+		HashSet<Skill> skills = new HashSet<>(Arrays.asList(player.getSkills()));
+		config.getMarkings().stream()
+			.filter(entry -> skills.containsAll(entry.getSkills()))
+			.map(AutoMarkingRecord::getMarking)
+			.forEach(marking::append);
+
+		return marking.toString();
 	}
 }
