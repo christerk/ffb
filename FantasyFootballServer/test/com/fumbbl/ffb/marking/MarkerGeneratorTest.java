8254893c08f36b5ce82b6e8b7cebd64e567acb76
Christian Huber <huberc@butterbrot.org>
2022-12-27 21:11:51 +0100

    Finish skill only test cases

--- FantasyFootballServer/test/com/fumbbl/ffb/marking/MarkerGeneratorTest.java
+++ FantasyFootballServer/test/com/fumbbl/ffb/marking/MarkerGeneratorTest.java
@@ -26,13 +26,21 @@ class MarkerGeneratorTest {
 	private static final String BLODGE_MARKING = "X";
 	private static final String DODGE_MARKING = "D";
 	private static final String BLACKLE_MARKING = "Y";
+	private static final String WRECKLE_MARKING = "Q";
+
 	private static final String TACKLE_MARKING = "T";
 	private static final String WRESTLE_MARKING = "W";
+	private static final String SNEAKY_GIT = "sneaky git";
+	private static final String BLOCK = "block";
+	private static final String DODGE = "dodge";
+	private static final String TACKLE = "tackle";
+	private static final String WRESTLE = "wrestle";
 
 	private final MarkerGenerator generator = new MarkerGenerator();
 	private AutoMarkingConfig config;
 	private AutoMarkingRecord.Builder builder;
 	private SkillFactory skillFactory;
+	private Set<AutoMarkingRecord> markings;
 	@Mock
 	private Player<Position> player;
 	@Mock
@@ -43,219 +51,18 @@ class MarkerGeneratorTest {
 		skillFactory = new MockSkillFactory();
 		config = new AutoMarkingConfig();
 		builder = new AutoMarkingRecord.Builder(skillFactory);
-	}
+		markings = config.getMarkings();
 
-
-	@Test
-	public void generateMarkingsForOverlappingConfigs() {
-		Set<AutoMarkingRecord> markings = config.getMarkings();
-		markings.add(builder.withSkill("block").withSkill("dodge").withMarking(BLODGE_MARKING).build());
-		markings.add(builder.withSkill("block").withSkill("tackle").withMarking(BLACKLE_MARKING).build());
-
-		Skill[] skills = {skillFactory.forName("block"), skillFactory.forName("dodge"), skillFactory.forName("tackle")};
-		given(player.getSkills()).willReturn(skills);
-
-		String marking = generator.generate(player, config, true);
-
-		assertEquals(BLACKLE_MARKING + BLODGE_MARKING, marking);
-	}
-
-	@Test
-	public void ignoreSubsets() {
-		Set<AutoMarkingRecord> markings = config.getMarkings();
-		markings.add(builder.withSkill("block").withSkill("dodge").withMarking(BLODGE_MARKING).build());
-		markings.add(builder.withSkill("dodge").withMarking(DODGE_MARKING).build());
-
-		Skill[] skills = {skillFactory.forName("block"), skillFactory.forName("dodge")};
-		given(player.getSkills()).willReturn(skills);
-
-		String marking = generator.generate(player, config, true);
-
-		assertEquals(BLODGE_MARKING, marking);
-	}
-
-	@Test
-	public void generateForAllMatchingConfigs() {
-		Set<AutoMarkingRecord> markings = config.getMarkings();
-		markings.add(builder.withSkill("block").withMarking(BLOCK_MARKING).build());
-		markings.add(builder.withSkill("dodge").withMarking(DODGE_MARKING).build());
-
-		Skill[] skills = {skillFactory.forName("block"), skillFactory.forName("dodge")};
-		given(player.getSkills()).willReturn(skills);
-
-		String marking = generator.generate(player, config, true);
-
-		assertEquals(BLOCK_MARKING + DODGE_MARKING, marking);
-	}
-
-	@Test
-	public void generateForAllMatchingConfigsWithOpponent() {
-		Set<AutoMarkingRecord> markings = config.getMarkings();
-		markings.add(builder.withSkill("block").withMarking(BLOCK_MARKING).build());
-		markings.add(builder.withSkill("dodge").withMarking(DODGE_MARKING).build());
-
-		Skill[] skills = {skillFactory.forName("block"), skillFactory.forName("dodge")};
-		given(player.getSkills()).willReturn(skills);
-
-		String marking = generator.generate(player, config, false);
-
-		assertEquals(BLOCK_MARKING + DODGE_MARKING, marking);
-	}
-
-	@Test
-	public void generateForAllMatchingConfigsWithMatchingApplyTo() {
-		Set<AutoMarkingRecord> markings = config.getMarkings();
-		markings.add(builder.withSkill("block").withMarking(BLOCK_MARKING).withApplyTo(ApplyTo.OWN).build());
-		markings.add(builder.withSkill("dodge").withMarking(DODGE_MARKING).withApplyTo(ApplyTo.OPPONENT).build());
-
-		Skill[] skills = {skillFactory.forName("block"), skillFactory.forName("dodge")};
-		given(player.getSkills()).willReturn(skills);
-
-		String marking = generator.generate(player, config, true);
-
-		assertEquals(BLOCK_MARKING, marking);
-	}
-
-	@Test
-	public void generateForAllMatchingConfigsWithOpponentAndMatchingApplyTo() {
-		Set<AutoMarkingRecord> markings = config.getMarkings();
-		markings.add(builder.withSkill("block").withMarking(BLOCK_MARKING).withApplyTo(ApplyTo.OWN).build());
-		markings.add(builder.withSkill("dodge").withMarking(DODGE_MARKING).withApplyTo(ApplyTo.OPPONENT).build());
-
-		Skill[] skills = {skillFactory.forName("block"), skillFactory.forName("dodge")};
-		given(player.getSkills()).willReturn(skills);
-
-		String marking = generator.generate(player, config, false);
-
-		assertEquals(DODGE_MARKING, marking);
-	}
-
-	@Test
-	public void generateForAllMatchingConfigsWithGainedOnly() {
-		Set<AutoMarkingRecord> markings = config.getMarkings();
-		markings.add(builder.withSkill("block").withMarking(BLOCK_MARKING).withGainedOnly(true).build());
-		markings.add(builder.withSkill("dodge").withMarking(DODGE_MARKING).withGainedOnly(true).build());
-
-		Skill[] skills = {skillFactory.forName("block"), skillFactory.forName("dodge")};
-		given(player.getSkills()).willReturn(skills);
-
-		String marking = generator.generate(player, config, true);
-
-		assertEquals(BLOCK_MARKING, marking);
-	}
-
-	@Test
-	public void generateForAllMatchingConfigsWithOpponentAndGainedOnly() {
-		Set<AutoMarkingRecord> markings = config.getMarkings();
-		markings.add(builder.withSkill("block").withMarking(BLOCK_MARKING).withGainedOnly(true).build());
-		markings.add(builder.withSkill("dodge").withMarking(DODGE_MARKING).withGainedOnly(true).build());
-
-		Skill[] skills = {skillFactory.forName("block"), skillFactory.forName("dodge")};
-		given(player.getSkills()).willReturn(skills);
-
-		String marking = generator.generate(player, config, false);
-
-		assertEquals(DODGE_MARKING, marking);
-	}
-
-	@Test
-	public void ignoreMatchingConfigsWithGainedOnlyForBaseSkills() {
-		Set<AutoMarkingRecord> markings = config.getMarkings();
-		markings.add(builder.withSkill("block").withMarking(BLOCK_MARKING).withGainedOnly(true).build());
-		markings.add(builder.withSkill("dodge").withMarking(DODGE_MARKING).withGainedOnly(true).build());
-
-		Skill[] skills = {skillFactory.forName("block"), skillFactory.forName("dodge")};
+		Skill[] gainedSkills = {skillFactory.forName(BLOCK), skillFactory.forName(DODGE)};
+		given(player.getSkills()).willReturn(gainedSkills);
+		Skill[] baseSkills = {skillFactory.forName(WRESTLE), skillFactory.forName(TACKLE)};
 		given(player.getPosition()).willReturn(position);
-		given(position.getSkills()).willReturn(skills);
-
-		String marking = generator.generate(player, config, true);
-
-		assertTrue(marking.isEmpty());
-	}
-
-	@Test
-	public void ignoreMatchingConfigsWithOpponentAndGainedOnlyForBaseSkills() {
-		Set<AutoMarkingRecord> markings = config.getMarkings();
-		markings.add(builder.withSkill("block").withMarking(BLOCK_MARKING).withGainedOnly(true).build());
-		markings.add(builder.withSkill("dodge").withMarking(DODGE_MARKING).withGainedOnly(true).build());
-
-		Skill[] skills = {skillFactory.forName("block"), skillFactory.forName("dodge")};
-		given(player.getPosition()).willReturn(position);
-		given(position.getSkills()).willReturn(skills);
-
-		String marking = generator.generate(player, config, false);
-
-		assertTrue(marking.isEmpty());
-	}
-
-
-	@Test
-	public void generateForAllMatchingConfigsWithMatchingGainedAndApplyTo() {
-		Set<AutoMarkingRecord> markings = config.getMarkings();
-		markings.add(builder.withSkill("block").withMarking(BLOCK_MARKING).withApplyTo(ApplyTo.OWN).build());
-		markings.add(builder.withSkill("dodge").withMarking(DODGE_MARKING).withGainedOnly(true).withApplyTo(ApplyTo.OWN).build());
-		markings.add(builder.withSkill("wrestle").withMarking(WRESTLE_MARKING).withApplyTo(ApplyTo.OPPONENT).build());
-		markings.add(builder.withSkill("tackle").withMarking(TACKLE_MARKING).withGainedOnly(true).withApplyTo(ApplyTo.OPPONENT).build());
-
-		Skill[] skills = {skillFactory.forName("block"), skillFactory.forName("dodge"),
-			skillFactory.forName("tackle"), skillFactory.forName("wrestle")};
-		given(player.getSkills()).willReturn(skills);
-
-		String marking = generator.generate(player, config, true);
-
-		assertEquals(BLOCK_MARKING, marking);
-	}
-
-	@Test
-	public void generateForAllMatchingConfigsWithOpponentAndMatchingGainedAndApplyTo() {
-		Set<AutoMarkingRecord> markings = config.getMarkings();
-		markings.add(builder.withSkill("block").withMarking(BLOCK_MARKING).withApplyTo(ApplyTo.OWN).build());
-		markings.add(builder.withSkill("dodge").withMarking(DODGE_MARKING).withGainedOnly(true).withApplyTo(ApplyTo.OWN).build());
-		markings.add(builder.withSkill("wrestle").withMarking(WRESTLE_MARKING).withApplyTo(ApplyTo.OPPONENT).build());
-		markings.add(builder.withSkill("tackle").withMarking(TACKLE_MARKING).withGainedOnly(true).withApplyTo(ApplyTo.OPPONENT).build());
-
-		Skill[] skills = {skillFactory.forName("block"), skillFactory.forName("dodge"),
-			skillFactory.forName("tackle"), skillFactory.forName("wrestle")};
-		given(player.getSkills()).willReturn(skills);
-
-		String marking = generator.generate(player, config, false);
-
-		assertEquals(DODGE_MARKING, marking);
-	}
-
-
-	@Test
-	public void generateOnlyForMatchingConfig() {
-		Set<AutoMarkingRecord> markings = config.getMarkings();
-		markings.add(builder.withSkill("block").withMarking(BLOCK_MARKING).build());
-		markings.add(builder.withSkill("dodge").withMarking(DODGE_MARKING).build());
-
-		Skill[] skills = {skillFactory.forName("block")};
-		given(player.getSkills()).willReturn(skills);
-
-		String marking = generator.generate(player, config, true);
-
-		assertEquals(BLOCK_MARKING, marking);
-	}
-
-	@Test
-	public void generateOnlyDefinedSkill() {
-		config.getMarkings().add(builder.withSkill("block").withMarking(BLOCK_MARKING).build());
-
-		Skill[] skills = {skillFactory.forName("block"), skillFactory.forName("dodge")};
-		given(player.getSkills()).willReturn(skills);
-
-		String marking = generator.generate(player, config, true);
-
-		assertEquals(BLOCK_MARKING, marking);
+		given(position.getSkills()).willReturn(baseSkills);
 	}
 
 	@Test
 	public void generate() {
-		config.getMarkings().add(builder.withSkill("block").withMarking(BLOCK_MARKING).build());
-
-		Skill[] skills = {skillFactory.forName("block")};
-		given(player.getSkills()).willReturn(skills);
+		config.getMarkings().add(builder.withSkill(BLOCK).withMarking(BLOCK_MARKING).build());
 
 		String marking = generator.generate(player, config, true);
 
@@ -264,14 +71,135 @@ class MarkerGeneratorTest {
 
 	@Test
 	public void generateNoMarking() {
-		config.getMarkings().add(builder.withSkill("block").withMarking(BLOCK_MARKING).build());
-
-		Skill[] skills = {skillFactory.forName("dodge")};
-		given(player.getSkills()).willReturn(skills);
+		config.getMarkings().add(builder.withSkill(SNEAKY_GIT).withMarking(BLOCK_MARKING).build());
 
 		String marking = generator.generate(player, config, true);
 
 		assertTrue(marking.isEmpty());
+	}
+
+	@Test
+	public void generateOnlyForPresentSkills() {
+		markings.add(builder.withSkill(BLOCK).withMarking(BLOCK_MARKING).build());
+		markings.add(builder.withSkill(SNEAKY_GIT).withMarking(DODGE_MARKING).build());
+
+		String marking = generator.generate(player, config, true);
+
+		assertEquals(BLOCK_MARKING, marking);
+	}
+
+	@Test
+	public void generateMarkingsForOverlappingConfigs() {
+		markings.add(builder.withSkill(BLOCK).withSkill(DODGE).withMarking(BLODGE_MARKING).build());
+		markings.add(builder.withSkill(BLOCK).withSkill(TACKLE).withMarking(BLACKLE_MARKING).build());
+
+		String marking = generator.generate(player, config, true);
+
+		assertEquals(BLACKLE_MARKING + BLODGE_MARKING, marking);
+	}
+
+	@Test
+	public void ignoreCombinedConfigsForGainedSkillsWithOnlyPartialMatch() {
+		markings.add(builder.withSkill(BLOCK).withSkill(DODGE).withMarking(BLODGE_MARKING).build());
+		markings.add(builder.withSkill(BLOCK).withSkill(TACKLE).withMarking(BLACKLE_MARKING).withGainedOnly(true).build());
+
+		String marking = generator.generate(player, config, true);
+
+		assertEquals(BLODGE_MARKING, marking);
+	}
+
+	@Test
+	public void ignoreSubsets() {
+		markings.add(builder.withSkill(BLOCK).withSkill(DODGE).withMarking(BLODGE_MARKING).build());
+		markings.add(builder.withSkill(DODGE).withMarking(DODGE_MARKING).build());
+
+		String marking = generator.generate(player, config, true);
+
+		assertEquals(BLODGE_MARKING, marking);
+	}
+
+	@Test
+	public void generateForAllMatchingConfigs() {
+		markings.add(builder.withSkill(BLOCK).withMarking(BLOCK_MARKING).build());
+		markings.add(builder.withSkill(DODGE).withMarking(DODGE_MARKING).build());
+
+		String marking = generator.generate(player, config, true);
+
+		assertEquals(BLOCK_MARKING + DODGE_MARKING, marking);
+	}
+
+	@Test
+	public void generateForAllMatchingConfigsWithOpponent() {
+		markings.add(builder.withSkill(BLOCK).withMarking(BLOCK_MARKING).build());
+		markings.add(builder.withSkill(DODGE).withMarking(DODGE_MARKING).build());
+
+		String marking = generator.generate(player, config, false);
+
+		assertEquals(BLOCK_MARKING + DODGE_MARKING, marking);
+	}
+
+	@Test
+	public void generateForAllMatchingConfigsWithMatchingApplyTo() {
+		markings.add(builder.withSkill(BLOCK).withMarking(BLOCK_MARKING).withApplyTo(ApplyTo.OWN).build());
+		markings.add(builder.withSkill(DODGE).withMarking(DODGE_MARKING).withApplyTo(ApplyTo.OPPONENT).build());
+
+		String marking = generator.generate(player, config, true);
+
+		assertEquals(BLOCK_MARKING, marking);
+	}
+
+	@Test
+	public void generateForAllMatchingConfigsWithOpponentAndMatchingApplyTo() {
+		markings.add(builder.withSkill(BLOCK).withMarking(BLOCK_MARKING).withApplyTo(ApplyTo.OWN).build());
+		markings.add(builder.withSkill(DODGE).withMarking(DODGE_MARKING).withApplyTo(ApplyTo.OPPONENT).build());
+
+		String marking = generator.generate(player, config, false);
+
+		assertEquals(DODGE_MARKING, marking);
+	}
+
+	@Test
+	public void generateForAllMatchingConfigsWithGainedOnly() {
+		markings.add(builder.withSkill(WRESTLE).withMarking(WRECKLE_MARKING).withGainedOnly(true).build());
+		markings.add(builder.withSkill(DODGE).withMarking(DODGE_MARKING).withGainedOnly(true).build());
+
+		String marking = generator.generate(player, config, true);
+
+		assertEquals(WRESTLE_MARKING, marking);
+	}
+
+	@Test
+	public void generateForAllMatchingConfigsWithOpponentAndGainedOnly() {
+		markings.add(builder.withSkill(WRESTLE).withMarking(WRECKLE_MARKING).withGainedOnly(true).build());
+		markings.add(builder.withSkill(DODGE).withMarking(DODGE_MARKING).withGainedOnly(true).build());
+
+		String marking = generator.generate(player, config, false);
+
+		assertEquals(WRESTLE_MARKING, marking);
+	}
+
+	@Test
+	public void generateForAllMatchingConfigsWithMatchingGainedAndApplyTo() {
+		markings.add(builder.withSkill(BLOCK).withMarking(BLOCK_MARKING).withApplyTo(ApplyTo.OWN).build());
+		markings.add(builder.withSkill(DODGE).withMarking(DODGE_MARKING).withGainedOnly(true).withApplyTo(ApplyTo.OWN).build());
+		markings.add(builder.withSkill(WRESTLE).withMarking(WRESTLE_MARKING).withApplyTo(ApplyTo.OPPONENT).build());
+		markings.add(builder.withSkill(TACKLE).withMarking(TACKLE_MARKING).withGainedOnly(true).withApplyTo(ApplyTo.OPPONENT).build());
+
+		String marking = generator.generate(player, config, true);
+
+		assertEquals(BLOCK_MARKING, marking);
+	}
+
+	@Test
+	public void generateForAllMatchingConfigsWithOpponentAndMatchingGainedAndApplyTo() {
+		markings.add(builder.withSkill(BLOCK).withMarking(BLOCK_MARKING).withApplyTo(ApplyTo.OWN).build());
+		markings.add(builder.withSkill(DODGE).withMarking(DODGE_MARKING).withGainedOnly(true).withApplyTo(ApplyTo.OWN).build());
+		markings.add(builder.withSkill(WRESTLE).withMarking(WRESTLE_MARKING).withApplyTo(ApplyTo.OPPONENT).build());
+		markings.add(builder.withSkill(TACKLE).withMarking(TACKLE_MARKING).withGainedOnly(true).withApplyTo(ApplyTo.OPPONENT).build());
+
+		String marking = generator.generate(player, config, false);
+
+		assertEquals(DODGE_MARKING, marking);
 	}
 
 	private static class MockSkillFactory extends SkillFactory {
