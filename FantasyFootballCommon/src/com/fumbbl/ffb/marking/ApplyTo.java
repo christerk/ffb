93b69c461921ba6001c2adf59fdab957669c61cf
Christian Huber <huberc@butterbrot.org>
2022-12-30 00:32:18 +0100

    Complete generator implementation, test still failing

--- FantasyFootballCommon/src/com/fumbbl/ffb/marking/ApplyTo.java
+++ FantasyFootballCommon/src/com/fumbbl/ffb/marking/ApplyTo.java
@@ -1,5 +1,21 @@
 package com.fumbbl.ffb.marking;
 
 public enum ApplyTo {
-	OWN, OPPONENT, BOTH
+	OWN(true, false), OPPONENT(false, true), BOTH(true, true);
+
+	private final boolean appliesToOwn;
+	private final boolean appliesToOpponent;
+
+	ApplyTo(boolean appliesToOwn, boolean appliesToOpponent) {
+		this.appliesToOwn = appliesToOwn;
+		this.appliesToOpponent = appliesToOpponent;
+	}
+
+	public boolean isAppliesToOwn() {
+		return appliesToOwn;
+	}
+
+	public boolean isAppliesToOpponent() {
+		return appliesToOpponent;
+	}
 }
--- FantasyFootballCommon/src/com/fumbbl/ffb/marking/MarkerGenerator.java
+++ FantasyFootballCommon/src/com/fumbbl/ffb/marking/MarkerGenerator.java
@@ -1,23 +1,72 @@
 package com.fumbbl.ffb.marking;
 
+import com.fumbbl.ffb.InjuryAttribute;
+import com.fumbbl.ffb.SeriousInjury;
 import com.fumbbl.ffb.model.Player;
 import com.fumbbl.ffb.model.skill.Skill;
 
+import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.HashSet;
+import java.util.Comparator;
+import java.util.List;
+import java.util.stream.Collectors;
 
 public class MarkerGenerator {
 
 	public String generate(Player<?> player, AutoMarkingConfig config, boolean playsForMarkingCoach) {
+
+		List<Skill> baseSkills = new ArrayList<>(Arrays.asList(player.getPosition().getSkills()));
+		List<Skill> gainedSkills = new ArrayList<>(Arrays.asList(player.getSkills()));
+		gainedSkills.removeAll(baseSkills);
+		List<InjuryAttribute> injuries = Arrays.stream(player.getLastingInjuries()).map(SeriousInjury::getInjuryAttribute).collect(Collectors.toList());
+
 		StringBuilder marking = new StringBuilder();
 
-		HashSet<Skill> skills = new HashSet<>(Arrays.asList(player.getSkills()));
 		config.getMarkings().stream()
-			.filter(entry -> skills.containsAll(entry.getSkills()))
-			.map(AutoMarkingRecord::getMarking)
-			.sorted()
-			.forEach(marking::append);
+			.filter(markingRecord -> appliesTo(markingRecord.getApplyTo(), playsForMarkingCoach))
+			.collect(Collectors.groupingBy(markingRecord -> markingRecord.getSkills().isEmpty()))
+			.entrySet().stream()
+			.sorted((entry1, entry2) -> entry1.getKey() == entry2.getKey() ? 0 : entry1.getKey() ? -1 : 1)
+			.flatMap(entry -> entry.getValue().stream())
+			.sorted(Comparator.comparingInt(
+					(AutoMarkingRecord record) ->
+						record.getSkills().size())
+				.thenComparingInt(
+					record -> record.getInjuries().size())
+				.thenComparing(
+					AutoMarkingRecord::getMarking))
+			.map(markingRecord -> getMarking(markingRecord, baseSkills, gainedSkills, injuries)).forEach(marking::append);
 
 		return marking.toString();
+	}
+
+	private boolean appliesTo(ApplyTo applyTo, boolean playsForMarkingCoach) {
+		return playsForMarkingCoach && applyTo.isAppliesToOwn() || !playsForMarkingCoach && applyTo.isAppliesToOpponent();
+	}
+
+	private String getMarking(AutoMarkingRecord markingRecord, List<Skill> baseSkills, List<Skill> gainedSkills, List<InjuryAttribute> injuries) {
+
+		List<Skill> skillsToCheck = new ArrayList<>(gainedSkills);
+
+		if (!markingRecord.isGainedOnly()) {
+			skillsToCheck.addAll(baseSkills);
+		}
+
+		//noinspection SlowListContainsAll
+		if (skillsToCheck.containsAll(markingRecord.getSkills()) && injuries.containsAll(markingRecord.getInjuries())) {
+			injuries.removeAll(markingRecord.getInjuries());
+
+			markingRecord.getSkills().forEach(skillToRemove -> {
+
+				if (!gainedSkills.remove(skillToRemove)) {
+					baseSkills.remove(skillToRemove);
+				}
+
+			});
+
+			return markingRecord.getMarking();
+		}
+
+		return "";
 	}
 }
