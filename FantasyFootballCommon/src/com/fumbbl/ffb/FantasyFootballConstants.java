cf6dbfaec258d0b4dbb1eb4f65b11389c4b71ae0
Christian Huber <huberc@butterbrot.org>
2023-04-30 23:34:05 +0200

    Add exception handling for frame background

--- FantasyFootballCommon/src/com/fumbbl/ffb/FantasyFootballConstants.java
+++ FantasyFootballCommon/src/com/fumbbl/ffb/FantasyFootballConstants.java
@@ -1,6 +1,6 @@
 package com.fumbbl.ffb;
 
 public class FantasyFootballConstants {
-	public static final String CLIENT_VERSION = "2.19.0";
+	public static final String CLIENT_VERSION = "2.19.1";
 	public static final String SERVER_VERSION = CLIENT_VERSION;
 }
