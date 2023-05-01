0207cd0cfd0f5b9b7c56ce7eab15083a6e33d9a6
Christian Huber <huberc@butterbrot.org>
2023-05-01 18:13:44 +0200

    Only update styleprovider if color is not null

--- FantasyFootballClient/src/com/fumbbl/ffb/client/ui/GameMenuBar.java
+++ FantasyFootballClient/src/com/fumbbl/ffb/client/ui/GameMenuBar.java
@@ -989,12 +989,11 @@ public class GameMenuBar extends JMenuBar implements ActionListener, IDialogClos
 			try {
 				newColor = new Color(Integer.parseInt(getClient().getProperty(CommonProperty.SETTING_BACKGROUND_FRAME_COLOR)));
 				frameBackgroundColor.setIcon(createColorIcon(newColor));
+				styleProvider.setFrameBackground(newColor);
 			} catch (NumberFormatException ex) {
 				getClient().getFactorySource().logWithOutGameId(ex);
 			}
 		}
-
-		styleProvider.setFrameBackground(newColor);
 
 		return !Objects.equals(oldColor, newColor);
 	}
