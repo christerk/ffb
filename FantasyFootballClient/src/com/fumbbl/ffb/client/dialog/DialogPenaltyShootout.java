0e5a064eae71c8fe452e7b75107f77d8a8f61778
Christian Huber <huberc@butterbrot.org>
2024-01-07 01:10:45 +0100

    Static penalty dialog

--- FantasyFootballClient/src/com/fumbbl/ffb/client/dialog/DialogPenaltyShootout.java
+++ FantasyFootballClient/src/com/fumbbl/ffb/client/dialog/DialogPenaltyShootout.java
@@ -1,16 +1,21 @@
 package com.fumbbl.ffb.client.dialog;
 
+import com.fumbbl.ffb.IIconProperty;
 import com.fumbbl.ffb.client.FantasyFootballClient;
+import com.fumbbl.ffb.client.StyleProvider;
 import com.fumbbl.ffb.client.ui.swing.JLabel;
 import com.fumbbl.ffb.dialog.DialogId;
 import com.fumbbl.ffb.dialog.DialogPenaltyShootoutParameter;
 
-import javax.swing.Box;
-import javax.swing.BoxLayout;
+import javax.swing.BorderFactory;
+import javax.swing.Icon;
+import javax.swing.ImageIcon;
 import javax.swing.JPanel;
 import javax.swing.SwingConstants;
+import javax.swing.border.Border;
 import javax.swing.event.InternalFrameEvent;
-import java.awt.Component;
+import java.awt.Color;
+import java.awt.Font;
 import java.awt.GridLayout;
 import java.util.ArrayList;
 import java.util.List;
@@ -20,37 +25,43 @@ import java.util.List;
  */
 public class DialogPenaltyShootout extends Dialog {
 
+	private static final Color HIGHLIGHT = Color.lightGray;
+
+	private final JPanel rootPanel;
+
 	public DialogPenaltyShootout(FantasyFootballClient pClient, DialogPenaltyShootoutParameter parameter) {
 		super(pClient, "Penalty Shootout", true);
 
+		rootPanel = new JPanel();
+
+		Border innerBorder = BorderFactory.createEmptyBorder(0, 20, 0, 20);
+		Border middleBorder = BorderFactory.createLineBorder(Color.BLACK, 1, true);
+		Border outerBorder = BorderFactory.createLineBorder(Color.WHITE, 5);
+		rootPanel.setBorder(BorderFactory.createCompoundBorder(outerBorder, BorderFactory.createCompoundBorder(middleBorder, innerBorder)));
+
+
+		getContentPane().add(rootPanel);
+
 		int limit = Math.min(Math.min(parameter.getAwayRolls().size(), parameter.getHomeRolls().size()), parameter.getHomeWon().size());
 
-		getContentPane().setLayout(new GridLayout(limit + 2, 3));
-
-		//getContentPane().add(verticalHeaderPanel());
+		rootPanel.setLayout(new GridLayout(limit + 2, 3, 0, 5));
 
 
-		linePanel("", "HOME", "AWAY").forEach(getContentPane()::add);
+		List<JLabel> labels = new ArrayList<>();
+		labels.add(headerLabel("Home", true));
+		labels.add(new JLabel(dimensionProvider()));
+		labels.add(headerLabel("Away", false));
 
 		for (int i = 0; i < limit; i++) {
-			panel(parameter.getHomeRolls().get(i), parameter.getAwayRolls().get(i), parameter.getHomeWon().get(i)).forEach(getContentPane()::add);
+			labels.addAll(rollPanel(parameter.getHomeRolls().get(i), parameter.getAwayRolls().get(i), parameter.getHomeWon().get(i)));
 		}
 
-		panel("Score", parameter.getHomeScore(), parameter.getAwayScore(), parameter.homeTeamWins()).forEach(getContentPane()::add);
+		labels.addAll(scorePanel(parameter.getHomeScore(), parameter.getAwayScore(), parameter.homeTeamWins()));
+
+		labels.forEach(this::addDecorated);
 
 		pack();
 		setLocationToCenter();
-	}
-
-	private JPanel verticalHeaderPanel() {
-		JPanel headerPanel = new JPanel();
-		headerPanel.setLayout(new BoxLayout(headerPanel, BoxLayout.Y_AXIS));
-		Component glue = Box.createVerticalGlue();
-		headerPanel.add(glue);
-		JLabel scoreLabel = new JLabel(dimensionProvider(), "Score");
-		scoreLabel.setVerticalAlignment(SwingConstants.BOTTOM);
-		headerPanel.add(scoreLabel);
-		return headerPanel;
 	}
 
 	public DialogId getId() {
@@ -63,21 +74,71 @@ public class DialogPenaltyShootout extends Dialog {
 		}
 	}
 
-	private List<JLabel> linePanel(String header, String home, String away) {
-		List<JLabel> panel = new ArrayList<>();
-		panel.add(new JLabel(dimensionProvider(), header));
-		panel.add(new JLabel(dimensionProvider(), home));
-		panel.add(new JLabel(dimensionProvider(), away));
-		return panel;
+	private void addDecorated(JLabel label) {
+		label.setHorizontalTextPosition(SwingConstants.CENTER);
+		label.setHorizontalAlignment(SwingConstants.CENTER);
+		label.setOpaque(true);
+		rootPanel.add(label);
 	}
 
-	private List<JLabel> panel(int homeRoll, int awayRoll, boolean homeWin) {
-		return panel("", homeRoll, awayRoll, homeWin);
+	private JLabel headerLabel(String text, boolean home) {
+		StyleProvider styleProvider = getClient().getUserInterface().getStyleProvider();
+		JLabel label = new JLabel(dimensionProvider(), text);
+		label.setForeground(home ? styleProvider.getHome() : styleProvider.getAway());
+		return boldLabel(label);
 	}
 
-	private List<JLabel> panel(String header, int homeRoll, int awayRoll, boolean homeWin) {
 
-		return linePanel(header, String.valueOf(homeRoll), String.valueOf(awayRoll));
+	private List<JLabel> scorePanel(int homeRoll, int awayRoll, boolean homeWin) {
+		List<JLabel> labels = new ArrayList<>();
+		labels.add(new JLabel(dimensionProvider(), String.valueOf(homeRoll)));
+		labels.add(new JLabel(dimensionProvider(), "Score"));
+		labels.add(new JLabel(dimensionProvider(), String.valueOf(awayRoll)));
+
+		JLabel winnerLabel = labels.get(homeWin ? 0 : 2);
+		boldLabel(winnerLabel);
+		JLabel loserLabel = labels.get(homeWin ? 2 : 0);
+		loserLabel.setForeground(Color.LIGHT_GRAY);
+		return labels;
+	}
+
+	private JLabel boldLabel(JLabel label) {
+		Font font = label.getFont();
+		label.setFont(new Font(font.getFamily(), Font.BOLD, font.getSize()));
+		return label;
+	}
+
+	private List<JLabel> rollPanel(int home, int away, boolean homeWin) {
+		List<JLabel> labels = new ArrayList<>();
+		labels.add(new JLabel(dimensionProvider(), icon(home)));
+		labels.add(new JLabel(dimensionProvider()));
+		labels.add(new JLabel(dimensionProvider(), icon(away)));
+		JLabel winnerLabel = labels.get(homeWin ? 0 : 2);
+		winnerLabel.setBackground(HIGHLIGHT);
+		winnerLabel.setBorder(BorderFactory.createLineBorder(Color.BLACK, 1));
+		return labels;
+	}
+
+
+	private Icon icon(int roll) {
+		return new ImageIcon(getClient().getUserInterface().getIconCache().getIconByProperty(iconProperty(roll)));
+	}
+
+	private String iconProperty(int roll) {
+		switch (roll) {
+			case 1:
+				return IIconProperty.PENALTY_DIE_1;
+			case 2:
+				return IIconProperty.PENALTY_DIE_2;
+			case 3:
+				return IIconProperty.PENALTY_DIE_3;
+			case 4:
+				return IIconProperty.PENALTY_DIE_4;
+			case 5:
+				return IIconProperty.PENALTY_DIE_5;
+			default:
+				return IIconProperty.PENALTY_DIE_6;
+		}
 	}
 
 }
