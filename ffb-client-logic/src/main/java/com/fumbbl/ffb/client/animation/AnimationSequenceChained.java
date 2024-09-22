f2ca6471a5763a8cf4d34476a229d9b99e607ca6
Christian Linse <christian@butterbrot.org>
2024-09-22 13:52:12 +0200

    Use correct animation graphics

--- ffb-client-logic/src/main/java/com/fumbbl/ffb/client/animation/AnimationSequenceChained.java
+++ ffb-client-logic/src/main/java/com/fumbbl/ffb/client/animation/AnimationSequenceChained.java
@@ -67,27 +67,22 @@ public class AnimationSequenceChained implements IAnimationSequence, ActionListe
 
 	public static AnimationSequenceChained createAnimationSequenceBlastin(FantasyFootballClient client, Animation animation) {
 		DimensionProvider dimensionProvider = client.getUserInterface().getDimensionProvider();
-		PlayerIconFactory playerIconFactory = client.getUserInterface().getPlayerIconFactory();
-		Player<?> startPlayer = client.getGame().getFieldModel().getPlayer(animation.getStartCoordinate());
-		BufferedImage startIcon = playerIconFactory.getIcon(client, startPlayer);
-		Player<?> targetPlayer = client.getGame().getFieldModel().getPlayer(animation.getEndCoordinate());
-		BufferedImage targetIcon = playerIconFactory.getIcon(client, targetPlayer);
 
 		return new AnimationSequenceChained(
 			new AnimationData(dimensionProvider.mapToLocal(animation.getStartCoordinate(), true),
 				new AnimationFrame[]{
-					new AnimationFrame(IIconProperty.ANIMATION_TRICKSTER_EXPLOSION_1, 0.8f, 0.5d, 60, SoundId.BLUNDER),
-					new AnimationFrame(IIconProperty.ANIMATION_TRICKSTER_SMOKE_1, 1.0f, 0.35d, 60),
-					new AnimationFrame(IIconProperty.ANIMATION_TRICKSTER_SMOKE_2, 1.0f, 0.15d,60),
+					new AnimationFrame(IIconProperty.ANIMATION_BLASTIN_EXPLOSION_1, 0.8f, 0.5d, 60, SoundId.BLUNDER),
+					new AnimationFrame(IIconProperty.ANIMATION_BLASTIN_SMOKE_1, 1.0f, 0.35d, 60),
+					new AnimationFrame(IIconProperty.ANIMATION_BLASTIN_SMOKE_2, 1.0f, 0.15d,60),
 				}),
 			new AnimationData(dimensionProvider.mapToLocal(animation.getEndCoordinate(), true),
 				new AnimationFrame[]{
-					new AnimationFrame(IIconProperty.ANIMATION_TRICKSTER_EXPLOSION_1, 0.8f, 0.6d,  120, SoundId.EXPLODE),
-					new AnimationFrame(IIconProperty.ANIMATION_TRICKSTER_EXPLOSION_2, 0.6f, 0.6d, 120),
-					new AnimationFrame(IIconProperty.ANIMATION_TRICKSTER_SMOKE_1, 1.0f, 0.35d,  120),
-					new AnimationFrame(IIconProperty.ANIMATION_TRICKSTER_SMOKE_2, 1.0f, 0.35d, 120),
-					new AnimationFrame(IIconProperty.ANIMATION_TRICKSTER_SMOKE_3, 0.7f, 0.35d, 120),
-					new AnimationFrame(IIconProperty.ANIMATION_TRICKSTER_SMOKE_4, 0.5f, 0.35d, 120),
+					new AnimationFrame(IIconProperty.ANIMATION_BLASTIN_EXPLOSION_1, 0.8f, 0.6d,  120, SoundId.EXPLODE),
+					new AnimationFrame(IIconProperty.ANIMATION_BLASTIN_EXPLOSION_2, 0.6f, 0.6d, 120),
+					new AnimationFrame(IIconProperty.ANIMATION_BLASTIN_SMOKE_1, 1.0f, 0.35d,  120),
+					new AnimationFrame(IIconProperty.ANIMATION_BLASTIN_SMOKE_2, 1.0f, 0.35d, 120),
+					new AnimationFrame(IIconProperty.ANIMATION_BLASTIN_SMOKE_3, 0.7f, 0.35d, 120),
+					new AnimationFrame(IIconProperty.ANIMATION_BLASTIN_SMOKE_4, 0.5f, 0.35d, 120),
 				})
 		);
 	}
