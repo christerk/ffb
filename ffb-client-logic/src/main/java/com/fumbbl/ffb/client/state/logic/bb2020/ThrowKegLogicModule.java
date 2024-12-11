e4bd13684587b18ea894925b33f95f8e3af42501
Christian Linse <christian@butterbrot.org>
2024-12-11 22:33:47 +0100

    Split throw keg state

---
+++ ffb-client-logic/src/main/java/com/fumbbl/ffb/client/state/logic/bb2020/ThrowKegLogicModule.java
@@ -0,0 +1,166 @@
+package com.fumbbl.ffb.client.state.logic.bb2020;
+
+import com.fumbbl.ffb.FieldCoordinate;
+import com.fumbbl.ffb.FieldCoordinateBounds;
+import com.fumbbl.ffb.MoveSquare;
+import com.fumbbl.ffb.PlayerState;
+import com.fumbbl.ffb.client.FantasyFootballClient;
+import com.fumbbl.ffb.client.net.ClientCommunication;
+import com.fumbbl.ffb.client.state.logic.ClientAction;
+import com.fumbbl.ffb.client.state.logic.LogicModule;
+import com.fumbbl.ffb.client.state.logic.interaction.InteractionResult;
+import com.fumbbl.ffb.model.ActingPlayer;
+import com.fumbbl.ffb.model.FieldModel;
+import com.fumbbl.ffb.model.Game;
+import com.fumbbl.ffb.model.Player;
+import com.fumbbl.ffb.model.property.NamedProperties;
+import com.fumbbl.ffb.model.skill.Skill;
+import com.fumbbl.ffb.util.UtilCards;
+
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
+
+public class ThrowKegLogicModule extends LogicModule {
+
+	public ThrowKegLogicModule(FantasyFootballClient pClient) {
+		super(pClient);
+	}
+
+	@Override
+	public void postInit() {
+		super.postInit();
+		FieldModel fieldModel = client.getGame().getFieldModel();
+		Player<?> player = client.getGame().getActingPlayer().getPlayer();
+		MoveSquare[] squares = Arrays.stream(fieldModel.findAdjacentCoordinates(fieldModel.getPlayerCoordinate(player), FieldCoordinateBounds.FIELD,
+			3, false)).map(fieldCoordinate -> new MoveSquare(fieldCoordinate, 0, 0)).toArray(MoveSquare[]::new);
+		fieldModel.add(squares);
+	}
+
+	@Override
+	public void teardown() {
+		FieldModel fieldModel = client.getGame().getFieldModel();
+		fieldModel.clearMoveSquares();
+	}
+
+	@Override
+	public InteractionResult playerInteraction(Player<?> player) {
+		Game game = client.getGame();
+		ActingPlayer actingPlayer = game.getActingPlayer();
+		if (player == actingPlayer.getPlayer()) {
+			return new InteractionResult(InteractionResult.Kind.SHOW_ACTIONS);
+		} else {
+			if (isValidTarget(player, game)) {
+				client.getCommunication().sendThrowKeg(player);
+				return new InteractionResult(InteractionResult.Kind.HANDLED);
+			}
+		}
+		return new InteractionResult(InteractionResult.Kind.IGNORE);
+	}
+
+	@Override
+	public InteractionResult playerPeek(Player<?> player) {
+		Game game = client.getGame();
+		client.getClientData().setSelectedPlayer(player);
+		if (isValidTarget(player, game)) {
+			return new InteractionResult(InteractionResult.Kind.PERFORM);
+		} else {
+			return new InteractionResult(InteractionResult.Kind.INVALID);
+		}
+	}
+
+	private boolean isValidTarget(Player<?> player, Game game) {
+		ActingPlayer actingPlayer = game.getActingPlayer();
+		FieldCoordinate actingPlayerCoordinate = game.getFieldModel().getPlayerCoordinate(actingPlayer.getPlayer());
+
+		FieldCoordinate playerCoordinate = game.getFieldModel().getPlayerCoordinate(player);
+		int distance = playerCoordinate.distanceInSteps(actingPlayerCoordinate);
+
+		PlayerState playerState = game.getFieldModel().getPlayerState(player);
+
+
+		return distance <= 3 && playerState.getBase() == PlayerState.STANDING && player.getTeam() != game.getActingTeam();
+	}
+
+	@Override
+	public Set<ClientAction> availableActions() {
+		return new HashSet<ClientAction>() {{
+			add(ClientAction.END_MOVE);
+			add(ClientAction.TREACHEROUS);
+			add(ClientAction.WISDOM);
+			add(ClientAction.RAIDING_PARTY);
+			add(ClientAction.LOOK_INTO_MY_EYES);
+			add(ClientAction.BALEFUL_HEX);
+			add(ClientAction.BLACK_INK);
+			add(ClientAction.CATCH_OF_THE_DAY);
+			add(ClientAction.THEN_I_STARTED_BLASTIN);
+		}};
+	}
+
+	@Override
+	protected void performAvailableAction(Player<?> player, ClientAction action) {
+		ClientCommunication communication = client.getCommunication();
+		switch (action) {
+			case END_MOVE:
+				if (isEndPlayerActionAvailable()) {
+					communication.sendActingPlayer(null, null, false);
+				}
+				break;
+			case TREACHEROUS:
+				if (isTreacherousAvailable(player)) {
+					Skill skill = player.getSkillWithProperty(NamedProperties.canStabTeamMateForBall);
+					communication.sendUseSkill(skill, true, player.getId());
+				}
+				break;
+			case WISDOM:
+				if (isWisdomAvailable(player)) {
+					communication.sendUseWisdom();
+				}
+				break;
+			case RAIDING_PARTY:
+				if (isRaidingPartyAvailable(player)) {
+					Skill raidingSkill = player.getSkillWithProperty(NamedProperties.canMoveOpenTeamMate);
+					communication.sendUseSkill(raidingSkill, true, player.getId());
+				}
+				break;
+			case LOOK_INTO_MY_EYES:
+				if (isLookIntoMyEyesAvailable(player)) {
+					UtilCards.getUnusedSkillWithProperty(player, NamedProperties.canStealBallFromOpponent)
+						.ifPresent(lookSkill -> communication.sendUseSkill(lookSkill, true, player.getId()));
+				}
+				break;
+			case BALEFUL_HEX:
+				if (isBalefulHexAvailable(player)) {
+					Skill balefulSkill = player.getSkillWithProperty(NamedProperties.canMakeOpponentMissTurn);
+					communication.sendUseSkill(balefulSkill, true, player.getId());
+				}
+				break;
+			case BLACK_INK:
+				if (isBlackInkAvailable(player)) {
+					Skill blackInkSkill = player.getSkillWithProperty(NamedProperties.canGazeAutomatically);
+					communication.sendUseSkill(blackInkSkill, true, player.getId());
+				}
+				break;
+			case CATCH_OF_THE_DAY:
+				if (isCatchOfTheDayAvailable(player)) {
+					Skill skill = player.getSkillWithProperty(NamedProperties.canGetBallOnGround);
+					communication.sendUseSkill(skill, true, player.getId());
+				}
+				break;
+			case THEN_I_STARTED_BLASTIN:
+				if (isThenIStartedBlastinAvailable(player)) {
+					Skill skill = player.getSkillWithProperty(NamedProperties.canBlastRemotePlayer);
+					communication.sendUseSkill(skill, true, player.getId());
+				}
+				break;
+			default:
+				break;
+		}
+	}
+
+	public boolean isEndPlayerActionAvailable() {
+		Game game = client.getGame();
+		ActingPlayer actingPlayer = game.getActingPlayer();
+		return !actingPlayer.hasActed();
+	}
+}
---
+++ ffb-client/src/main/java/com/fumbbl/ffb/client/state/bb2020/ClientStateThrowKeg.java
@@ -0,0 +1,183 @@
+package com.fumbbl.ffb.client.state.bb2020;
+
+import com.fumbbl.ffb.ClientStateId;
+import com.fumbbl.ffb.FieldCoordinate;
+import com.fumbbl.ffb.IIconProperty;
+import com.fumbbl.ffb.client.ActionKey;
+import com.fumbbl.ffb.client.FantasyFootballClientAwt;
+import com.fumbbl.ffb.client.IconCache;
+import com.fumbbl.ffb.client.UserInterface;
+import com.fumbbl.ffb.client.state.ClientStateAwt;
+import com.fumbbl.ffb.client.state.IPlayerPopupMenuKeys;
+import com.fumbbl.ffb.client.state.logic.ClientAction;
+import com.fumbbl.ffb.client.state.logic.bb2020.ThrowKegLogicModule;
+import com.fumbbl.ffb.client.state.logic.interaction.InteractionResult;
+import com.fumbbl.ffb.client.ui.swing.JMenuItem;
+import com.fumbbl.ffb.client.util.UtilClientCursor;
+import com.fumbbl.ffb.model.ActingPlayer;
+import com.fumbbl.ffb.model.FieldModel;
+import com.fumbbl.ffb.model.Game;
+import com.fumbbl.ffb.model.Player;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class ClientStateThrowKeg extends ClientStateAwt<ThrowKegLogicModule> {
+
+	public ClientStateThrowKeg(FantasyFootballClientAwt pClient) {
+		super(pClient, new ThrowKegLogicModule(pClient));
+	}
+
+	public ClientStateId getId() {
+		return ClientStateId.THROW_KEG;
+	}
+
+	@Override
+	public void initUI() {
+		super.initUI();
+		getClient().getUserInterface().getFieldComponent().refresh();
+	}
+
+	@Override
+	public void leaveState() {
+		FieldModel fieldModel = getClient().getGame().getFieldModel();
+		fieldModel.clearMoveSquares();
+		getClient().getUserInterface().getFieldComponent().refresh();
+	}
+
+	protected void clickOnPlayer(Player<?> player) {
+		InteractionResult result = logicModule.playerInteraction(player);
+
+		switch (result.getKind()) {
+			case SHOW_ACTIONS:
+				createAndShowPopupMenuForActingPlayer();
+				break;
+			default:
+				break;
+		}
+	}
+
+	protected boolean mouseOverPlayer(Player<?> player) {
+		InteractionResult result = logicModule.playerPeek(player);
+		UserInterface userInterface = getClient().getUserInterface();
+		userInterface.refreshSideBars();
+		switch (result.getKind()) {
+			case PERFORM:
+				UtilClientCursor.setCustomCursor(userInterface, IIconProperty.CURSOR_KEG);
+				break;
+			case INVALID:
+				UtilClientCursor.setCustomCursor(userInterface, IIconProperty.CURSOR_INVALID_KEG);
+				break;
+			default:
+				break;
+		}
+
+		return true;
+	}
+
+	@Override
+	protected boolean mouseOverField(FieldCoordinate pCoordinate) {
+		UserInterface userInterface = getClient().getUserInterface();
+		UtilClientCursor.setCustomCursor(userInterface, IIconProperty.CURSOR_INVALID_KEG);
+		return super.mouseOverField(pCoordinate);
+	}
+
+	protected void createAndShowPopupMenuForActingPlayer() {
+
+		Game game = getClient().getGame();
+		UserInterface userInterface = getClient().getUserInterface();
+		IconCache iconCache = userInterface.getIconCache();
+		userInterface.getFieldComponent().getLayerUnderPlayers().clearMovePath();
+		List<JMenuItem> menuItemList = new ArrayList<>();
+		ActingPlayer actingPlayer = game.getActingPlayer();
+
+		if (logicModule.isEndPlayerActionAvailable()) {
+			addEndActionLabel(iconCache, menuItemList);
+		}
+
+		if (logicModule.isTreacherousAvailable(actingPlayer)) {
+			menuItemList.add(createTreacherousItem(iconCache));
+		}
+		if (logicModule.isWisdomAvailable(actingPlayer)) {
+			menuItemList.add(createWisdomItem(iconCache));
+		}
+		if (logicModule.isRaidingPartyAvailable(actingPlayer)) {
+			menuItemList.add(createRaidingPartyItem(iconCache));
+		}
+		if (logicModule.isLookIntoMyEyesAvailable(actingPlayer)) {
+			menuItemList.add(createLookIntoMyEyesItem(iconCache));
+		}
+		if (logicModule.isBalefulHexAvailable(actingPlayer)) {
+			menuItemList.add(createBalefulHexItem(iconCache));
+		}
+		if (logicModule.isBlackInkAvailable(actingPlayer)) {
+			menuItemList.add(createBlackInkItem(iconCache));
+		}
+		if (logicModule.isCatchOfTheDayAvailable(actingPlayer)) {
+			menuItemList.add(createCatchOfTheDayItem(iconCache));
+		}
+		if (logicModule.isThenIStartedBlastinAvailable(actingPlayer)) {
+			menuItemList.add(createThenIStartedBlastinItem(iconCache));
+		}
+		createPopupMenu(menuItemList.toArray(new JMenuItem[0]));
+		showPopupMenuForPlayer(actingPlayer.getPlayer());
+
+	}
+
+	public boolean actionKeyPressed(ActionKey pActionKey) {
+		if (pActionKey == null) {
+			return false;
+		}
+		Player<?> player = getClient().getGame().getActingPlayer().getPlayer();
+		switch (pActionKey) {
+			case PLAYER_ACTION_END_MOVE:
+				menuItemSelected(player, IPlayerPopupMenuKeys.KEY_END_MOVE);
+				return true;
+			case PLAYER_ACTION_TREACHEROUS:
+				menuItemSelected(player, IPlayerPopupMenuKeys.KEY_TREACHEROUS);
+				return true;
+			case PLAYER_ACTION_WISDOM:
+				menuItemSelected(player, IPlayerPopupMenuKeys.KEY_WISDOM);
+				return true;
+			case PLAYER_ACTION_RAIDING_PARTY:
+				menuItemSelected(player, IPlayerPopupMenuKeys.KEY_RAIDING_PARTY);
+				return true;
+			case PLAYER_ACTION_LOOK_INTO_MY_EYES:
+				menuItemSelected(player, IPlayerPopupMenuKeys.KEY_LOOK_INTO_MY_EYES);
+				return true;
+			case PLAYER_ACTION_BALEFUL_HEX:
+				menuItemSelected(player, IPlayerPopupMenuKeys.KEY_BALEFUL_HEX);
+				return true;
+			case PLAYER_ACTION_BLACK_INK:
+				menuItemSelected(player, IPlayerPopupMenuKeys.KEY_BLACK_INK);
+				return true;
+			case PLAYER_ACTION_CATCH_OF_THE_DAY:
+				menuItemSelected(player, IPlayerPopupMenuKeys.KEY_CATCH_OF_THE_DAY);
+				return true;
+			case PLAYER_ACITON_THEN_I_STARTED_BLASTIN:
+				menuItemSelected(player, IPlayerPopupMenuKeys.KEY_THEN_I_STARTED_BLASTIN);
+				return true;
+			default:
+				return super.actionKeyPressed(pActionKey);
+		}
+	}
+
+
+	@Override
+	protected Map<Integer, ClientAction> actionMapping() {
+		return new HashMap<Integer, ClientAction>() {{
+			put(IPlayerPopupMenuKeys.KEY_END_MOVE,ClientAction.END_MOVE);
+			put(IPlayerPopupMenuKeys.KEY_TREACHEROUS,ClientAction.TREACHEROUS);
+			put(IPlayerPopupMenuKeys.KEY_WISDOM,ClientAction.WISDOM);
+			put(IPlayerPopupMenuKeys.KEY_RAIDING_PARTY,ClientAction.RAIDING_PARTY);
+			put(IPlayerPopupMenuKeys.KEY_LOOK_INTO_MY_EYES,ClientAction.LOOK_INTO_MY_EYES);
+			put(IPlayerPopupMenuKeys.KEY_BALEFUL_HEX,ClientAction.BALEFUL_HEX);
+			put(IPlayerPopupMenuKeys.KEY_BLACK_INK,ClientAction.BLACK_INK);
+			put(IPlayerPopupMenuKeys.KEY_CATCH_OF_THE_DAY,ClientAction.CATCH_OF_THE_DAY);
+			put(IPlayerPopupMenuKeys.KEY_THEN_I_STARTED_BLASTIN,ClientAction.THEN_I_STARTED_BLASTIN);
+		}};
+	}
+
+}
