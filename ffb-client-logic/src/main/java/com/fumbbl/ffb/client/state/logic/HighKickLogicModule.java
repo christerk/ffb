5f81e8cbd9faf4ac9d24348d434169502d350827
Christian Linse <christian@butterbrot.org>
2024-12-12 23:02:30 +0100

    Split high kick state

---
+++ ffb-client-logic/src/main/java/com/fumbbl/ffb/client/state/logic/HighKickLogicModule.java
@@ -0,0 +1,85 @@
+package com.fumbbl.ffb.client.state.logic;
+
+import com.fumbbl.ffb.FieldCoordinate;
+import com.fumbbl.ffb.PlayerState;
+import com.fumbbl.ffb.client.FantasyFootballClient;
+import com.fumbbl.ffb.client.state.logic.interaction.InteractionResult;
+import com.fumbbl.ffb.model.Game;
+import com.fumbbl.ffb.model.Player;
+
+import java.util.Collections;
+import java.util.Set;
+
+/**
+ *
+ * @author Kalimar
+ */
+public class HighKickLogicModule extends LogicModule {
+
+	private FieldCoordinate fOldCoordinate;
+
+	public HighKickLogicModule(FantasyFootballClient pClient) {
+		super(pClient);
+	}
+
+	@Override
+	public void postInit() {
+		super.postInit();
+		fOldCoordinate = null;
+	}
+
+	@Override
+	public InteractionResult playerInteraction(Player<?> pPlayer) {
+		if (isPlayerSelectable(pPlayer)) {
+			Game game = client.getGame();
+			Player<?> oldPlayer = game.getFieldModel().getPlayer(game.getFieldModel().getBallCoordinate());
+			if (pPlayer != oldPlayer) {
+				if ((oldPlayer != null) && (fOldCoordinate != null)) {
+					client.getCommunication().sendSetupPlayer(oldPlayer, fOldCoordinate);
+				}
+				fOldCoordinate = game.getFieldModel().getPlayerCoordinate(pPlayer);
+				client.getCommunication().sendSetupPlayer(pPlayer, game.getFieldModel().getBallCoordinate());
+			} else {
+				client.getCommunication().sendSetupPlayer(pPlayer, fOldCoordinate);
+				fOldCoordinate = null;
+			}
+			return new InteractionResult(InteractionResult.Kind.HANDLED);
+		}
+		return new InteractionResult(InteractionResult.Kind.IGNORE);
+	}
+
+	@Override
+	public InteractionResult playerPeek(Player<?> pPlayer) {
+		if (isPlayerSelectable(pPlayer)) {
+			return new InteractionResult(InteractionResult.Kind.PERFORM);
+		} else {
+			return new InteractionResult(InteractionResult.Kind.RESET);
+		}
+	}
+
+	private boolean isPlayerSelectable(Player<?> pPlayer) {
+		boolean selectable = false;
+		if (pPlayer != null) {
+			Game game = client.getGame();
+			PlayerState playerState = game.getFieldModel().getPlayerState(pPlayer);
+			selectable = ((playerState != null) && playerState.isActive() && game.getTeamHome().hasPlayer(pPlayer));
+		}
+		return selectable;
+	}
+
+	@Override
+	public void endTurn() {
+		client.getCommunication().sendEndTurn(client.getGame().getTurnMode());
+		client.getClientData().setEndTurnButtonHidden(true);
+	}
+
+	@Override
+	public Set<ClientAction> availableActions() {
+		return Collections.emptySet();
+	}
+	@Override
+	protected void performAvailableAction(Player<?> player, ClientAction action) {
+		// no actions
+	}
+
+}
---
+++ ffb-client/src/main/java/com/fumbbl/ffb/client/state/ClientStateHighKick.java
@@ -0,0 +1,71 @@
+package com.fumbbl.ffb.client.state;
+
+import com.fumbbl.ffb.BoxType;
+import com.fumbbl.ffb.ClientStateId;
+import com.fumbbl.ffb.FieldCoordinate;
+import com.fumbbl.ffb.IIconProperty;
+import com.fumbbl.ffb.client.FantasyFootballClientAwt;
+import com.fumbbl.ffb.client.state.logic.ClientAction;
+import com.fumbbl.ffb.client.state.logic.HighKickLogicModule;
+import com.fumbbl.ffb.client.ui.SideBarComponent;
+import com.fumbbl.ffb.model.Player;
+
+import java.util.Collections;
+import java.util.Map;
+
+/**
+ *
+ * @author Kalimar
+ */
+public class ClientStateHighKick extends ClientStateAwt<HighKickLogicModule> {
+
+
+	protected ClientStateHighKick(FantasyFootballClientAwt pClient) {
+		super(pClient, new HighKickLogicModule(pClient));
+	}
+
+	public ClientStateId getId() {
+		return ClientStateId.HIGH_KICK;
+	}
+
+	public void initUI() {
+		super.initUI();
+		setClickable(true);
+	}
+
+	protected void clickOnPlayer(Player<?> pPlayer) {
+		logicModule.playerInteraction(pPlayer);
+	}
+
+	protected boolean mouseOverPlayer(Player<?> pPlayer) {
+		super.mouseOverPlayer(pPlayer);
+		determineCursor(logicModule.playerInteraction(pPlayer));
+		return true;
+	}
+
+	protected boolean mouseOverField(FieldCoordinate pCoordinate) {
+		super.mouseOverField(pCoordinate);
+		determineCursor(logicModule.fieldPeek(pCoordinate));
+		return true;
+	}
+
+	@Override
+	protected Map<Integer, ClientAction> actionMapping() {
+		return Collections.emptyMap();
+	}
+
+	@Override
+	protected String validCursor() {
+		return IIconProperty.CURSOR_PASS;
+	}
+
+	@Override
+	protected void postEndTurn() {
+		SideBarComponent sideBarHome = getClient().getUserInterface().getSideBarHome();
+		if (sideBarHome.getOpenBox() == BoxType.RESERVES) {
+			sideBarHome.closeBox();
+		}
+		getClient().getClientData().setEndTurnButtonHidden(true);
+		sideBarHome.refresh();
+	}
+}
