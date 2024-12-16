39352439154c4a0fa205781e2e691375b245500b
Christian Linse <christian@butterbrot.org>
2024-12-16 14:09:29 +0100

    Split quick snap state

---
+++ ffb-client/src/main/java/com/fumbbl/ffb/client/state/ClientStateQuickSnap.java
@@ -0,0 +1,36 @@
+package com.fumbbl.ffb.client.state;
+
+import com.fumbbl.ffb.ClientStateId;
+import com.fumbbl.ffb.FieldCoordinate;
+import com.fumbbl.ffb.client.ClientData;
+import com.fumbbl.ffb.client.FantasyFootballClientAwt;
+import com.fumbbl.ffb.client.state.logic.QuickSnapLogicModule;
+
+public class ClientStateQuickSnap extends AbstractClientStateSetup<QuickSnapLogicModule> {
+
+	protected ClientStateQuickSnap(FantasyFootballClientAwt pClient) {
+		super(pClient, new QuickSnapLogicModule(pClient));
+	}
+
+	public ClientStateId getId() {
+		return ClientStateId.QUICK_SNAP;
+	}
+
+	@Override
+	public boolean isInitDragAllowed(FieldCoordinate pCoordinate) {
+		return logicModule.squareIsOnPitch(pCoordinate);
+	}
+
+	@Override
+	public boolean isDragAllowed(FieldCoordinate pCoordinate) {
+		ClientData clientData = getClient().getClientData();
+		return logicModule.squareIsEmpty(pCoordinate)
+			&& logicModule.squaresAreSameOrAdjacent(pCoordinate, clientData.getDragStartPosition());
+	}
+
+	@Override
+	public boolean isDropAllowed(FieldCoordinate pCoordinate) {
+		ClientData clientData = getClient().getClientData();
+		return logicModule.squaresAreSameOrAdjacent(pCoordinate, clientData.getDragStartPosition());
+	}
+}
