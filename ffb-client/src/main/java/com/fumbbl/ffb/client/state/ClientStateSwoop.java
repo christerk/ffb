0add33101db5afeff76651871dba56e5b29a2d75
Christian Linse <christian@butterbrot.org>
2024-12-16 22:46:38 +0100

    Split swoop state

---
+++ ffb-client/src/main/java/com/fumbbl/ffb/client/state/ClientStateSwoop.java
@@ -0,0 +1,47 @@
+package com.fumbbl.ffb.client.state;
+
+import com.fumbbl.ffb.ClientStateId;
+import com.fumbbl.ffb.FieldCoordinate;
+import com.fumbbl.ffb.client.FantasyFootballClientAwt;
+import com.fumbbl.ffb.client.UserInterface;
+import com.fumbbl.ffb.client.state.logic.SwoopLogicModule;
+import com.fumbbl.ffb.model.Player;
+
+/**
+ *
+ * @author Kalimar
+ */
+public class ClientStateSwoop extends AbstractClientStateMove<SwoopLogicModule> {
+
+	protected ClientStateSwoop(FantasyFootballClientAwt pClient) {
+		super(pClient, new SwoopLogicModule(pClient));
+	}
+
+	public ClientStateId getId() {
+		return ClientStateId.SWOOP;
+	}
+
+	protected void clickOnField(FieldCoordinate pCoordinate) {
+		logicModule.fieldInteraction(pCoordinate);
+	}
+
+	protected void clickOnPlayer(Player<?> pPlayer) {
+		logicModule.playerInteraction(pPlayer);
+	}
+
+	protected boolean mouseOverPlayer(Player<?> pPlayer) {
+		UserInterface userInterface = getClient().getUserInterface();
+		determineCursor(logicModule.playerPeek(pPlayer));
+		getClient().getClientData().setSelectedPlayer(pPlayer);
+		userInterface.refreshSideBars();
+		return true;
+	}
+
+	@Override
+	public void leaveState() {
+		// clear marked players
+		UserInterface userInterface = getClient().getUserInterface();
+		userInterface.getFieldComponent().getLayerRangeRuler().clearMarkedCoordinates();
+		userInterface.getFieldComponent().refresh();
+	}
+}
