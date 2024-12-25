6c958469fb2283be6f9cb8b98e0bff1e91b0013c
Christian Linse <christian@butterbrot.org>
2024-12-25 21:03:01 +0100

    Trigger dialog display after receiving team lists

--- ffb-client/src/main/java/com/fumbbl/ffb/client/state/ClientStateSetup.java
+++ ffb-client/src/main/java/com/fumbbl/ffb/client/state/ClientStateSetup.java
@@ -4,6 +4,7 @@ import com.fumbbl.ffb.ClientStateId;
 import com.fumbbl.ffb.client.ActionKey;
 import com.fumbbl.ffb.client.FantasyFootballClientAwt;
 import com.fumbbl.ffb.client.state.logic.SetupLogicModule;
+import com.fumbbl.ffb.client.state.logic.interaction.InteractionResult;
 import com.fumbbl.ffb.net.NetCommand;
 
 /**
@@ -37,7 +38,14 @@ public class ClientStateSetup extends AbstractClientStateSetup<SetupLogicModule>
 	}
 
 	public void handleCommand(NetCommand pNetCommand) {
-		logicModule.handleCommand(pNetCommand, fLoadDialog);
+		InteractionResult result = logicModule.handleCommand(pNetCommand, fLoadDialog);
+		switch (result.getKind()) {
+			case HANDLED:
+				getClient().getUserInterface().getDialogManager().updateDialog();
+				break;
+			default:
+				break;
+		}
 	}
 
 }
