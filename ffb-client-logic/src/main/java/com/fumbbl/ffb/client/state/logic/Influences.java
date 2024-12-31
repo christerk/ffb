b9d171169ab71a63cf8e123e35607bf6514b34e9
Christian Linse <christian@butterbrot.org>
2024-12-31 13:48:53 +0100

    Add influenced actions to influences

--- ffb-client-logic/src/main/java/com/fumbbl/ffb/client/state/logic/Influences.java
+++ ffb-client-logic/src/main/java/com/fumbbl/ffb/client/state/logic/Influences.java
@@ -1,7 +1,21 @@
 package com.fumbbl.ffb.client.state.logic;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public enum Influences {
-	BALL_ACTIONS_DUE_TO_TREACHEROUS,
-	IS_JUMPING,
-	VOMIT_DUE_TO_PUTRID_REGURGITATION
+	BALL_ACTIONS_DUE_TO_TREACHEROUS(ClientAction.PASS, ClientAction.HAND_OVER),
+	IS_JUMPING(ClientAction.JUMP),
+	VOMIT_DUE_TO_PUTRID_REGURGITATION(ClientAction.PROJECTILE_VOMIT);
+
+	private final List<ClientAction> influencedActions = new ArrayList<>();
+
+	Influences(ClientAction... influencedActions) {
+		this.influencedActions.addAll(Arrays.asList(influencedActions));
+	}
+
+	public List<ClientAction> getInfluencedActions() {
+		return influencedActions;
+	}
 }
