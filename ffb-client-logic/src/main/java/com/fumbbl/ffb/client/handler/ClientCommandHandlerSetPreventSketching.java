690caf8803024fd36ae22623220110a89e040d2a
Christian Linse <christian@butterbrot.org>
2025-06-12 00:11:43 +0200

    Log when coaches get (un)blocked

--- ffb-client-logic/src/main/java/com/fumbbl/ffb/client/handler/ClientCommandHandlerSetPreventSketching.java
+++ ffb-client-logic/src/main/java/com/fumbbl/ffb/client/handler/ClientCommandHandlerSetPreventSketching.java
@@ -1,7 +1,9 @@
 package com.fumbbl.ffb.client.handler;
 
 import com.fumbbl.ffb.client.FantasyFootballClient;
+import com.fumbbl.ffb.client.TextStyle;
 import com.fumbbl.ffb.client.overlay.sketch.ClientSketchManager;
+import com.fumbbl.ffb.client.ui.ChatComponent;
 import com.fumbbl.ffb.model.change.ModelChange;
 import com.fumbbl.ffb.model.change.ModelChangeId;
 import com.fumbbl.ffb.model.sketch.SketchState;
@@ -24,11 +26,25 @@ public class ClientCommandHandlerSetPreventSketching extends ClientCommandHandle
 	public boolean handleNetCommand(NetCommand pNetCommand, ClientCommandHandlerMode pMode) {
 		ClientSketchManager sketchManager = getClient().getUserInterface().getSketchManager();
 		ServerCommandSetPreventSketching command = (ServerCommandSetPreventSketching) pNetCommand;
+
+		String prefix;
+
+		if (getClient().getParameters().getCoach().equals(command.getCoach())) {
+			prefix = "You are";
+		} else {
+			prefix = command.getCoach() + " is";
+		}
+
+		String action;
 		if (command.isPreventSketching()) {
 			sketchManager.preventedFromSketching(command.getCoach());
+			action = "blocked";
 		} else {
 			sketchManager.allowSketching(command.getCoach());
+			action = "unblocked";
 		}
+		ChatComponent chat = getClient().getUserInterface().getChat();
+		chat.append(TextStyle.SPECTATOR, prefix + " " + action);
 		SketchState sketchState = new SketchState(sketchManager.getAllSketches());
 		ModelChange modelChange = new ModelChange(ModelChangeId.SKETCH_UPDATE, null, sketchState);
 		getClient().getGame().notifyObservers(modelChange);
