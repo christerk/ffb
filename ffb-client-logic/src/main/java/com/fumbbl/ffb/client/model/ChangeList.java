ef14b51370329c2413da0a9e101263cab512b87d
Christian Linse <christian@butterbrot.org>
2025-06-13 21:55:24 +0200

    Add improvement entry for latest change

--- ffb-client-logic/src/main/java/com/fumbbl/ffb/client/model/ChangeList.java
+++ ffb-client-logic/src/main/java/com/fumbbl/ffb/client/model/ChangeList.java
@@ -15,6 +15,7 @@ public class ChangeList {
 		versions.add(new VersionChangeList("2.36.0")
 			.addBugfix("Savage Blow: When performing an uphill block wthout any other re-roll sources left the dialog did not show any buttons to continue")
 			.addBehaviorChange("Players in dugouts are spaced more evenly")
+			.addImprovement("Player in dugout are scaled up in wide mode")
 		);
 
 		versions.add(new VersionChangeList("2.35.2")
