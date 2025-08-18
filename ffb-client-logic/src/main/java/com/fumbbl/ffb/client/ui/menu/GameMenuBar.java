160b36c2c37832d80e19ab6e758005a934c7f6cc
Christian Linse <christian@butterbrot.org>
2025-08-18 23:23:14 +0200

    Move inducments menu to separate class

--- ffb-client-logic/src/main/java/com/fumbbl/ffb/client/ui/menu/GameMenuBar.java
+++ ffb-client-logic/src/main/java/com/fumbbl/ffb/client/ui/menu/GameMenuBar.java
@@ -7,8 +7,6 @@ import com.fumbbl.ffb.FantasyFootballException;
 import com.fumbbl.ffb.FieldCoordinate;
 import com.fumbbl.ffb.IClientProperty;
 import com.fumbbl.ffb.IClientPropertyValue;
-import com.fumbbl.ffb.IIconProperty;
-import com.fumbbl.ffb.PlayerType;
 import com.fumbbl.ffb.TurnMode;
 import com.fumbbl.ffb.client.ActionKey;
 import com.fumbbl.ffb.client.ClientData;
@@ -37,15 +35,9 @@ import com.fumbbl.ffb.client.ui.menu.game.StandardGameMenu;
 import com.fumbbl.ffb.client.ui.swing.JMenu;
 import com.fumbbl.ffb.client.ui.swing.JMenuItem;
 import com.fumbbl.ffb.client.ui.swing.JRadioButtonMenuItem;
-import com.fumbbl.ffb.inducement.Card;
-import com.fumbbl.ffb.inducement.CardType;
-import com.fumbbl.ffb.inducement.Inducement;
-import com.fumbbl.ffb.inducement.Usage;
 import com.fumbbl.ffb.model.Game;
-import com.fumbbl.ffb.model.InducementSet;
 import com.fumbbl.ffb.model.Player;
 import com.fumbbl.ffb.model.PlayerResult;
-import com.fumbbl.ffb.model.Team;
 import com.fumbbl.ffb.option.GameOptionBoolean;
 import com.fumbbl.ffb.option.GameOptionId;
 import com.fumbbl.ffb.util.StringTool;
@@ -69,17 +61,13 @@ import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
 import java.io.File;
 import java.io.IOException;
-import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.Comparator;
 import java.util.HashMap;
 import java.util.LinkedHashMap;
-import java.util.List;
 import java.util.Map;
 import java.util.Objects;
 import java.util.function.Consumer;
 import java.util.function.Supplier;
-import java.util.stream.Collectors;
 
 import static com.fumbbl.ffb.CommonProperty.SETTING_AUTOMOVE;
 import static com.fumbbl.ffb.CommonProperty.SETTING_BACKGROUND_CHAT;
@@ -244,9 +232,8 @@ public class GameMenuBar extends JMenuBar implements ActionListener, IDialogClos
 
 	private JMenu fMissingPlayersMenu;
 
-	private JMenu fInducementsMenu;
-
 	private CardsMenu cardsMenu;
+	private InducementsMenu inducementsMenu;
 	private PrayersMenu prayersMenu;
 	private OptionsMenu optionsMenu;
 	private HelpMenu helpMenu;
@@ -258,11 +245,6 @@ public class GameMenuBar extends JMenuBar implements ActionListener, IDialogClos
 	private JMenuItem resetBackgroundColors;
 	private JMenuItem resetFontColors;
 
-
-	private int fCurrentInducementTotalHome;
-	private int fCurrentUsedCardsHome;
-	private int fCurrentInducementTotalAway;
-	private int fCurrentUsedCardsAway;
 
 	private final Map<CommonProperty, JMenu> exposedMenus = new HashMap<>();
 
@@ -309,12 +291,6 @@ public class GameMenuBar extends JMenuBar implements ActionListener, IDialogClos
 		fMissingPlayersMenu.setMnemonic(KeyEvent.VK_M);
 		fMissingPlayersMenu.setEnabled(false);
 		add(fMissingPlayersMenu);
-
-		fInducementsMenu = new JMenu(dimensionProvider, "Inducements");
-		fInducementsMenu.setMnemonic(KeyEvent.VK_I);
-		fInducementsMenu.setEnabled(false);
-		add(fInducementsMenu);
-
 
 	}
 
@@ -860,10 +836,6 @@ public class GameMenuBar extends JMenuBar implements ActionListener, IDialogClos
 	}
 
 	public void init() {
-		fCurrentInducementTotalHome = -1;
-		fCurrentUsedCardsHome = 0;
-		fCurrentInducementTotalAway = -1;
-		fCurrentUsedCardsAway = 0;
 
 		Arrays.stream(this.getComponents()).filter(comp -> comp != gameModeMenu).forEach(this::remove);
 
@@ -884,6 +856,9 @@ public class GameMenuBar extends JMenuBar implements ActionListener, IDialogClos
 		createTeamSetupMenu();
 		createUserSettingsMenu();
 		createGameStatusMenus();
+
+		inducementsMenu = new InducementsMenu(getClient(), dimensionProvider);
+		add(inducementsMenu);
 
 		cardsMenu = new CardsMenu(getClient(), dimensionProvider);
 		add(cardsMenu);
@@ -1345,72 +1320,7 @@ public class GameMenuBar extends JMenuBar implements ActionListener, IDialogClos
 	}
 
 	public void updateInducements() {
-
-		boolean refreshNecessary = false;
-		Game game = getClient().getGame();
-
-		InducementSet inducementSetHome = game.getTurnDataHome().getInducementSet();
-		int totalInducementHome = inducementSetHome.totalInducements();
-		if ((fCurrentInducementTotalHome < 0) || (fCurrentInducementTotalHome != totalInducementHome)) {
-			fCurrentInducementTotalHome = totalInducementHome;
-			refreshNecessary = true;
-		}
-		int usedCardsHome = inducementSetHome.getDeactivatedCards().length + inducementSetHome.getActiveCards().length;
-		if (usedCardsHome != fCurrentUsedCardsHome) {
-			fCurrentUsedCardsHome = usedCardsHome;
-			refreshNecessary = true;
-		}
-
-		InducementSet inducementSetAway = game.getTurnDataAway().getInducementSet();
-		int totalInducementAway = inducementSetAway.totalInducements();
-		if ((fCurrentInducementTotalAway < 0) || (fCurrentInducementTotalAway != totalInducementAway)) {
-			fCurrentInducementTotalAway = totalInducementAway;
-			refreshNecessary = true;
-		}
-		int usedCardsAway = inducementSetAway.getDeactivatedCards().length + inducementSetAway.getActiveCards().length;
-		if (usedCardsAway != fCurrentUsedCardsAway) {
-			fCurrentUsedCardsAway = usedCardsAway;
-			refreshNecessary = true;
-		}
-
-		if (refreshNecessary) {
-
-			JMenu fInducementsHomeMenu;
-			JMenu fInducementsAwayMenu;
-			fInducementsMenu.removeAll();
-
-			if (fCurrentInducementTotalHome > 0) {
-				fInducementsHomeMenu = new JMenu(dimensionProvider, totalInducementHome + " Home Team");
-				fInducementsHomeMenu.setForeground(Color.RED);
-				fInducementsHomeMenu.setMnemonic(KeyEvent.VK_H);
-				fInducementsMenu.add(fInducementsHomeMenu);
-				addInducements(fInducementsHomeMenu, inducementSetHome);
-			}
-
-			if (fCurrentInducementTotalAway > 0) {
-				fInducementsAwayMenu = new JMenu(dimensionProvider, totalInducementAway + " Away Team");
-				fInducementsAwayMenu.setForeground(Color.BLUE);
-				fInducementsAwayMenu.setMnemonic(KeyEvent.VK_A);
-				fInducementsMenu.add(fInducementsAwayMenu);
-				addInducements(fInducementsAwayMenu, inducementSetAway);
-			}
-
-			if ((fCurrentInducementTotalHome + fCurrentInducementTotalAway) > 0) {
-				StringBuilder menuText = new StringBuilder().append(fCurrentInducementTotalHome + fCurrentInducementTotalAway);
-				if ((fCurrentInducementTotalHome + fCurrentInducementTotalAway) > 1) {
-					menuText.append(" Inducements");
-				} else {
-					menuText.append(" Inducement");
-				}
-				fInducementsMenu.setText(menuText.toString());
-				fInducementsMenu.setEnabled(true);
-			} else {
-				fInducementsMenu.setText("No Inducements");
-				fInducementsMenu.setEnabled(false);
-			}
-
-		}
-
+		inducementsMenu.refresh();
 	}
 
 	private void updateActiveCards() {
@@ -1420,127 +1330,6 @@ public class GameMenuBar extends JMenuBar implements ActionListener, IDialogClos
 
 	private void updatePrayers() {
 		prayersMenu.refresh();
-	}
-
-	private void addInducements(JMenu pInducementMenu, InducementSet pInducementSet) {
-		Inducement[] inducements = pInducementSet.getInducements();
-		Arrays.sort(inducements, Comparator.comparing(pInducement -> pInducement.getType().getName()));
-		for (Inducement inducement : inducements) {
-			if (!Usage.EXCLUDE_FROM_RESULT.containsAll(inducement.getType().getUsages())) {
-				if (inducement.getValue() > 0) {
-					StringBuilder inducementText = new StringBuilder();
-					inducementText.append(inducement.getValue()).append(" ");
-					if (inducement.getValue() > 1) {
-						inducementText.append(inducement.getType().getPlural());
-					} else {
-						inducementText.append(inducement.getType().getSingular());
-					}
-					JMenuItem inducementItem = new JMenuItem(dimensionProvider, inducementText.toString());
-					pInducementMenu.add(inducementItem);
-				}
-			}
-		}
-
-		Game game = getClient().getGame();
-		Team team = pInducementSet.getTurnData().isHomeData() ? game.getTeamHome() : game.getTeamAway();
-		List<Player<?>> starPlayers = new ArrayList<>();
-		for (Player<?> player : team.getPlayers()) {
-			if (player.getPlayerType() == PlayerType.STAR) {
-				starPlayers.add(player);
-			}
-		}
-		if (!starPlayers.isEmpty()) {
-			StringBuilder starPlayerMenuText = new StringBuilder();
-			starPlayerMenuText.append(starPlayers.size());
-			if (starPlayers.size() == 1) {
-				starPlayerMenuText.append(" Star Player");
-			} else {
-				starPlayerMenuText.append(" Star Players");
-			}
-			JMenu starPlayerMenu = new JMenu(dimensionProvider, starPlayerMenuText.toString());
-			pInducementMenu.add(starPlayerMenu);
-			for (Player<?> player : starPlayers) {
-				addPlayerMenuItem(starPlayerMenu, player, player.getName());
-			}
-		}
-
-		List<Player<?>> mercenaries = new ArrayList<>();
-		for (Player<?> player : team.getPlayers()) {
-			if (player.getPlayerType() == PlayerType.MERCENARY) {
-				mercenaries.add(player);
-			}
-		}
-		if (!mercenaries.isEmpty()) {
-			StringBuilder mercenaryMenuText = new StringBuilder();
-			mercenaryMenuText.append(mercenaries.size());
-			if (mercenaries.size() == 1) {
-				mercenaryMenuText.append(" Mercenary");
-			} else {
-				mercenaryMenuText.append(" Mercenaries");
-			}
-			JMenu mercenaryMenu = new JMenu(dimensionProvider, mercenaryMenuText.toString());
-			pInducementMenu.add(mercenaryMenu);
-			for (Player<?> player : mercenaries) {
-				addPlayerMenuItem(mercenaryMenu, player, player.getName());
-			}
-		}
-
-		List<Player<?>> staff = new ArrayList<>();
-		for (Player<?> player : team.getPlayers()) {
-			if (player.getPlayerType() == PlayerType.INFAMOUS_STAFF) {
-				staff.add(player);
-			}
-		}
-		if (!staff.isEmpty()) {
-			String staffText = staff.size() + " Infamous Staff";
-			JMenu staffMenu = new JMenu(dimensionProvider, staffText);
-			pInducementMenu.add(staffMenu);
-			for (Player<?> player : staff) {
-				addPlayerMenuItem(staffMenu, player, player.getName());
-			}
-		}
-
-		UserInterface userInterface = getClient().getUserInterface();
-		Map<CardType, List<Card>> cardMap = buildCardMap(pInducementSet);
-		for (CardType type : cardMap.keySet()) {
-			List<Card> cardList = cardMap.get(type);
-			StringBuilder cardTypeText = new StringBuilder();
-			cardTypeText.append(cardList.size()).append(" ");
-			if (cardList.size() > 1) {
-				cardTypeText.append(type.getInducementNameMultiple());
-			} else {
-				cardTypeText.append(type.getInducementNameSingle());
-			}
-			int available = 0;
-			for (Card card : cardList) {
-				if (pInducementSet.isAvailable(card)) {
-					available++;
-				}
-			}
-			cardTypeText.append(" (");
-			cardTypeText.append((available > 0) ? available : "None");
-			cardTypeText.append(" available)");
-			if (pInducementSet.getTurnData().isHomeData() && (getClient().getMode() == ClientMode.PLAYER)) {
-				JMenu cardMenu = new JMenu(dimensionProvider, cardTypeText.toString());
-				pInducementMenu.add(cardMenu);
-				ImageIcon cardIcon = new ImageIcon(
-					userInterface.getIconCache().getIconByProperty(IIconProperty.SIDEBAR_OVERLAY_PLAYER_CARD, dimensionProvider));
-				for (Card card : cardList) {
-					if (pInducementSet.isAvailable(card)) {
-						String cardText = "<html>" +
-							"<b>" + card.getName() + "</b>" +
-							"<br>" + card.getHtmlDescriptionWithPhases() +
-							"</html>";
-						JMenuItem cardItem = new JMenuItem(dimensionProvider, cardText, cardIcon);
-						cardMenu.add(cardItem);
-					}
-				}
-			} else {
-				JMenuItem cardItem = new JMenuItem(dimensionProvider, cardTypeText.toString());
-				pInducementMenu.add(cardItem);
-			}
-		}
-
 	}
 
 	public void updateMissingPlayers() {
@@ -1610,12 +1399,6 @@ public class GameMenuBar extends JMenuBar implements ActionListener, IDialogClos
 		JMenuItem playersMenuItem = new JMenuItem(dimensionProvider, pText, playerIcon);
 		playersMenuItem.addMouseListener(new MenuPlayerMouseListener(pPlayer));
 		pPlayersMenu.add(playersMenuItem);
-	}
-
-	private Map<CardType, List<Card>> buildCardMap(InducementSet pInducementSet) {
-		Card[] allCards = pInducementSet.getAllCards();
-
-		return Arrays.stream(allCards).collect(Collectors.groupingBy(Card::getType));
 	}
 
 	public String menuName(CommonProperty menuProperty) {
---
+++ ffb-client-logic/src/main/java/com/fumbbl/ffb/client/ui/menu/InducementsMenu.java
@@ -0,0 +1,251 @@
+package com.fumbbl.ffb.client.ui.menu;
+
+import com.fumbbl.ffb.ClientMode;
+import com.fumbbl.ffb.IIconProperty;
+import com.fumbbl.ffb.PlayerType;
+import com.fumbbl.ffb.client.DimensionProvider;
+import com.fumbbl.ffb.client.FantasyFootballClient;
+import com.fumbbl.ffb.client.UserInterface;
+import com.fumbbl.ffb.client.ui.swing.JMenu;
+import com.fumbbl.ffb.client.ui.swing.JMenuItem;
+import com.fumbbl.ffb.inducement.Card;
+import com.fumbbl.ffb.inducement.CardType;
+import com.fumbbl.ffb.inducement.Inducement;
+import com.fumbbl.ffb.inducement.Usage;
+import com.fumbbl.ffb.model.Game;
+import com.fumbbl.ffb.model.InducementSet;
+import com.fumbbl.ffb.model.Player;
+import com.fumbbl.ffb.model.Team;
+
+import javax.swing.ImageIcon;
+import java.awt.Color;
+import java.awt.event.ActionEvent;
+import java.awt.event.KeyEvent;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+public class InducementsMenu extends FfbMenu {
+
+	private int fCurrentInducementTotalHome;
+	private int fCurrentUsedCardsHome;
+	private int fCurrentInducementTotalAway;
+	private int fCurrentUsedCardsAway;
+
+	protected InducementsMenu(FantasyFootballClient client, DimensionProvider dimensionProvider) {
+		super("Inducements", client, dimensionProvider);
+		setMnemonic(KeyEvent.VK_I);
+		setEnabled(false);
+	}
+
+	@Override
+	protected void init() {
+		fCurrentInducementTotalHome = -1;
+		fCurrentUsedCardsHome = 0;
+		fCurrentInducementTotalAway = -1;
+		fCurrentUsedCardsAway = 0;
+	}
+
+	@Override
+	protected void refresh() {
+		boolean refreshNecessary = false;
+		Game game = client.getGame();
+
+		InducementSet inducementSetHome = game.getTurnDataHome().getInducementSet();
+		int totalInducementHome = inducementSetHome.totalInducements();
+		if ((fCurrentInducementTotalHome < 0) || (fCurrentInducementTotalHome != totalInducementHome)) {
+			fCurrentInducementTotalHome = totalInducementHome;
+			refreshNecessary = true;
+		}
+		int usedCardsHome = inducementSetHome.getDeactivatedCards().length + inducementSetHome.getActiveCards().length;
+		if (usedCardsHome != fCurrentUsedCardsHome) {
+			fCurrentUsedCardsHome = usedCardsHome;
+			refreshNecessary = true;
+		}
+
+		InducementSet inducementSetAway = game.getTurnDataAway().getInducementSet();
+		int totalInducementAway = inducementSetAway.totalInducements();
+		if ((fCurrentInducementTotalAway < 0) || (fCurrentInducementTotalAway != totalInducementAway)) {
+			fCurrentInducementTotalAway = totalInducementAway;
+			refreshNecessary = true;
+		}
+		int usedCardsAway = inducementSetAway.getDeactivatedCards().length + inducementSetAway.getActiveCards().length;
+		if (usedCardsAway != fCurrentUsedCardsAway) {
+			fCurrentUsedCardsAway = usedCardsAway;
+			refreshNecessary = true;
+		}
+
+		if (refreshNecessary) {
+
+			JMenu fInducementsHomeMenu;
+			JMenu fInducementsAwayMenu;
+			removeAll();
+
+			if (fCurrentInducementTotalHome > 0) {
+				fInducementsHomeMenu = new JMenu(dimensionProvider, totalInducementHome + " Home Team");
+				fInducementsHomeMenu.setForeground(Color.RED);
+				fInducementsHomeMenu.setMnemonic(KeyEvent.VK_H);
+				add(fInducementsHomeMenu);
+				addInducements(fInducementsHomeMenu, inducementSetHome);
+			}
+
+			if (fCurrentInducementTotalAway > 0) {
+				fInducementsAwayMenu = new JMenu(dimensionProvider, totalInducementAway + " Away Team");
+				fInducementsAwayMenu.setForeground(Color.BLUE);
+				fInducementsAwayMenu.setMnemonic(KeyEvent.VK_A);
+				add(fInducementsAwayMenu);
+				addInducements(fInducementsAwayMenu, inducementSetAway);
+			}
+
+			if ((fCurrentInducementTotalHome + fCurrentInducementTotalAway) > 0) {
+				StringBuilder menuText = new StringBuilder().append(fCurrentInducementTotalHome + fCurrentInducementTotalAway);
+				if ((fCurrentInducementTotalHome + fCurrentInducementTotalAway) > 1) {
+					menuText.append(" Inducements");
+				} else {
+					menuText.append(" Inducement");
+				}
+				setText(menuText.toString());
+				setEnabled(true);
+			} else {
+				setText("No Inducements");
+				setEnabled(false);
+			}
+
+		}
+	}
+
+	private void addInducements(JMenu pInducementMenu, InducementSet pInducementSet) {
+		Inducement[] inducements = pInducementSet.getInducements();
+		Arrays.sort(inducements, Comparator.comparing(pInducement -> pInducement.getType().getName()));
+		for (Inducement inducement : inducements) {
+			if (!Usage.EXCLUDE_FROM_RESULT.containsAll(inducement.getType().getUsages())) {
+				if (inducement.getValue() > 0) {
+					StringBuilder inducementText = new StringBuilder();
+					inducementText.append(inducement.getValue()).append(" ");
+					if (inducement.getValue() > 1) {
+						inducementText.append(inducement.getType().getPlural());
+					} else {
+						inducementText.append(inducement.getType().getSingular());
+					}
+					JMenuItem inducementItem = new JMenuItem(dimensionProvider, inducementText.toString());
+					pInducementMenu.add(inducementItem);
+				}
+			}
+		}
+
+		Game game = client.getGame();
+		Team team = pInducementSet.getTurnData().isHomeData() ? game.getTeamHome() : game.getTeamAway();
+		List<Player<?>> starPlayers = new ArrayList<>();
+		for (Player<?> player : team.getPlayers()) {
+			if (player.getPlayerType() == PlayerType.STAR) {
+				starPlayers.add(player);
+			}
+		}
+		if (!starPlayers.isEmpty()) {
+			StringBuilder starPlayerMenuText = new StringBuilder();
+			starPlayerMenuText.append(starPlayers.size());
+			if (starPlayers.size() == 1) {
+				starPlayerMenuText.append(" Star Player");
+			} else {
+				starPlayerMenuText.append(" Star Players");
+			}
+			JMenu starPlayerMenu = new JMenu(dimensionProvider, starPlayerMenuText.toString());
+			pInducementMenu.add(starPlayerMenu);
+			for (Player<?> player : starPlayers) {
+				addPlayerMenuItem(starPlayerMenu, player, player.getName());
+			}
+		}
+
+		List<Player<?>> mercenaries = new ArrayList<>();
+		for (Player<?> player : team.getPlayers()) {
+			if (player.getPlayerType() == PlayerType.MERCENARY) {
+				mercenaries.add(player);
+			}
+		}
+		if (!mercenaries.isEmpty()) {
+			StringBuilder mercenaryMenuText = new StringBuilder();
+			mercenaryMenuText.append(mercenaries.size());
+			if (mercenaries.size() == 1) {
+				mercenaryMenuText.append(" Mercenary");
+			} else {
+				mercenaryMenuText.append(" Mercenaries");
+			}
+			JMenu mercenaryMenu = new JMenu(dimensionProvider, mercenaryMenuText.toString());
+			pInducementMenu.add(mercenaryMenu);
+			for (Player<?> player : mercenaries) {
+				addPlayerMenuItem(mercenaryMenu, player, player.getName());
+			}
+		}
+
+		List<Player<?>> staff = new ArrayList<>();
+		for (Player<?> player : team.getPlayers()) {
+			if (player.getPlayerType() == PlayerType.INFAMOUS_STAFF) {
+				staff.add(player);
+			}
+		}
+		if (!staff.isEmpty()) {
+			String staffText = staff.size() + " Infamous Staff";
+			JMenu staffMenu = new JMenu(dimensionProvider, staffText);
+			pInducementMenu.add(staffMenu);
+			for (Player<?> player : staff) {
+				addPlayerMenuItem(staffMenu, player, player.getName());
+			}
+		}
+
+		UserInterface userInterface = client.getUserInterface();
+		Map<CardType, List<Card>> cardMap = buildCardMap(pInducementSet);
+		for (CardType type : cardMap.keySet()) {
+			List<Card> cardList = cardMap.get(type);
+			StringBuilder cardTypeText = new StringBuilder();
+			cardTypeText.append(cardList.size()).append(" ");
+			if (cardList.size() > 1) {
+				cardTypeText.append(type.getInducementNameMultiple());
+			} else {
+				cardTypeText.append(type.getInducementNameSingle());
+			}
+			int available = 0;
+			for (Card card : cardList) {
+				if (pInducementSet.isAvailable(card)) {
+					available++;
+				}
+			}
+			cardTypeText.append(" (");
+			cardTypeText.append((available > 0) ? available : "None");
+			cardTypeText.append(" available)");
+			if (pInducementSet.getTurnData().isHomeData() && (client.getMode() == ClientMode.PLAYER)) {
+				JMenu cardMenu = new JMenu(dimensionProvider, cardTypeText.toString());
+				pInducementMenu.add(cardMenu);
+				ImageIcon cardIcon = new ImageIcon(
+					userInterface.getIconCache().getIconByProperty(IIconProperty.SIDEBAR_OVERLAY_PLAYER_CARD, dimensionProvider));
+				for (Card card : cardList) {
+					if (pInducementSet.isAvailable(card)) {
+						String cardText = "<html>" +
+							"<b>" + card.getName() + "</b>" +
+							"<br>" + card.getHtmlDescriptionWithPhases() +
+							"</html>";
+						JMenuItem cardItem = new JMenuItem(dimensionProvider, cardText, cardIcon);
+						cardMenu.add(cardItem);
+					}
+				}
+			} else {
+				JMenuItem cardItem = new JMenuItem(dimensionProvider, cardTypeText.toString());
+				pInducementMenu.add(cardItem);
+			}
+		}
+
+	}
+
+	private Map<CardType, List<Card>> buildCardMap(InducementSet pInducementSet) {
+		Card[] allCards = pInducementSet.getAllCards();
+
+		return Arrays.stream(allCards).collect(Collectors.groupingBy(Card::getType));
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+
+	}
+}
