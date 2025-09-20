95f15104e35a815593d165bac118b01272fb4cc4
Christian Linse <christian@butterbrot.org>
2025-09-20 15:43:38 +0200

    Add lookup method

--- ffb-client-logic/src/main/java/com/fumbbl/ffb/client/ui/strategies/click/ClickStrategyRegistry.java
+++ ffb-client-logic/src/main/java/com/fumbbl/ffb/client/ui/strategies/click/ClickStrategyRegistry.java
@@ -4,20 +4,30 @@ import com.fumbbl.ffb.util.Scanner;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Comparator;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class ClickStrategyRegistry {
 	private final List<ClickStrategy> strategies;
+	private final Map<String, ClickStrategy> strategyByKey;
 
 	public ClickStrategyRegistry() {
 		Scanner<ClickStrategy> scanner = new Scanner<>(ClickStrategy.class);
 		List<ClickStrategy> found = new ArrayList<>(scanner.getInstancesImplementing());
 		found.sort(Comparator.comparingInt(ClickStrategy::getOrder));
 		this.strategies = Collections.unmodifiableList(found);
+		this.strategyByKey = new HashMap<>();
+		for (ClickStrategy strategy : found) {
+			strategyByKey.put(strategy.getKey(), strategy);
+		}
 	}
 
 	public List<ClickStrategy> getStrategies() {
 		return strategies;
 	}
-}
 
+	public ClickStrategy getStrategyByKey(String key) {
+		return strategyByKey.get(key);
+	}
+}
