3649aed21eb0400884d3306755b685d43b7f5bdc
Christian Linse <christian@butterbrot.org>
2025-10-19 16:27:09 +0200

    Create debug mode for local automarking server

--- ffb-server/local/fakesite.js
+++ ffb-server/local/fakesite.js
@@ -1,5 +1,9 @@
 const fs = require('fs');
 const http = require('http');
+const {argv} = require('node:process');
+
+const debug = argv.indexOf('-d') > -1
+const basePath = debug ? './debug' : '.';
 
 const port = 22228;
 
@@ -7,7 +11,9 @@ const requestListener = function (req, res) {
 
     try {
         const url = new URL(req.url, 'http://localhost:' + port);
-        let content = fs.readFileSync('.' + url.pathname);
+        let file = basePath + url.pathname;
+        console.log("Serving " + file)
+        let content = fs.readFileSync(file);
         res.writeHead(200);
         res.end(content);
     } catch (e) {
