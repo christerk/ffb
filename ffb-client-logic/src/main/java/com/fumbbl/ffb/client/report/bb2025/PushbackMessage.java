309a6b518f786a2af0249daa350c8c3349eef4ec
Matias Del Bene <matias@delbene.com.ar>
2025-11-03 13:34:34 -0300

    added sidestep skill

---
+++ ffb-client-logic/src/main/java/com/fumbbl/ffb/client/report/bb2025/PushbackMessage.java
@@ -0,0 +1,35 @@
+package com.fumbbl.ffb.client.report.bb2025;
+
+import com.fumbbl.ffb.PushbackMode;
+import com.fumbbl.ffb.RulesCollection;
+import com.fumbbl.ffb.RulesCollection.Rules;
+import com.fumbbl.ffb.model.ActingPlayer;
+import com.fumbbl.ffb.model.Player;
+import com.fumbbl.ffb.report.ReportId;
+import com.fumbbl.ffb.report.ReportPushback;
+import com.fumbbl.ffb.client.report.ReportMessageBase;
+import com.fumbbl.ffb.client.report.ReportMessageType;
+
+@ReportMessageType(ReportId.PUSHBACK)
+@RulesCollection(Rules.BB2025)
+public class PushbackMessage extends ReportMessageBase<ReportPushback> {
+
+    @Override
+    protected void render(ReportPushback report) {
+  		int indent = getIndent() + 1;
+  		StringBuilder status = new StringBuilder();
+  		Player<?> defender = game.getPlayerById(report.getDefenderId());
+  		if (report.getPushbackMode() == PushbackMode.SIDE_STEP) {
+  			print(indent, false, defender);
+  			status.append(" uses Sidestep to avoid being pushed.");
+  			println(indent, status.toString());
+  		}
+  		if (report.getPushbackMode() == PushbackMode.GRAB) {
+  			ActingPlayer actingPlayer = game.getActingPlayer();
+  			print(indent, false, actingPlayer.getPlayer());
+  			status.append(" uses Grab to place ").append(actingPlayer.getPlayer().getPlayerGender().getGenitive())
+  				.append(" opponent.");
+  			println(indent, status.toString());
+  		}
+    }
+}
