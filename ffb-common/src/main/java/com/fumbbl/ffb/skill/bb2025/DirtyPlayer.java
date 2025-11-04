37cb34c4d51648de35586ff373c77e7cf8dbdaed
Matias Del Bene <matias@delbene.com.ar>
2025-11-04 00:21:51 -0300

    Added Dirty Player skill

---
+++ ffb-common/src/main/java/com/fumbbl/ffb/skill/bb2025/DirtyPlayer.java
@@ -0,0 +1,54 @@
+package com.fumbbl.ffb.skill.bb2025;
+
+import java.util.Arrays;
+
+import com.fumbbl.ffb.RulesCollection;
+import com.fumbbl.ffb.SkillCategory;
+import com.fumbbl.ffb.RulesCollection.Rules;
+import com.fumbbl.ffb.model.property.NamedProperties;
+import com.fumbbl.ffb.model.skill.Skill;
+import com.fumbbl.ffb.model.skill.SkillValueEvaluator;
+import com.fumbbl.ffb.modifiers.ArmorModifierContext;
+import com.fumbbl.ffb.modifiers.InjuryModifierContext;
+import com.fumbbl.ffb.modifiers.VariableArmourModifier;
+import com.fumbbl.ffb.modifiers.VariableInjuryModifierAttacker;
+
+/**
+ * When this player performs a Foul Action, they may apply a +1 modifier to 
+ * either the Armour Roll or Injury Roll. This modifier may be applied after 
+ * the roll has been made
+ */
+@RulesCollection(Rules.BB2025)
+public class DirtyPlayer extends Skill {
+
+	public DirtyPlayer() {
+		super("Dirty Player", SkillCategory.DEVIOUS, 1);
+	}
+
+	@Override
+	public void postConstruct() {
+		registerModifier(new VariableArmourModifier("Dirty Player",false) {
+			@Override
+			public boolean appliesToContext(ArmorModifierContext context) {
+				return context.isFoul();
+			}
+		});
+		registerModifier(new VariableInjuryModifierAttacker("Dirty Player",false) {
+			@Override
+			public boolean appliesToContext(InjuryModifierContext context) {
+				return super.appliesToContext(context)
+					&&(context.isFoul()
+					&& Arrays.stream(context.getInjuryContext().getArmorModifiers())
+					.noneMatch(modifier -> modifier.isRegisteredToSkillWithProperty(NamedProperties.affectsEitherArmourOrInjuryOnFoul)));
+			}
+		});
+		registerProperty(NamedProperties.affectsEitherArmourOrInjuryOnFoul);
+
+	}
+
+	@Override
+	public SkillValueEvaluator evaluator() {
+		return SkillValueEvaluator.MODIFIER;
+	}
+
+}
