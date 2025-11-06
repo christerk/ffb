516e344da58f52d3da6fcf5e4d9414f71240f347
Matias Del Bene <matias@delbene.com.ar>
2025-11-05 21:23:20 -0300

    Added Sneaky Git skill and behaviour (BB2025)

---
+++ ffb-common/src/main/java/com/fumbbl/ffb/skill/bb2025/SneakyGit.java
@@ -0,0 +1,27 @@
+package com.fumbbl.ffb.skill.bb2025;
+
+import com.fumbbl.ffb.RulesCollection;
+import com.fumbbl.ffb.SkillCategory;
+import com.fumbbl.ffb.RulesCollection.Rules;
+import com.fumbbl.ffb.model.property.NamedProperties;
+import com.fumbbl.ffb.model.skill.Skill;
+
+/**
+ * This player is not Sent-off when performing a Foul Action if a natural 
+ * double is rolled for the Armour Roll, so long as the target player’s 
+ * Armour is not broken. If the target player’s Armour is broken, this 
+ * player will be sent off as normal.
+ */
+@RulesCollection(Rules.BB2025)
+public class SneakyGit extends Skill {
+
+	public SneakyGit() {
+		super("Sneaky Git", SkillCategory.DEVIOUS);
+	}
+
+	@Override
+	public void postConstruct() {
+		registerProperty(NamedProperties.canAlwaysAssistFouls);
+	}
+
+}
