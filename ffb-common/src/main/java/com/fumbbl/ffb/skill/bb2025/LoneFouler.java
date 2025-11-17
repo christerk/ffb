4e2dd8c5f5565bad02cda124afa0eb515307bb0b
Matias Del Bene <matias@delbene.com.ar>
2025-11-17 13:46:37 -0300

    Added skilllone fouler. Need to check what is the condition to trgiier it still

---
+++ ffb-common/src/main/java/com/fumbbl/ffb/skill/bb2025/LoneFouler.java
@@ -0,0 +1,18 @@
+package com.fumbbl.ffb.skill.bb2025;
+
+import com.fumbbl.ffb.RulesCollection;
+import com.fumbbl.ffb.SkillCategory;
+import com.fumbbl.ffb.model.skill.Skill;
+
+/**
+ * When this player performs a Foul Action, if there are no players providing
+ * an Offensive or Defensive Assist, this player may re-roll a failed Armour Roll.
+ */
+
+@RulesCollection(RulesCollection.Rules.BB2025)
+public class LoneFouler extends Skill {
+
+  public LoneFouler() {
+    super("Lone Fouler", SkillCategory.DEVIOUS);
+  }
+}
