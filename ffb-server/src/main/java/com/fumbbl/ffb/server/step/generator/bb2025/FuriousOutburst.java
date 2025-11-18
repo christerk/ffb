9d60eb4b3f5c951a479a9762ec522656f23ecee1
Christian Linse <christian@butterbrot.org>
2025-11-18 21:07:23 +0100

    Use new init sequence helper in all relevant generators and add steady footing where applicable

--- ffb-server/src/main/java/com/fumbbl/ffb/server/step/generator/bb2025/FuriousOutburst.java
+++ ffb-server/src/main/java/com/fumbbl/ffb/server/step/generator/bb2025/FuriousOutburst.java
@@ -1,4 +1,4 @@
-package com.fumbbl.ffb.server.step.generator.bb2020;
+package com.fumbbl.ffb.server.step.generator.bb2025;
 
 import com.fumbbl.ffb.ApothecaryMode;
 import com.fumbbl.ffb.RulesCollection;
@@ -11,7 +11,7 @@ import com.fumbbl.ffb.server.step.generator.Sequence;
 
 import static com.fumbbl.ffb.server.step.StepParameter.from;
 
-@RulesCollection(RulesCollection.Rules.BB2020)
+@RulesCollection(RulesCollection.Rules.BB2025)
 public class FuriousOutburst extends com.fumbbl.ffb.server.step.generator.FuriousOutburst {
 	@Override
 	public void pushSequence(SequenceParams params) {
@@ -22,20 +22,8 @@ public class FuriousOutburst extends com.fumbbl.ffb.server.step.generator.Furiou
 		Sequence sequence = new Sequence(gameState);
 
 		sequence.add(StepId.INIT_FURIOUS_OUTBURST, from(StepParameterKey.GOTO_LABEL_ON_END, IStepLabel.END));
-		sequence.add(StepId.INIT_ACTIVATION);
-		sequence.add(StepId.ANIMAL_SAVAGERY,
-			from(StepParameterKey.GOTO_LABEL_ON_FAILURE, IStepLabel.END));
-		sequence.add(StepId.HANDLE_DROP_PLAYER_CONTEXT);
-		sequence.add(StepId.PLACE_BALL);
-		sequence.add(StepId.APOTHECARY,
-			from(StepParameterKey.APOTHECARY_MODE, ApothecaryMode.ANIMAL_SAVAGERY));
-		sequence.add(StepId.CATCH_SCATTER_THROW_IN);
-		sequence.add(StepId.GOTO_LABEL, from(StepParameterKey.GOTO_LABEL, IStepLabel.NEXT), from(StepParameterKey.ALTERNATE_GOTO_LABEL, IStepLabel.END));
-		sequence.add(StepId.BONE_HEAD, IStepLabel.NEXT, from(StepParameterKey.GOTO_LABEL_ON_FAILURE, IStepLabel.END));
-		sequence.add(StepId.REALLY_STUPID, from(StepParameterKey.GOTO_LABEL_ON_FAILURE, IStepLabel.END));
-		sequence.add(StepId.TAKE_ROOT);
-		sequence.add(StepId.UNCHANNELLED_FURY, from(StepParameterKey.GOTO_LABEL_ON_FAILURE, IStepLabel.END));
-		sequence.add(StepId.BLOOD_LUST);
+		ActivationSequenceBuilder.create().withFailureLabel(IStepLabel.END).addTo(sequence);
+
 		sequence.add(StepId.FOUL_APPEARANCE, from(StepParameterKey.GOTO_LABEL_ON_FAILURE, IStepLabel.END));
 		sequence.add(StepId.DUMP_OFF);
 		sequence.add(StepId.FIRST_MOVE_FURIOUS_OUTBURST, from(StepParameterKey.GOTO_LABEL_ON_END, IStepLabel.END));
---
+++ ffb-server/src/main/java/com/fumbbl/ffb/server/step/generator/bb2025/ScatterPlayer.java
@@ -0,0 +1,56 @@
+package com.fumbbl.ffb.server.step.generator.bb2025;
+
+import com.fumbbl.ffb.ApothecaryMode;
+import com.fumbbl.ffb.RulesCollection;
+import com.fumbbl.ffb.server.GameState;
+import com.fumbbl.ffb.server.IServerLogLevel;
+import com.fumbbl.ffb.server.step.IStepLabel;
+import com.fumbbl.ffb.server.step.StepId;
+import com.fumbbl.ffb.server.step.StepParameterKey;
+import com.fumbbl.ffb.server.step.generator.Sequence;
+
+import static com.fumbbl.ffb.server.step.StepParameter.from;
+
+@RulesCollection(RulesCollection.Rules.BB2025)
+public class ScatterPlayer extends com.fumbbl.ffb.server.step.generator.ScatterPlayer {
+
+	@Override
+	public void pushSequence(SequenceParams params) {
+		GameState gameState = params.getGameState();
+		gameState.getServer().getDebugLog().log(IServerLogLevel.DEBUG, gameState.getId(),
+			"push scatterPlayerSequence onto stack");
+
+		Sequence sequence = new Sequence(gameState);
+
+		if (params.hasSwoop()) {
+			sequence.add(StepId.SWOOP, from(StepParameterKey.THROWN_PLAYER_ID, params.getThrownPlayerId()),
+				from(StepParameterKey.THROWN_PLAYER_STATE, params.getThrownPlayerState()),
+				from(StepParameterKey.THROWN_PLAYER_HAS_BALL, params.isThrownPlayerHasBall()),
+				from(StepParameterKey.THROWN_PLAYER_COORDINATE, params.getThrownPlayerCoordinate()),
+				from(StepParameterKey.THROW_SCATTER, params.isThrowScatter()),
+				from(StepParameterKey.GOTO_LABEL_ON_FALL_DOWN, IStepLabel.APOTHECARY_HIT_PLAYER));
+		}
+		sequence.add(StepId.INIT_SCATTER_PLAYER, from(StepParameterKey.THROWN_PLAYER_ID, params.getThrownPlayerId()),
+			from(StepParameterKey.THROWN_PLAYER_STATE, params.getThrownPlayerState()),
+			from(StepParameterKey.THROWN_PLAYER_HAS_BALL, params.isThrownPlayerHasBall()),
+			from(StepParameterKey.THROWN_PLAYER_COORDINATE, params.getThrownPlayerCoordinate()),
+			from(StepParameterKey.THROW_SCATTER, params.isThrowScatter()),
+			from(StepParameterKey.PASS_DEVIATES, params.deviates()),
+			from(StepParameterKey.CRASH_LANDING, params.isCrashLanding()));
+
+		sequence.add(StepId.TRAP_DOOR, IStepLabel.SCATTER_BALL);
+		sequence.add(StepId.APOTHECARY, from(StepParameterKey.APOTHECARY_MODE, ApothecaryMode.TRAP_DOOR));
+
+		sequence.add(StepId.STEADY_FOOTING, from(StepParameterKey.APOTHECARY_MODE, ApothecaryMode.HIT_PLAYER),
+			from(StepParameterKey.GOTO_LABEL_ON_SUCCESS, IStepLabel.END));
+		sequence.add(StepId.PLACE_BALL);
+		sequence.add(StepId.APOTHECARY, IStepLabel.APOTHECARY_HIT_PLAYER,
+			from(StepParameterKey.APOTHECARY_MODE, ApothecaryMode.HIT_PLAYER));
+		sequence.add(StepId.CATCH_SCATTER_THROW_IN);
+		sequence.add(StepId.END_SCATTER_PLAYER, IStepLabel.END);
+		// may insert a new scatterPlayerSequence at this point
+
+		gameState.getStepStack().push(sequence.getSequence());
+
+	}
+}
