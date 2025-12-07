33ea2c5a35953d2e9c0284527d20d3d29e5261c3
Christian Linse <christian@butterbrot.org>
2025-12-07 10:34:06 +0100

    Reset changelist

--- ffb-client-logic/src/main/java/com/fumbbl/ffb/client/dialog/DialogChangeList.java
+++ ffb-client-logic/src/main/java/com/fumbbl/ffb/client/dialog/DialogChangeList.java
@@ -63,7 +63,7 @@ public class DialogChangeList extends Dialog {
 			.collect(Collectors.joining());
 
 		String info = "<div style=\"font-size:" + dimensionProvider.scale(11) + "px;padding-bottom:10px\">List of changes in recent versions. Only shows once at startup for each version, can be displayed again from the Help menu.</div>" +
-			"<div style=\"font-size:" + dimensionProvider.scale(11) + "px;padding-bottom:" + dimensionProvider.scale(10) + "px\">Unless stated otherwise the changes apply to 2020 rules.</div>";
+			"<div style=\"font-size:" + dimensionProvider.scale(11) + "px;padding-bottom:" + dimensionProvider.scale(10) + "px\">Unless stated otherwise the changes apply to 2025 rules.</div>";
 
 		contentPane.setText("<div style=\"padding:" + dimensionProvider.scale(10) + "px\"><div>" + info + "</div><div>" + versionsText + "</div></div>");
 		contentPane.setCaretPosition(0);
