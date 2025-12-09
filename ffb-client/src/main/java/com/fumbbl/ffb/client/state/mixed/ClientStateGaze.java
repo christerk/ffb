e81a047cda1ea6f3d2d8f063d5057baba989a302
Matias Del Bene <matias@delbene.com.ar>
2025-12-09 17:04:58 -0300

    Split ClientStateGaze into mixed and 2025

--- ffb-client/src/main/java/com/fumbbl/ffb/client/state/mixed/ClientStateGaze.java
+++ ffb-client/src/main/java/com/fumbbl/ffb/client/state/mixed/ClientStateGaze.java
@@ -1,4 +1,4 @@
-package com.fumbbl.ffb.client.state.common;
+package com.fumbbl.ffb.client.state.mixed;
 
 import com.fumbbl.ffb.FieldCoordinate;
 import com.fumbbl.ffb.IIconProperty;
@@ -13,7 +13,8 @@ import com.fumbbl.ffb.model.ActingPlayer;
 import com.fumbbl.ffb.model.Game;
 import com.fumbbl.ffb.model.Player;
 
-@RulesCollection(RulesCollection.Rules.COMMON)
+@RulesCollection(RulesCollection.Rules.BB2016)
+@RulesCollection(RulesCollection.Rules.BB2020)
 public class ClientStateGaze extends AbstractClientStateMove<GazeLogicModule> {
 
 	public ClientStateGaze(FantasyFootballClientAwt pClient) {
