23f4824cc89165c5de136de4bff728403c453344
Christian Linse <christian@butterbrot.org>
2025-12-23 16:00:29 +0100

    Fix mascot message to use roll values in appropriate places

--- ffb-client-logic/src/main/java/com/fumbbl/ffb/client/report/bb2025/MascotUsedMessage.java
+++ ffb-client-logic/src/main/java/com/fumbbl/ffb/client/report/bb2025/MascotUsedMessage.java
@@ -12,17 +12,20 @@ import com.fumbbl.ffb.report.bb2025.ReportMascotUsed;
 public class MascotUsedMessage extends ReportMessageBase<ReportMascotUsed> {
 	@Override
 	protected void render(ReportMascotUsed report) {
-		println(getIndent(), TextStyle.ROLL, "Mascot Roll [ " + report.getMinimumRoll() + " ]");
+		println(getIndent(), TextStyle.ROLL, "Mascot Roll [ " + report.getRoll() + " ]");
 		printTeamName(game, false, report.getTeamId());
 		StringBuilder builder = new StringBuilder(" used their Team Mascot");
 		if (report.isSuccessful()) {
 			builder.append(" successfully.");
-		} else if (report.isFallback()){
+		} else if (report.isFallback()) {
 			builder.append(" but it failed so they used a regular re-roll instead.");
 		} else {
 			builder.append(" but it failed.");
 		}
 		println(getIndent(), builder.toString());
 
+		if (!report.isSuccessful()) {
+			println(getIndent() + 1, TextStyle.NONE, "(Roll >= " + report.getMinimumRoll() + " to succceed)");
+		}
 	}
 }
