1e1eb28cec2872a034610b877b5c8385fe45212b
Christian Linse <christian@butterbrot.org>
2025-12-30 17:28:51 +0100

    Add H&R to 2025 rules version

--- ffb-common/src/main/java/com/fumbbl/ffb/skill/mixed/HitAndRun.java
+++ ffb-common/src/main/java/com/fumbbl/ffb/skill/mixed/HitAndRun.java
@@ -1,4 +1,4 @@
-package com.fumbbl.ffb.skill.bb2020;
+package com.fumbbl.ffb.skill.mixed;
 
 import com.fumbbl.ffb.RulesCollection;
 import com.fumbbl.ffb.SkillCategory;
@@ -6,6 +6,7 @@ import com.fumbbl.ffb.model.property.NamedProperties;
 import com.fumbbl.ffb.model.skill.Skill;
 
 @RulesCollection(RulesCollection.Rules.BB2020)
+@RulesCollection(RulesCollection.Rules.BB2025)
 public class HitAndRun extends Skill {
 	public HitAndRun() {
 		super("Hit And Run", SkillCategory.TRAIT);
