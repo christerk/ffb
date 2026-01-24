c9596c235158ba7bea7e139528cc8772d0618856
Christian Linse <christian@butterbrot.org>
2026-01-24 14:24:10 +0100

    Move existing blastin logic to legacy plugin

--- ffb-client-logic/src/main/java/com/fumbbl/ffb/client/state/logic/plugin/MoveLogicPlugin.java
+++ ffb-client-logic/src/main/java/com/fumbbl/ffb/client/state/logic/plugin/MoveLogicPlugin.java
@@ -1,4 +1,20 @@
 package com.fumbbl.ffb.client.state.logic.plugin;
 
+import com.fumbbl.ffb.client.net.ClientCommunication;
+import com.fumbbl.ffb.client.state.logic.ClientAction;
+import com.fumbbl.ffb.client.state.logic.MoveLogicModule;
+import com.fumbbl.ffb.client.state.logic.interaction.ActionContext;
+import com.fumbbl.ffb.model.ActingPlayer;
+
+import java.util.Set;
+
 public abstract class MoveLogicPlugin implements LogicPlugin {
+
+	public abstract Set<ClientAction> availableActions();
+
+	public abstract void performAvailableAction(ClientAction action, ActingPlayer actingPlayer,
+		MoveLogicModule logicModule, ClientCommunication communication);
+
+	public abstract ActionContext actionContext(ActingPlayer actingPlayer, ActionContext actionContext,
+		MoveLogicModule logicModule);
 }
--- ffb-client-logic/src/main/java/com/fumbbl/ffb/client/state/logic/plugin/mixed/MoveLogicPlugin.java
+++ ffb-client-logic/src/main/java/com/fumbbl/ffb/client/state/logic/plugin/mixed/MoveLogicPlugin.java
@@ -1,6 +1,16 @@
 package com.fumbbl.ffb.client.state.logic.plugin.mixed;
 
 import com.fumbbl.ffb.RulesCollection;
+import com.fumbbl.ffb.client.net.ClientCommunication;
+import com.fumbbl.ffb.client.state.logic.ClientAction;
+import com.fumbbl.ffb.client.state.logic.MoveLogicModule;
+import com.fumbbl.ffb.client.state.logic.interaction.ActionContext;
+import com.fumbbl.ffb.model.ActingPlayer;
+import com.fumbbl.ffb.model.property.NamedProperties;
+import com.fumbbl.ffb.model.skill.Skill;
+
+import java.util.Collections;
+import java.util.Set;
 
 @RulesCollection(RulesCollection.Rules.BB2020)
 @RulesCollection(RulesCollection.Rules.BB2016)
@@ -9,5 +19,37 @@ public class MoveLogicPlugin extends com.fumbbl.ffb.client.state.logic.plugin.Mo
 	@Override
 	public Type getType() {
 		return Type.MOVE;
+	}
+
+	@Override
+	public Set<ClientAction> availableActions() {
+		return Collections.singleton(ClientAction.THEN_I_STARTED_BLASTIN);
+	}
+
+	@Override
+	public void performAvailableAction(ClientAction action, ActingPlayer actingPlayer,
+		MoveLogicModule logicModule, ClientCommunication communication) {
+		switch (action) {
+			case THEN_I_STARTED_BLASTIN:
+				if (logicModule.isThenIStartedBlastinAvailable(actingPlayer)) {
+					Skill skill = actingPlayer.getPlayer().getSkillWithProperty(NamedProperties.canBlastRemotePlayer);
+					communication.sendUseSkill(skill, true, actingPlayer.getPlayerId());
+				}
+				break;
+			default:
+				break;
+		}
+
+	}
+
+	@Override
+	public ActionContext actionContext(ActingPlayer actingPlayer, ActionContext actionContext,
+		MoveLogicModule logicModule) {
+
+		if (logicModule.isThenIStartedBlastinAvailable(actingPlayer)) {
+			actionContext.add(ClientAction.THEN_I_STARTED_BLASTIN);
+		}
+
+		return actionContext;
 	}
 }
