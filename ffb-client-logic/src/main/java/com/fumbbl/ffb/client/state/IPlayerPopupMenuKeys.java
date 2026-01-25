3339315db2c5a714a91471a11999372f4d0620c7
Christian Linse <christian@butterbrot.org>
2026-01-25 23:49:36 +0100

    Resolve key conflict between raiding party and breathe fire (occurred in generic block mappings, alternative would be to also split these menus analog to select state in 2025)

--- ffb-client-logic/src/main/java/com/fumbbl/ffb/client/state/IPlayerPopupMenuKeys.java
+++ ffb-client-logic/src/main/java/com/fumbbl/ffb/client/state/IPlayerPopupMenuKeys.java
@@ -40,7 +40,7 @@ public interface IPlayerPopupMenuKeys {
 	int KEY_TREACHEROUS = KeyEvent.VK_A;
 	int KEY_WISDOM = KeyEvent.VK_W;
 	int KEY_BEER_BARREL_BASH = KeyEvent.VK_L;
-	int KEY_RAIDING_PARTY = KeyEvent.VK_I;
+	int KEY_RAIDING_PARTY = KeyEvent.VK_2;
 	int KEY_LOOK_INTO_MY_EYES = KeyEvent.VK_Y;
 	int KEY_BALEFUL_HEX = KeyEvent.VK_X;
 	int KEY_HIT_AND_RUN = KeyEvent.VK_H;
