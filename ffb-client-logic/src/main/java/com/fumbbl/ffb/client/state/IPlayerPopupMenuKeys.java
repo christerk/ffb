88ec3a1ffcf22992aed0187a59685a3a0a1f0f8c
Matias Del Bene <matias@delbene.com.ar>
2026-01-27 16:12:47 -0300

    sorted IPlayerPopupMenuKeys by KeyEvent

--- ffb-client-logic/src/main/java/com/fumbbl/ffb/client/state/IPlayerPopupMenuKeys.java
+++ ffb-client-logic/src/main/java/com/fumbbl/ffb/client/state/IPlayerPopupMenuKeys.java
@@ -8,58 +8,84 @@ import java.awt.event.KeyEvent;
  */
 public interface IPlayerPopupMenuKeys {
 
-	int KEY_END_MOVE = KeyEvent.VK_E;
-	int KEY_HAND_OVER = KeyEvent.VK_H;
-	int KEY_BLITZ = KeyEvent.VK_Z;
-	int KEY_BLOCK = KeyEvent.VK_B;
-	int KEY_BOMB = KeyEvent.VK_O;
-	int KEY_MULTIPLE_BLOCK = KeyEvent.VK_U;
-	int KEY_STAB = KeyEvent.VK_Q;
-	int KEY_CHAINSAW = KeyEvent.VK_C;
-	int KEY_MOVE = KeyEvent.VK_M;
-	int KEY_STAND_UP = KeyEvent.VK_S;
-	int KEY_STAND_UP_BLITZ = KeyEvent.VK_W;
-	int KEY_PASS = KeyEvent.VK_P;
-	int KEY_THROW_TEAM_MATE = KeyEvent.VK_T;
-	int KEY_KICK_TEAM_MATE = KeyEvent.VK_K;
-	int KEY_JUMP = KeyEvent.VK_J;
-	int KEY_FOUL = KeyEvent.VK_F;
-	int KEY_RECOVER = KeyEvent.VK_R;
-	int KEY_GAZE = KeyEvent.VK_G;
 	int KEY_GAZE_ZOAT = KeyEvent.VK_A;
-	int KEY_RANGE_GRID = KeyEvent.VK_R;
+	int KEY_TREACHEROUS = KeyEvent.VK_A;
+
+	int KEY_BLOCK = KeyEvent.VK_B;
+
+	int KEY_CHAINSAW = KeyEvent.VK_C;
+	int KEY_KICK_EM_BLOCK = KeyEvent.VK_C;
+
+	int KEY_CATCH_OF_THE_DAY = KeyEvent.VK_D;
+	int KEY_THE_FLASHING_BLADE = KeyEvent.VK_D;
+
+	int KEY_END_MOVE = KeyEvent.VK_E;
+	int KEY_SECURE_THE_BALL = KeyEvent.VK_E;
+
+	int KEY_FORGO = KeyEvent.VK_F;
+	int KEY_FOUL = KeyEvent.VK_F;
+	int KEY_FUMBLEROOSKIE = KeyEvent.VK_F;	
+
+	int KEY_GAZE = KeyEvent.VK_G;
+	
 	int KEY_HAIL_MARY_PASS = KeyEvent.VK_H;
 	int KEY_HAIL_MARY_BOMB = KeyEvent.VK_H;
-	int KEY_INCORPOREAL = KeyEvent.VK_C;
-	int KEY_SHORT = KeyEvent.VK_S;
-	int KEY_LONG = KeyEvent.VK_L;
-	int KEY_FUMBLEROOSKIE = KeyEvent.VK_F;
-	int KEY_PROJECTILE_VOMIT = KeyEvent.VK_V;
-	int KEY_FRENZIED_RUSH = KeyEvent.VK_Q;
-	int KEY_SHOT_TO_NOTHING = KeyEvent.VK_N;
-	int KEY_SHOT_TO_NOTHING_BOMB = KeyEvent.VK_V;
-	int KEY_TREACHEROUS = KeyEvent.VK_A;
-	int KEY_WISDOM = KeyEvent.VK_W;
-	int KEY_BEER_BARREL_BASH = KeyEvent.VK_L;
-	int KEY_RAIDING_PARTY = KeyEvent.VK_I;
-	int KEY_LOOK_INTO_MY_EYES = KeyEvent.VK_Y;
-	int KEY_BALEFUL_HEX = KeyEvent.VK_X;
+	int KEY_HAND_OVER = KeyEvent.VK_H;
 	int KEY_HIT_AND_RUN = KeyEvent.VK_H;
-	int KEY_ALL_YOU_CAN_EAT = KeyEvent.VK_Y;
-	int KEY_KICK_EM_BLOCK = KeyEvent.VK_C;
-	int KEY_KICK_EM_BLITZ = KeyEvent.VK_I;
-	int KEY_GORED_BY_THE_BULL = KeyEvent.VK_O;
-	int KEY_BLACK_INK = KeyEvent.VK_L;
-	int KEY_CATCH_OF_THE_DAY = KeyEvent.VK_D;
-	int KEY_BOUNDING_LEAP = KeyEvent.VK_N;
+
 	int KEY_BREATHE_FIRE = KeyEvent.VK_I;
-	int KEY_THEN_I_STARTED_BLASTIN = KeyEvent.VK_EXCLAMATION_MARK;
-	int KEY_THE_FLASHING_BLADE = KeyEvent.VK_D;
+	int KEY_KICK_EM_BLITZ = KeyEvent.VK_I;
+	int KEY_RAIDING_PARTY = KeyEvent.VK_I;
+
+	int KEY_JUMP = KeyEvent.VK_J;
+
+	int KEY_KICK_TEAM_MATE = KeyEvent.VK_K;
+
+	int KEY_BEER_BARREL_BASH = KeyEvent.VK_L;
+	int KEY_BLACK_INK = KeyEvent.VK_L;
+	int KEY_LONG = KeyEvent.VK_L;
+
+	int KEY_MOVE = KeyEvent.VK_M;
+
+	int KEY_BOUNDING_LEAP = KeyEvent.VK_N;
+	int KEY_SHOT_TO_NOTHING = KeyEvent.VK_N;	
+
+	int KEY_BOMB = KeyEvent.VK_O;
+	int KEY_GORED_BY_THE_BULL = KeyEvent.VK_O;
+
+	int KEY_INCORPOREAL = KeyEvent.VK_P;
+	int KEY_PASS = KeyEvent.VK_P;
+	
+	int KEY_FRENZIED_RUSH = KeyEvent.VK_Q;
+	int KEY_STAB = KeyEvent.VK_Q;
+	
+	int KEY_RANGE_GRID = KeyEvent.VK_R;
+	int KEY_RECOVER = KeyEvent.VK_R;	
+
+	int KEY_SHORT = KeyEvent.VK_S;	
+	int KEY_SLASHING_NAILS = KeyEvent.VK_S;
+	int KEY_STAND_UP = KeyEvent.VK_S;
+
+	int KEY_THROW_TEAM_MATE = KeyEvent.VK_T;
+
+	int KEY_MULTIPLE_BLOCK = KeyEvent.VK_U;
+
+	int KEY_PROJECTILE_VOMIT = KeyEvent.VK_V;
+	int KEY_SHOT_TO_NOTHING_BOMB = KeyEvent.VK_V;
+
+	int KEY_STAND_UP_BLITZ = KeyEvent.VK_W;
+	int KEY_WISDOM = KeyEvent.VK_W;
+
+	int KEY_BALEFUL_HEX = KeyEvent.VK_X;
+
+	int KEY_ALL_YOU_CAN_EAT = KeyEvent.VK_Y;	
+	int KEY_LOOK_INTO_MY_EYES = KeyEvent.VK_Y;
+
+	int KEY_AUTO_GAZE_ZOAT = KeyEvent.VK_Z;
+	int KEY_BLITZ = KeyEvent.VK_Z;	
+		
+	int KEY_THEN_I_STARTED_BLASTIN = KeyEvent.VK_EXCLAMATION_MARK;	
 	int KEY_VICIOUS_VINES = KeyEvent.VK_PLUS;
 	int KEY_FURIOUS_OUTBURST = KeyEvent.VK_ASTERISK;
-	int KEY_MORE_ACTION = KeyEvent.VK_TAB;
-	int KEY_SECURE_THE_BALL = KeyEvent.VK_E;
-	int KEY_SLASHING_NAILS = KeyEvent.VK_S;
-	int KEY_AUTO_GAZE_ZOAT = KeyEvent.VK_Z;
-	int KEY_FORGO = KeyEvent.VK_F;
+	int KEY_MORE_ACTION = KeyEvent.VK_TAB;	
 }
