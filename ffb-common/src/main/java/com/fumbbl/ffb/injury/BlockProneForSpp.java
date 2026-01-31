8f52a9da5690506b8bebe8e7d94555d7e3a09261
Matias Del Bene <matias@delbene.com.ar>
2026-01-31 19:17:53 -0300

    add support for violent innovator and ball and chain

---
+++ ffb-common/src/main/java/com/fumbbl/ffb/injury/BlockProneForSpp.java
@@ -0,0 +1,16 @@
+package com.fumbbl.ffb.injury;
+
+import com.fumbbl.ffb.SendToBoxReason;
+
+public class BlockProneForSpp extends InjuryType {
+
+	public BlockProneForSpp() {
+		super("blockProneForSpp", true, SendToBoxReason.BLOCKED);
+	}
+
+	@Override
+	public boolean isCausedByOpponent() {
+		return true;
+	}
+
+}
