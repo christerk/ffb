70a224d2fa7199887d9e3df3ebd18ed049ffc2c3
Christian Linse <christian@butterbrot.org>
2026-02-02 22:46:00 +0100

    Add chomp cursor

--- ffb-client/src/main/java/com/fumbbl/ffb/client/state/bb2025/ClientStateBlock.java
+++ ffb-client/src/main/java/com/fumbbl/ffb/client/state/bb2025/ClientStateBlock.java
@@ -2,6 +2,7 @@ package com.fumbbl.ffb.client.state.bb2025;
 
 import com.fumbbl.ffb.FieldCoordinate;
 import com.fumbbl.ffb.IIconProperty;
+import com.fumbbl.ffb.PlayerAction;
 import com.fumbbl.ffb.RulesCollection;
 import com.fumbbl.ffb.client.ActionKey;
 import com.fumbbl.ffb.client.FantasyFootballClientAwt;
@@ -129,5 +130,13 @@ public class ClientStateBlock extends AbstractClientStateBlock<BlockLogicModule>
 
     return itemConfigs;
 
+  }
+
+  @Override
+  protected String validCursor() {
+    if (getClient().getGame().getActingPlayer().getPlayerAction() == PlayerAction.CHOMP) {
+      return IIconProperty.CURSOR_CHOMP;
+    }
+    return super.validCursor();
   }
 }
