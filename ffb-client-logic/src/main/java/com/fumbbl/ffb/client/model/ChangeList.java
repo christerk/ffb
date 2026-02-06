8a092636e38607c7e7f7eb3c5a56ff432e6cd2df
Matias Del Bene <matias@delbene.com.ar>
2026-02-06 17:53:49 -0300

    added mesage for testers

--- ffb-client-logic/src/main/java/com/fumbbl/ffb/client/model/ChangeList.java
+++ ffb-client-logic/src/main/java/com/fumbbl/ffb/client/model/ChangeList.java
@@ -15,6 +15,7 @@ public class ChangeList {
 			.addFeature("Swift Brothers")
 			.addBugfix("One team's mascot was offered for opposing team on steady footing and other rolls")
 			.addBugfix("Catching Kick-Off still caused -1 modifier")
+			.addBugfix("Tentacles only works on dodge and jump/leap")
 		);
 
 		versions.add(new VersionChangeList("2026-02-01")
