15d82cc7eddcc4fad4a3905dc5273a9f2a607309
Matias Del Bene <matias@delbene.com.ar>
2026-02-06 14:12:53 -0300

    moved logic from interpreter to mechanic. Added corner throw ins for 2025

---
+++ ffb-common/src/main/java/com/fumbbl/ffb/mechanics/bb2020/ThrowInMechanic.java
@@ -0,0 +1,43 @@
+package com.fumbbl.ffb.mechanics.bb2020;
+
+import com.fumbbl.ffb.Direction;
+import com.fumbbl.ffb.FieldCoordinate;
+import com.fumbbl.ffb.RulesCollection;
+
+@RulesCollection(RulesCollection.Rules.BB2020)
+public class ThrowInMechanic extends com.fumbbl.ffb.mechanics.ThrowInMechanic {
+  
+  @Override
+	public int distance(int[] distanceRoll) {
+		return distanceRoll[0] + distanceRoll[1] + 1;
+	}
+
+  @Override
+  public boolean isCornerThrowIn(FieldCoordinate pStartCoordinate) {
+    return false;
+  }
+
+  @Override
+  public Direction interpretThrowInDirectionRoll(FieldCoordinate pStartCoordinate, int roll,
+    boolean isCornerThrowIn) {
+    // Endzone Home Team
+    if (pStartCoordinate.getX() < 1) {
+      return interpretThrowInDirectionRoll(Direction.EAST, roll);
+    }
+    // Endzone Away Team
+    if (pStartCoordinate.getX() > 24) {
+      return interpretThrowInDirectionRoll(Direction.WEST, roll);
+    }
+    // Lower Sideline
+    if (pStartCoordinate.getY() > 13) {
+      return interpretThrowInDirectionRoll(Direction.NORTH, roll);
+    }
+    // Upper Sideline
+    if (pStartCoordinate.getY() < 1) {
+      return interpretThrowInDirectionRoll(Direction.SOUTH, roll);
+    }
+
+		throw new IllegalStateException("Unable to determine throwInDirection.");
+  }
+  
+}
