733257b292d43a156d7a8a5d1d2192f4a568ec41
Christian Linse <christian@butterbrot.org>
2026-02-07 21:10:02 +0100

    Create init step

---
+++ ffb-server/src/main/java/com/fumbbl/ffb/server/step/bb2025/punt/StepInitPunt.java
@@ -0,0 +1,183 @@
+package com.fumbbl.ffb.server.step.bb2025.punt;
+
+import com.eclipsesource.json.JsonObject;
+import com.eclipsesource.json.JsonValue;
+import com.fumbbl.ffb.FieldCoordinate;
+import com.fumbbl.ffb.FieldCoordinateBounds;
+import com.fumbbl.ffb.MoveSquare;
+import com.fumbbl.ffb.PlayerAction;
+import com.fumbbl.ffb.RulesCollection;
+import com.fumbbl.ffb.factory.IFactorySource;
+import com.fumbbl.ffb.json.UtilJson;
+import com.fumbbl.ffb.model.ActingPlayer;
+import com.fumbbl.ffb.model.FieldModel;
+import com.fumbbl.ffb.model.Game;
+import com.fumbbl.ffb.model.Player;
+import com.fumbbl.ffb.model.property.NamedProperties;
+import com.fumbbl.ffb.model.skill.Skill;
+import com.fumbbl.ffb.net.commands.ClientCommandActingPlayer;
+import com.fumbbl.ffb.net.commands.ClientCommandFieldCoordinate;
+import com.fumbbl.ffb.server.GameState;
+import com.fumbbl.ffb.server.IServerJsonOption;
+import com.fumbbl.ffb.server.net.ReceivedCommand;
+import com.fumbbl.ffb.server.step.AbstractStep;
+import com.fumbbl.ffb.server.step.StepAction;
+import com.fumbbl.ffb.server.step.StepCommandStatus;
+import com.fumbbl.ffb.server.step.StepId;
+import com.fumbbl.ffb.server.step.StepParameter;
+import com.fumbbl.ffb.server.step.StepParameterKey;
+import com.fumbbl.ffb.server.step.StepParameterSet;
+import com.fumbbl.ffb.server.step.UtilServerSteps;
+import com.fumbbl.ffb.util.StringTool;
+import com.fumbbl.ffb.util.UtilCards;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+@RulesCollection(RulesCollection.Rules.BB2025)
+public class StepInitPunt extends AbstractStep {
+
+	private boolean endPlayerAction, endTurn;
+	private String goToLabelOnEnd;
+	private FieldCoordinate coordinate;
+
+	public StepInitPunt(GameState pGameState) {
+		super(pGameState);
+	}
+
+	@Override
+	public StepId getId() {
+		return StepId.INIT_PUNT;
+	}
+
+	@Override
+	public void init(StepParameterSet pParameterSet) {
+		super.init(pParameterSet);
+		if (pParameterSet != null) {
+			Arrays.stream(pParameterSet.values()).forEach(parameter -> {
+				if (parameter.getKey() == StepParameterKey.GOTO_LABEL_ON_END) {
+					goToLabelOnEnd = (String) parameter.getValue();
+				}
+			});
+		}
+	}
+
+	@Override
+	public StepCommandStatus handleCommand(ReceivedCommand pReceivedCommand) {
+		StepCommandStatus commandStatus = super.handleCommand(pReceivedCommand);
+		if (commandStatus == StepCommandStatus.UNHANDLED_COMMAND) {
+			switch (pReceivedCommand.getId()) {
+				case CLIENT_END_TURN:
+					if (UtilServerSteps.checkCommandIsFromCurrentPlayer(getGameState(), pReceivedCommand)) {
+						endTurn = true;
+						commandStatus = StepCommandStatus.EXECUTE_STEP;
+					}
+					break;
+				case CLIENT_ACTING_PLAYER:
+					ClientCommandActingPlayer actingPlayerCommand = (ClientCommandActingPlayer) pReceivedCommand.getCommand();
+					if (StringTool.isProvided(actingPlayerCommand.getPlayerId())) {
+						UtilServerSteps.changePlayerAction(this, actingPlayerCommand.getPlayerId(),
+							actingPlayerCommand.getPlayerAction(), actingPlayerCommand.isJumping());
+					} else {
+						endPlayerAction = true;
+					}
+					commandStatus = StepCommandStatus.EXECUTE_STEP;
+					break;
+				case CLIENT_FIELD_COORDINATE:
+					ClientCommandFieldCoordinate commandFieldCoordinate = (ClientCommandFieldCoordinate) pReceivedCommand.getCommand();
+					coordinate = commandFieldCoordinate.getFieldCoordinate();
+					commandStatus = StepCommandStatus.EXECUTE_STEP;
+					break;
+				default:
+					break;
+			}
+		}
+
+		if (commandStatus == StepCommandStatus.EXECUTE_STEP) {
+			executeStep();
+		}
+		return commandStatus;
+	}
+
+
+	@Override
+	public void start() {
+		super.start();
+		executeStep();
+	}
+
+	private void executeStep() {
+
+		Game game = getGameState().getGame();
+		FieldModel fieldModel = game.getFieldModel();
+		ActingPlayer actingPlayer = game.getActingPlayer();
+		Skill skill = UtilCards.getUnusedSkillWithProperty(actingPlayer, NamedProperties.canPunt);
+		if (endTurn) {
+			publishParameter(new StepParameter(StepParameterKey.END_TURN, true));
+			publishParameter(StepParameter.from(StepParameterKey.CHECK_FORGO, true));
+			getResult().setNextAction(StepAction.GOTO_LABEL, goToLabelOnEnd);
+		} else if (endPlayerAction) {
+			publishParameter(new StepParameter(StepParameterKey.END_PLAYER_ACTION, true));
+			getResult().setNextAction(StepAction.GOTO_LABEL, goToLabelOnEnd);
+		} else if (actingPlayer.getPlayerAction() == PlayerAction.PUNT && skill != null) {
+			if (coordinate != null) {
+				game.getTurnData().setPuntUsed(true);
+				publishParameter(new StepParameter(StepParameterKey.COORDINATE_TO, coordinate));
+				fieldModel.clearMoveSquares();
+				getResult().setNextAction(StepAction.NEXT_STEP);
+			} else {
+				findPuntSquares(fieldModel, actingPlayer.getPlayer()).stream()
+					.map(coord -> new MoveSquare(coord, 0, 0))
+					.forEach(fieldModel::add);
+				getResult().setNextAction(StepAction.CONTINUE);
+			}
+		}
+		getResult().setNextAction(StepAction.GOTO_LABEL, goToLabelOnEnd);
+	}
+
+	private Set<FieldCoordinate> findPuntSquares(FieldModel fieldModel, Player<?> player) {
+		FieldCoordinate playerCoordinate = fieldModel.getPlayerCoordinate(player);
+		List<Integer> deltas = new ArrayList<Integer>() {{
+			add(1);
+			add(-1);
+		}};
+
+		Set<FieldCoordinate> coordinates = new HashSet<>();
+
+		for (int delta : deltas) {
+			FieldCoordinate deltaX = playerCoordinate.add(delta, 0);
+			if (FieldCoordinateBounds.FIELD.isInBounds(deltaX)) {
+				coordinates.add(deltaX);
+			}
+			FieldCoordinate deltaY = playerCoordinate.add(0, delta);
+			if (FieldCoordinateBounds.FIELD.isInBounds(deltaY)) {
+				coordinates.add(deltaY);
+			}
+		}
+		return coordinates;
+	}
+
+	@Override
+	public JsonObject toJsonValue() {
+		JsonObject jsonObject = super.toJsonValue();
+		IServerJsonOption.END_TURN.addTo(jsonObject, endTurn);
+		IServerJsonOption.END_PLAYER_ACTION.addTo(jsonObject, endPlayerAction);
+		IServerJsonOption.GOTO_LABEL_ON_END.addTo(jsonObject, goToLabelOnEnd);
+		IServerJsonOption.COORDINATE_TO.addTo(jsonObject, coordinate);
+		return jsonObject;
+	}
+
+	@Override
+	public AbstractStep initFrom(IFactorySource source, JsonValue jsonValue) {
+		super.initFrom(source, jsonValue);
+		JsonObject jsonObject = UtilJson.toJsonObject(jsonValue);
+		endPlayerAction = IServerJsonOption.END_PLAYER_ACTION.getFrom(source, jsonObject);
+		endTurn = IServerJsonOption.END_TURN.getFrom(source, jsonObject);
+		goToLabelOnEnd = IServerJsonOption.GOTO_LABEL_ON_END.getFrom(source, jsonObject);
+		coordinate = IServerJsonOption.COORDINATE_TO.getFrom(source, jsonObject);
+		return this;
+	}
+}
--- ffb-server/src/main/java/com/fumbbl/ffb/server/step/generator/bb2025/Punt.java
+++ ffb-server/src/main/java/com/fumbbl/ffb/server/step/generator/bb2025/Punt.java
@@ -5,7 +5,10 @@ import com.fumbbl.ffb.server.GameState;
 import com.fumbbl.ffb.server.IServerLogLevel;
 import com.fumbbl.ffb.server.step.IStepLabel;
 import com.fumbbl.ffb.server.step.StepId;
+import com.fumbbl.ffb.server.step.StepParameterKey;
 import com.fumbbl.ffb.server.step.generator.Sequence;
+
+import static com.fumbbl.ffb.server.step.StepParameter.from;
 
 @RulesCollection(RulesCollection.Rules.BB2025)
 public class Punt extends com.fumbbl.ffb.server.step.generator.Punt {
@@ -17,11 +20,11 @@ public class Punt extends com.fumbbl.ffb.server.step.generator.Punt {
 
 		Sequence sequence = new Sequence(gameState);
 
-		sequence.add(StepId.INIT_PUNT);
+		sequence.add(StepId.INIT_PUNT, from(StepParameterKey.GOTO_LABEL_ON_END, IStepLabel.END));
 		ActivationSequenceBuilder.create().withFailureLabel(IStepLabel.END).addTo(sequence);
 		sequence.add(StepId.PUNT);
 		sequence.add(StepId.CATCH_SCATTER_THROW_IN);
-		sequence.add(StepId.END_PUNT);
+		sequence.add(StepId.END_PUNT, IStepLabel.END);
 
 		gameState.getStepStack().push(sequence.getSequence());
 	}
