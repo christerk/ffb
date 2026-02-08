30cdbfb2accb21118983f1ea592d3968a94ffadb
Christian Linse <christian@butterbrot.org>
2026-02-08 19:48:53 +0100

    Polish end punt step

--- ffb-server/src/main/java/com/fumbbl/ffb/server/step/bb2025/punt/StepEndPunt.java
+++ ffb-server/src/main/java/com/fumbbl/ffb/server/step/bb2025/punt/StepEndPunt.java
@@ -6,7 +6,6 @@ import com.fumbbl.ffb.FactoryType;
 import com.fumbbl.ffb.RulesCollection;
 import com.fumbbl.ffb.factory.IFactorySource;
 import com.fumbbl.ffb.json.UtilJson;
-import com.fumbbl.ffb.model.ActingPlayer;
 import com.fumbbl.ffb.model.Game;
 import com.fumbbl.ffb.model.Player;
 import com.fumbbl.ffb.server.GameState;
@@ -18,7 +17,6 @@ import com.fumbbl.ffb.server.step.StepId;
 import com.fumbbl.ffb.server.step.StepParameter;
 import com.fumbbl.ffb.server.step.UtilServerSteps;
 import com.fumbbl.ffb.server.step.generator.EndPlayerAction;
-import com.fumbbl.ffb.server.step.generator.Move;
 import com.fumbbl.ffb.server.step.generator.SequenceGenerator;
 import com.fumbbl.ffb.server.util.UtilServerDialog;
 import com.fumbbl.ffb.util.StringTool;
@@ -70,25 +68,19 @@ public final class StepEndPunt extends AbstractStep {
 	private void executeStep() {
 		UtilServerDialog.hideDialog(getGameState());
 		Game game = getGameState().getGame();
-		game.getFieldModel().setRangeRuler(null);
-		game.getFieldModel().setOutOfBounds(false);
-		ActingPlayer actingPlayer = game.getActingPlayer();
+
 		SequenceGeneratorFactory factory = game.getFactory(FactoryType.Factory.SEQUENCE_GENERATOR);
 		EndPlayerAction endGenerator = ((EndPlayerAction) factory.forName(SequenceGenerator.Type.EndPlayerAction.name()));
-		Move moveGenerator = (Move) factory.forName(SequenceGenerator.Type.Move.name());
 
-
-		Player<?> catcher = game.getPlayerById(catcherId);
-
+		Player<?> catcher;
 		boolean ballWasSnatched = StringTool.isProvided(ballSnatcherId);
 		if (ballWasSnatched) {
 			catcher = game.getPlayerById(catcherId);
 		} else {
 			catcher = game.getFieldModel().getPlayer(game.getFieldModel().getBallCoordinate());
 		}
-		//TODO check if we need the other conditions
-		endTurn |= (UtilServerSteps.checkTouchdown(getGameState()) /*|| (catcher == null) ||
-				UtilPlayer.findOtherTeam(game, game.getThrower()).hasPlayer(catcher)*/);
+
+		endTurn |= (UtilServerSteps.checkTouchdown(getGameState()) || (catcher == null));
 		endGenerator.pushSequence(new EndPlayerAction.SequenceParams(getGameState(), true, true, endTurn));
 
 		getResult().setNextAction(StepAction.NEXT_STEP);
