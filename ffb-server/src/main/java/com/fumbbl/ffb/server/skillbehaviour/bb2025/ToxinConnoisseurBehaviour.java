dc78bc0a23719c456841b977aa06c17d77bc5a66
Matias Del Bene <matias@delbene.com.ar>
2026-02-09 23:54:09 -0300

    toxin

--- ffb-server/src/main/java/com/fumbbl/ffb/server/skillbehaviour/bb2025/ToxinConnoisseurBehaviour.java
+++ ffb-server/src/main/java/com/fumbbl/ffb/server/skillbehaviour/bb2025/ToxinConnoisseurBehaviour.java
@@ -1,11 +1,10 @@
-package com.fumbbl.ffb.server.skillbehaviour.mixed;
+package com.fumbbl.ffb.server.skillbehaviour.bb2025;
 
 import com.fumbbl.ffb.RulesCollection;
-import com.fumbbl.ffb.server.injury.modification.ToxinConnoisseurModification;
+import com.fumbbl.ffb.server.injury.modification.bb2025.ToxinConnoisseurModification;
 import com.fumbbl.ffb.server.model.SkillBehaviour;
 import com.fumbbl.ffb.skill.mixed.special.ToxinConnoisseur;
 
-@RulesCollection(RulesCollection.Rules.BB2020)
 @RulesCollection(RulesCollection.Rules.BB2025)
 public class ToxinConnoisseurBehaviour extends SkillBehaviour<ToxinConnoisseur> {
 
