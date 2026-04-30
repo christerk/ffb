0e3b02b34366418fe2ceb9c2195dd735dcd59270
Matias Del Bene <matias@delbene.com.ar>
2026-04-30 15:23:19 -0300

    added changelist entry

--- ffb-client-logic/src/main/java/com/fumbbl/ffb/client/model/ChangeList.java
+++ ffb-client-logic/src/main/java/com/fumbbl/ffb/client/model/ChangeList.java
@@ -24,6 +24,7 @@ public class ChangeList {
 			.addBugfix("Gaining additional Hatred results in duplication of existing Hatred skill listings")
 			.addBugfix("Bloodlust: When opting to move instead of fouling directly due to failed Bloodlust the game crashed")
 			.addBugfix("Missing Zoat and Spite keywords caused Hatred/Getting Even to show Unknown")
+			.addFeature("BB2025: Added support for I'll Carry You (Stars Grak & Crumbleberry)")
 		);
 
 		versions.add(new VersionChangeList("3.2.2")
