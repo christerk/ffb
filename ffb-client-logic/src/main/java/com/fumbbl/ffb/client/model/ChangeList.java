4f7a65c3bef3421dcf1578bf442635d65ed87a64
Matias Del Bene <matias@delbene.com.ar>
2026-04-30 21:47:40 -0300

    add changelist entry

--- ffb-client-logic/src/main/java/com/fumbbl/ffb/client/model/ChangeList.java
+++ ffb-client-logic/src/main/java/com/fumbbl/ffb/client/model/ChangeList.java
@@ -24,6 +24,7 @@ public class ChangeList {
 			.addBugfix("Gaining additional Hatred results in duplication of existing Hatred skill listings")
 			.addBugfix("Bloodlust: When opting to move instead of fouling directly due to failed Bloodlust the game crashed")
 			.addBugfix("Missing Zoat and Spite keywords caused Hatred/Getting Even to show Unknown")
+			.addFeature("Optional client-side captions for spectator-triggered sounds")
 		);
 
 		versions.add(new VersionChangeList("3.2.2")
