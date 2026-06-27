cfc8aa825073b8226c2a75f80300cf61bdf16fb3
Matias Del Bene <matias@delbene.com.ar>
2026-06-27 17:53:24 -0300

    Make failed dodge injuries modification-aware

--- ffb-server/src/main/java/com/fumbbl/ffb/server/injury/modification/bb2025/DwarfenGritModification.java
+++ ffb-server/src/main/java/com/fumbbl/ffb/server/injury/modification/bb2025/DwarfenGritModification.java
@@ -1,20 +1,27 @@
 package com.fumbbl.ffb.server.injury.modification.bb2025;
 
 import com.fumbbl.ffb.injury.Block;
+import com.fumbbl.ffb.injury.DropDodge;
+import com.fumbbl.ffb.injury.DropDodgeForSpp;
 import com.fumbbl.ffb.injury.InjuryType;
 import com.fumbbl.ffb.server.injury.modification.ModificationParams;
 
-import java.util.Collections;
+import java.util.HashSet;
 
 public class DwarfenGritModification extends RerollArmourModification {
 
 	public DwarfenGritModification() {
-		super(Collections.<Class<? extends InjuryType>>singleton(Block.class));
+		super(new HashSet<Class<? extends InjuryType>>() {{
+			add(Block.class);
+			add(DropDodge.class);
+			add(DropDodgeForSpp.class);
+		}});
 	}
 
 	@Override
 	protected boolean tryArmourRollModification(ModificationParams params) {
-		return params.getNewContext().isArmorBroken();
+		return params.getNewContext().getArmorRoll() != null
+			&& params.getNewContext().isArmorBroken();
 	}
 
 	@Override
