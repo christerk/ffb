c4f069a348c5a7e5a81e74734be1cc3f83784a0a
Matias Del Bene <matias@delbene.com.ar>
2026-06-28 12:20:47 -0300

    Allow Dwarfen Grit to check all armour rolls

--- ffb-server/src/main/java/com/fumbbl/ffb/server/injury/modification/bb2025/DwarfenGritModification.java
+++ ffb-server/src/main/java/com/fumbbl/ffb/server/injury/modification/bb2025/DwarfenGritModification.java
@@ -1,21 +1,12 @@
 package com.fumbbl.ffb.server.injury.modification.bb2025;
 
-import com.fumbbl.ffb.injury.Block;
-import com.fumbbl.ffb.injury.DropDodge;
-import com.fumbbl.ffb.injury.DropDodgeForSpp;
-import com.fumbbl.ffb.injury.InjuryType;
 import com.fumbbl.ffb.server.injury.modification.ModificationParams;
 
-import java.util.HashSet;
 
 public class DwarfenGritModification extends RerollArmourModification {
 
 	public DwarfenGritModification() {
-		super(new HashSet<Class<? extends InjuryType>>() {{
-			add(Block.class);
-			add(DropDodge.class);
-			add(DropDodgeForSpp.class);
-		}});
+		super(true); // no injury type restriction
 	}
 
 	@Override
