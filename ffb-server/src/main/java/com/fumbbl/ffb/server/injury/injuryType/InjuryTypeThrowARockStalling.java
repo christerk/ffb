79a86219654f61dcb3538948de45c2e8967a5b2c
Matias Del Bene <matias@delbene.com.ar>
2026-06-28 17:24:52 -0300

    Make stalling rock injuries modification-aware

--- ffb-server/src/main/java/com/fumbbl/ffb/server/injury/injuryType/InjuryTypeThrowARockStalling.java
+++ ffb-server/src/main/java/com/fumbbl/ffb/server/injury/injuryType/InjuryTypeThrowARockStalling.java
@@ -1,9 +1,7 @@
 package com.fumbbl.ffb.server.injury.injuryType;
 
-import com.fumbbl.ffb.ApothecaryMode;
 import com.fumbbl.ffb.FactoryType;
 import com.fumbbl.ffb.FieldCoordinate;
-import com.fumbbl.ffb.PlayerState;
 import com.fumbbl.ffb.factory.InjuryModifierFactory;
 import com.fumbbl.ffb.injury.ThrowARock;
 import com.fumbbl.ffb.injury.context.InjuryContext;
@@ -12,36 +10,35 @@ import com.fumbbl.ffb.model.Player;
 import com.fumbbl.ffb.server.DiceInterpreter;
 import com.fumbbl.ffb.server.DiceRoller;
 import com.fumbbl.ffb.server.GameState;
-import com.fumbbl.ffb.server.step.IStep;
 
-public class InjuryTypeThrowARockStalling extends InjuryTypeServer<ThrowARock> {
+public class InjuryTypeThrowARockStalling extends ModificationAwareInjuryTypeServer<ThrowARock> {
 	public InjuryTypeThrowARockStalling() {
 		super(new ThrowARock());
 	}
 
 	@Override
-	public void handleInjury(IStep step, Game game, GameState gameState, DiceRoller diceRoller,
-	                         Player<?> pAttacker, Player<?> pDefender, FieldCoordinate pDefenderCoordinate, FieldCoordinate fromCoordinate, InjuryContext pOldInjuryContext,
-	                         ApothecaryMode pApothecaryMode) {
-
-		DiceInterpreter diceInterpreter = DiceInterpreter.getInstance();
+	protected void armourRoll(Game game, GameState gameState, DiceRoller diceRoller, Player<?> pAttacker,
+		Player<?> pDefender, FieldCoordinate pDefenderCoordinate, FieldCoordinate fromCoordinate,
+		DiceInterpreter diceInterpreter, InjuryContext injuryContext, boolean roll) {
 
 		if (!injuryContext.isArmorBroken()) {
-			injuryContext.setArmorRoll(diceRoller.rollArmour());
+			if (roll) {
+				injuryContext.setArmorRoll(diceRoller.rollArmour());
+			}
 			injuryContext.setArmorBroken(diceInterpreter.isArmourBroken(gameState, injuryContext));
 		}
+	}
 
-		if (injuryContext.isArmorBroken()) {
-			injuryContext.setInjuryRoll(diceRoller.rollInjury());
-			InjuryModifierFactory factory = game.getFactory(FactoryType.Factory.INJURY_MODIFIER);
-			factory.findInjuryModifiers(game, injuryContext, pAttacker,
-				pDefender, isStab(), isFoul(), isVomitLike()).forEach(injuryModifier -> injuryContext.addInjuryModifier(injuryModifier));
+	@Override
+	protected void injuryRoll(Game game, GameState gameState, DiceRoller diceRoller, Player<?> pAttacker,
+		Player<?> pDefender, FieldCoordinate pDefenderCoordinate, FieldCoordinate fromCoordinate,
+		InjuryContext injuryContext) {
 
-			setInjury(pDefender, gameState, diceRoller);
+		injuryContext.setInjuryRoll(diceRoller.rollInjury());
+		InjuryModifierFactory factory = game.getFactory(FactoryType.Factory.INJURY_MODIFIER);
+		factory.findInjuryModifiers(game, injuryContext, pAttacker,
+			pDefender, isStab(), isFoul(), isVomitLike()).forEach(injuryModifier -> injuryContext.addInjuryModifier(injuryModifier));
 
-		} else {
-			injuryContext.setInjury(new PlayerState(PlayerState.PRONE));
-		}
-
+		setInjury(pDefender, gameState, diceRoller, injuryContext);
 	}
 }
