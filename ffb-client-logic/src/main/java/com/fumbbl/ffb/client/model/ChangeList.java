e07686e1536d062d69aaf8ec021598f8c6874ee2
Christian Linse <christian@butterbrot.org>
2026-07-08 14:24:56 +0200

    Add bugfix to changelist

--- ffb-client-logic/src/main/java/com/fumbbl/ffb/client/model/ChangeList.java
+++ ffb-client-logic/src/main/java/com/fumbbl/ffb/client/model/ChangeList.java
@@ -31,6 +31,7 @@ public class ChangeList {
 			.addImprovement("Added chat command to reset used skills for selected players")
 			.addFeature("Added support for I'll Carry You (Stars Grak & Crumbleberry)")
 			.addBugfix("Special team re-rolls like Leader or Brilliant Coaching that were saved by Team Captain got converted to regular team re-rolls")
+			.addBugfix("Mascot/Loner fail on DT dodge re-roll caused the game to lock up")
 		);
 
 		versions.add(new VersionChangeList("3.2.3")
