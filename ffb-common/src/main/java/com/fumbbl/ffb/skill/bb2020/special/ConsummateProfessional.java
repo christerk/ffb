a7e157805d72d45b562d6f4f2aedaa63dd8bda14
Christian Linse <christian@butterbrot.org>
2026-09-06 16:34:20 +0200

    Add skill skeleton

--- ffb-common/src/main/java/com/fumbbl/ffb/skill/bb2020/special/ConsummateProfessional.java
+++ ffb-common/src/main/java/com/fumbbl/ffb/skill/bb2020/special/ConsummateProfessional.java
@@ -1,18 +1,14 @@
 package com.fumbbl.ffb.skill.bb2020.special;
 
-import com.fumbbl.ffb.ReRollSources;
-import com.fumbbl.ffb.ReRolledActions;
 import com.fumbbl.ffb.RulesCollection;
 import com.fumbbl.ffb.RulesCollection.Rules;
 import com.fumbbl.ffb.SkillCategory;
-import com.fumbbl.ffb.model.property.NamedProperties;
 import com.fumbbl.ffb.model.skill.Skill;
 import com.fumbbl.ffb.model.skill.SkillUsageType;
 
 /**
- * Once per game, Griff may re-roll one die that was rolled either as a single dice roll,
- * as port of a multiple dice roll or as part of a dice pool (this cannot be a dice that was rolled
- * as part of an Armour, Injury or Casualty roll)
+ * Once per game, Griff may apply a +1 modifier to an Agility Test he has made.
+ * This modifier may be applied after the roll has been made.
  */
 
 @RulesCollection(Rules.BB2020)
@@ -23,7 +19,6 @@ public class ConsummateProfessional extends Skill {
 
 	@Override
 	public void postConstruct() {
-		registerProperty(NamedProperties.canRerollSingleDieOncePerPeriod);
-		registerRerollSource(ReRolledActions.SINGLE_DIE, ReRollSources.CONSUMMATE_PROFESSIONAL);
+
 	}
 }
